be12a59b2091f64e3b43cf871a660cbabea3f452
David Fishburn <dfishburn@users.sourceforge.net>
2006-10-06 15:44:20 +0000

    Each of these files contain open quotes which the sql parser went into an endless loop processing.
    git-svn-id: https://ctags.svn.sourceforge.net/svnroot/ctags/trunk@465 c5d04d22-be80-434c-894e-aa346cc9e8e8

--- Test/bug1324663.sql
+++ Test/bug1324663.sql
@@ -1,1 +1,4 @@
+-- The sql parser would go into an endless loop with the open but no closing comment.
+-- ctags -f - --format=2 --excmd=pattern --fields=nks  --sort=no  --sql-types=cdfFlLPprstTvieURDVnxy bug1324663.sql
+--
 '\'/*'
