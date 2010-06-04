bf79ef4efd9fc992d2a569427124ec97f26b9ca9
dfishburn <dfishburn@c5d04d22-be80-434c-894e-aa346cc9e8e8>
2010-06-04 12:38:36 +0000

    SQL Parser updates for bug 2961855.
    The scope changes unfortunately also picked up OWNER and CREATOR names of
    functions and procedures.  So the scope changes should only be picked up if
    we are inside a PACKAGE first.
    
    git-svn-id: https://ctags.svn.sourceforge.net/svnroot/ctags/trunk@760 c5d04d22-be80-434c-894e-aa346cc9e8e8

--- sql.c
+++ sql.c
@@ -875,7 +875,16 @@ static void parseSubProgram (tokenInfo *const token)
 
 	if (isType (token, TOKEN_PERIOD))
 	{
-		addToScope(token, name->string);
+		// If this is an Oracle package, then the token->scope should
+		// already be set.  If this is the case, also add this value to the
+		// scope.
+		// If this is not an Oracle package, chances are the scope should be
+		// blank and the value just read is the OWNER or CREATOR of the
+		// function and should not be considered part of the scope.
+		if ( vStringLength(saveScope) > 0 )
+		{
+			addToScope(token, name->string);
+		}
 		readToken (token);
 		copyToken (name, token);
 		readToken (token);
