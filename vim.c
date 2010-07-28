ec0c139ab285d6d02d07294edf253a00c8480c5d
dfishburn <dfishburn@c5d04d22-be80-434c-894e-aa346cc9e8e8>
2010-07-28 11:38:19 +0000

    VIM Parser updates for bug 3032253.
    Vim parser did not handle an invalid 'command' format and hung".
    Now it handles and checks for these formats:
       command.
       command!.
       comma!.
       comma! ThisIsValid
    
    git-svn-id: https://ctags.svn.sourceforge.net/svnroot/ctags/trunk@762 c5d04d22-be80-434c-894e-aa346cc9e8e8

--- vim.c
+++ vim.c
@@ -362,6 +362,17 @@ static boolean parseCommand (const unsigned char *line)
 		if ((int) *cp == '!')
 			++cp;
 
+		if ((int) *cp != ' ')
+		{
+			/*
+			 * :command must be followed by a space.  If it is not, it is 
+			 * not a valid command.
+			 * Treat the line as processed and continue.
+			 */
+			cmdProcessed = TRUE;
+			goto cleanUp;
+		}
+
 		while (*cp && isspace ((int) *cp))
 			++cp; 
 	} 
@@ -389,7 +400,7 @@ static boolean parseCommand (const unsigned char *line)
 		else if (*cp == '-')
 		{
 			/* 
-			 * Read until the next space which sparates options or the name
+			 * Read until the next space which separates options or the name
 			 */
 			while (*cp && !isspace ((int) *cp))
 				++cp; 
