8aa63acc4538f169859bb8a30c20165f1ffa2ba3
K.Takata <kentkt@csc.jp>
2011-06-09 20:51:35 +0900

    /(?<=a).*b/ =~ "aab" doesn't match
    see:
    http://redmine.ruby-lang.org/issues/show/3568

--- regexec.c
+++ regexec.c
@@ -3628,10 +3628,12 @@ onig_search_gpos(regex_t* reg, const UChar* str, const UChar* end,
             prev = s;
             s += enclen(reg->enc, s);
 
+#if 0
             while (!ONIGENC_IS_MBC_NEWLINE(reg->enc, prev, end) && s < range) {
               prev = s;
               s += enclen(reg->enc, s);
             }
+#endif
           } while (s < range);
           goto mismatch;
         }
