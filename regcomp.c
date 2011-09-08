bd1897f15a33f84cf89f73b44dc79d85df9d1ce9
K.Takata <kentkt@csc.jp>
2011-09-08 21:18:36 +0900

    print its address including 0.

--- regcomp.c
+++ regcomp.c
@@ -6265,17 +6265,15 @@ print_compiled_byte_code_list(FILE* f, regex_t* reg)
   UChar* bp = reg->p;
   UChar* end = reg->p + reg->used;
 
-  fprintf(f, "code length: %d\n", reg->used);
+  fprintf(f, "code length: %d", reg->used);
 
   ncode = -1;
   while (bp < end) {
     ncode++;
-    if (bp > reg->p) {
-      if (ncode % 5 == 0)
-	fprintf(f, "\n%ld:", bp - reg->p);
-      else
-	fprintf(f, " %ld:", bp - reg->p);
-    }
+    if (ncode % 5 == 0)
+      fprintf(f, "\n%ld:", bp - reg->p);
+    else
+      fprintf(f, " %ld:", bp - reg->p);
     onig_print_compiled_byte_code(f, bp, &bp, reg->enc);
   }
 
