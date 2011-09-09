87da384a82f8376831f8d9741a717813d7e3ce47
K.Takata <kentkt@csc.jp>
2011-09-10 06:59:37 +0900

    remove redundant parentheses

--- regparse.c
+++ regparse.c
@@ -2842,10 +2842,10 @@ CLOSE_BRACKET_WITHOUT_ESC_WARN(ScanEnv* env, UChar* c)
 {
   if (onig_warn == onig_null_warn) return ;
 
-  if (IS_SYNTAX_BV((env)->syntax, ONIG_SYN_WARN_CC_OP_NOT_ESCAPED)) {
+  if (IS_SYNTAX_BV(env->syntax, ONIG_SYN_WARN_CC_OP_NOT_ESCAPED)) {
     UChar buf[WARN_BUFSIZE];
-    onig_snprintf_with_pattern(buf, WARN_BUFSIZE, (env)->enc,
-		(env)->pattern, (env)->pattern_end,
+    onig_snprintf_with_pattern(buf, WARN_BUFSIZE, env->enc,
+		env->pattern, env->pattern_end,
 		(UChar* )"regular expression has '%s' without escape", c);
     (*onig_warn)((char* )buf);
   }
