818a43efb6488b7314d466b6e8c1968d1ed054c1
K.Takata <kentkt@csc.jp>
2011-10-16 16:26:03 +0900

    update documents and version number (5.11.4)
     * Update the version number to 5.11.4.
     * Update LTVERSION to 3:0:2.

--- oniguruma.h
+++ oniguruma.h
@@ -37,7 +37,7 @@ extern "C" {
 #define ONIGURUMA
 #define ONIGURUMA_VERSION_MAJOR   5
 #define ONIGURUMA_VERSION_MINOR   11
-#define ONIGURUMA_VERSION_TEENY   3
+#define ONIGURUMA_VERSION_TEENY   4
 
 #ifdef __cplusplus
 # ifndef  HAVE_PROTOTYPES
