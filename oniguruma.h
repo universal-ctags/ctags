5d20809edb4de7cf408453654097cfde8b11b00e
K.Takata <kentkt@csc.jp>
2011-12-30 22:06:05 +0900

    add ONIG_SYN_POSIX_BRACKET_ALWAYS_ALL_RANGE option

--- oniguruma.h
+++ oniguruma.h
@@ -532,6 +532,7 @@ ONIG_EXTERN OnigSyntaxType*   OnigDefaultSyntax;
 #define ONIG_SYN_ALLOW_MULTIPLEX_DEFINITION_NAME_CALL (1U<<10)  /* (?<x>)(?<x>)(?&x) */
 
 /* syntax (behavior) in char class [...] */
+#define ONIG_SYN_POSIX_BRACKET_ALWAYS_ALL_RANGE  (1U<<19) /* (?a) doesn't affect POSIX brackets */
 #define ONIG_SYN_NOT_NEWLINE_IN_NEGATIVE_CC      (1U<<20) /* [^...] */
 #define ONIG_SYN_BACKSLASH_ESCAPE_IN_CC          (1U<<21) /* [..\w..] etc.. */
 #define ONIG_SYN_ALLOW_EMPTY_RANGE_IN_CC         (1U<<22)
