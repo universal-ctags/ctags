d606d14557b8f0d8b24c616c9e8c71ea017a5b68
K.Takata <kentkt@csc.jp>
2012-02-05 04:24:04 +0900

    update documents and version number (5.13.1)

--- oniguruma.h
+++ oniguruma.h
@@ -37,7 +37,7 @@ extern "C" {
 #define ONIGURUMA
 #define ONIGURUMA_VERSION_MAJOR   5
 #define ONIGURUMA_VERSION_MINOR   13
-#define ONIGURUMA_VERSION_TEENY   0
+#define ONIGURUMA_VERSION_TEENY   1
 
 #ifdef __cplusplus
 # ifndef  HAVE_PROTOTYPES
