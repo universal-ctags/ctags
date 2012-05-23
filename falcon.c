15aa8f5f73ff2a7d5e8fb32779b097235d151f5d
Steven Oliver <oliver.steven@gmail.com>
2012-05-22 22:25:31 -0400

    Update and cleanup falcon parser:
    * Add helper functions skipString and skipSpace
    * Update copyright header
    * Replace white space loops with new skipSpace function
    * Add another file type extension to parserDefinition
    * Add boolean function isIdentifierChar to identify alpha-numeric
      characters or underscores. Implement in loops.

--- falcon.c
+++ falcon.c
@@ -1,7 +1,7 @@
 /*
  * $Id$
  * 
- * Copyright (c) 2011 Steven Oliver <oliver.steven@gmail.com>
+ * Copyright (c) 2011, 2012 Steven Oliver <oliver.steven@gmail.com>
  * 
  * This source code is released for free distribution under the terms of the
  * GNU General Public License.
@@ -44,6 +44,38 @@ static kindOption FalconKinds [] = {
 /* 
  * Function Definitions
  */
+
+static boolean isIdentifierChar (int c)
+{
+    return (boolean) (isalnum (c) || c == '_');
+}
+
+static const char *skipSpace (const char *cp)
+{
+    while (isspace ((int) *cp))
+        ++cp;
+        
+    return cp;
+}
+
+static const char *skipString (const char *cp)
+{
+    const char *start = cp;
+    int escaped = 0;
+    
+    for (cp++; *cp; cp++)
+    {
+        if (escaped)
+            escaped--;
+        else if (*cp == '\\')
+            escaped++;
+        else if (*cp == *start)
+            return cp + 1;
+    }
+    
+    return cp;
+}
+
 static void findFalconTags (void)
 {
     vString *name = vStringNew ();
@@ -59,9 +91,9 @@ static void findFalconTags (void)
         if (strncmp ((const char*) cp, "function", (size_t) 8) == 0)
         {
             cp += 8;
-            while (isspace ((int) *cp))
-                ++cp;
-            while (isalnum ((int) *cp)  ||  *cp == '_')
+            cp = skipSpace (cp);
+            
+            while (isIdentifierChar ((int) *cp))
             {
                 vStringPut (name, (int) *cp);
                 ++cp;
@@ -73,9 +105,9 @@ static void findFalconTags (void)
         else if (strncmp ((const char*) cp, "class", (size_t) 5) == 0)
         {
             cp += 5;
-            while (isspace ((int) *cp))
-                ++cp;
-            while (isalnum ((int) *cp)  ||  *cp == '_')
+            cp = skipSpace (cp);
+            
+            while (isIdentifierChar ((int) *cp))
             {
                 vStringPut (name, (int) *cp);
                 ++cp;
@@ -87,9 +119,9 @@ static void findFalconTags (void)
         else if (strncmp ((const char*) cp, "load", (size_t) 4) == 0)
         {
             cp += 4;
-            while (isspace ((int) *cp))
-                ++cp;
-            while (isalnum ((int) *cp)  ||  *cp == '_')
+            cp = skipSpace (cp);
+            
+            while (isIdentifierChar ((int) *cp))
             {
                 vStringPut (name, (int) *cp);
                 ++cp;
@@ -107,7 +139,7 @@ static void findFalconTags (void)
  */
 extern parserDefinition* FalconParser (void)
 {
-    static const char *const extensions [] = { "fal", NULL };
+    static const char *const extensions [] = { "fal", "ftd", NULL };
     parserDefinition *def = parserNew ("Falcon");
     def->kinds      = FalconKinds;
     def->kindCount  = KIND_COUNT (FalconKinds);
