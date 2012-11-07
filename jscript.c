5ff0fd514e4e6b5269fe6d36fb0d39249a6e10a3
dfishburn <dfishburn@c5d04d22-be80-434c-894e-aa346cc9e8e8>
2012-11-07 03:06:10 +0000

    Parsing javascript quotes like this:       "('adding '+a+' and ' +b);\    // separate string using "\"
    Starts with a " and has a " at the end of the line, though that is part of a
    comment and I assume the \ after the ; means it is a line continuation
    indicator and the rest of the line should be ignored.  So, I have updated
    parseString() to do just that.
    
    This correctly parses:
        Test\jsFunc_tutorial.js
    
    git-svn-id: http://svn.code.sf.net/p/ctags/code/trunk@798 c5d04d22-be80-434c-894e-aa346cc9e8e8
    (cherry picked from commit 7b84378496fba3e09d0b1d439a11952d1ffd4e06)
    Signed-off-by: Masatake YAMATO <yamato@redhat.com>

--- jscript.c
+++ jscript.c
@@ -330,7 +330,16 @@ static void parseString (vString *const string, const int delimiter)
 		else if (c == '\\')
 		{
 			c = fileGetc(); /* This maybe a ' or ". */
-			vStringPut(string, c);
+			if (c == ' ' )
+			{
+				/* Line continuation character ignore reset of line */
+				fileSkipToCharacter ('\n');
+			}
+			else if (c != '\n');
+			{
+				/* Line continuation character already at end of line */
+				vStringPut(string, c);
+			}
 		}
 		else if (c == delimiter)
 			end = TRUE;
