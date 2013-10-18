5e1c4bbcb76d7615e1a1fa8b57cae13d7f8d6c85
dobkeratops <dobkeratopsgit@gmail.com>
2013-10-17 20:08:57 -0400

    Add Rust ctag tests

---
+++ Units/rust-test_input.d/input.rs
@@ -0,0 +1,90 @@
+#[feature(globs)];
+use std::*;
+use test_input2::*;
+mod test_input2;
+
+fn yada(a:int,c:Foo,b:test_input2::fruit::SomeStruct)->~str { a.to_str() }
+fn main() {	
+	use test_input2::fruit::*;	
+	io::println(foo_bar_test_func(SomeStruct{red_value:1,green_value:2,blue_value:3},(4,5)).to_str());
+	let a=Foo{foo_field_1:2};
+	a.my_method(1);
+	let c=a_cat(3);
+	let d=Foo{foo_field_1:a.foo_field_1+2}; a.test();
+	println(a.foo_field_1.to_str());
+}
+struct Bar(int);
+struct Baz(int);
+
+struct Foo{foo_field_1:int}
+struct Foo2 {
+		x:int,
+		y:int
+}
+
+impl Foo {
+	fn my_method(&self,_:int){ print("my_method of foo");}
+}
+
+enum Animal {
+	a_anteater(int),
+	a_bear(int),
+	a_cat(int),
+	a_dog(int),
+}
+
+trait Testable 
+{	fn test(&self);
+	fn test1(&self);
+	fn test2(&self);
+}
+trait DoZ {
+	fn do_z(&self);
+}
+
+impl Testable for Foo {
+	fn test(&self) {
+		println(self.foo_field_1.to_str());
+	}
+	fn test1(&self) 
+	{
+		println(self.foo_field_1.to_str());
+	}	fn test2(&self) 
+	{
+		println(self.foo_field_1.to_str());
+	}
+
+}
+impl DoZ for Foo {
+	fn do_z(&self) {
+		println(self.foo_field_1.to_str());
+	}
+}
+
+trait SuperTraitTest:Testable+DoZ {
+}
+
+fn gfunc<X:Testable+DoZ>(x:&X) {
+	let a1=a_anteater(1);
+	let a2=a_bear(1);
+	let a3=a_cat(1);
+	let a4=a_dog(1);
+	x.test();
+	x.do_z();
+}
+struct TraitedStructTest<X> {
+	x:X
+}
+fn some2(a:Animal) {
+	match a {
+		a_cat(x)=> println("cat"),
+		_ => println("not a cat")
+	}
+
+}
+
+
+
+
+
+
---
+++ Units/rust-test_input2.d/input.rs
@@ -0,0 +1,43 @@
+pub fn foo_bar_test_func(apples:fruit::SomeStruct,(oranges,lemon):(int,int))->int{
+	let some_var_name=2*oranges;
+	let a=SomeLongStructName{v:0};
+	println("a");println("b");	println("c");
+	veg::another_function(apples.red_value,oranges,lemon);
+	some_var_name-apples.red_value+lemon+a.v
+}
+
+
+pub mod fruit {
+	pub struct SomeStruct{
+		red_value:int,green_value:int,blue_value:int
+	}
+}
+fn free_func() {
+}
+
+impl SomeLongStructName {
+	fn fooo() {
+	}
+	fn baaz() {
+	}
+}
+
+
+pub struct SomeLongStructName {v:int}
+
+mod veg{
+	pub fn another_function(a:int,b:int,c:int)->int {
+		a+b+c
+	}
+}
+
+
+
+mod mineral {
+	fn granite() {
+	}
+	fn limestone() {
+	}
+	fn chalk() {
+	}
+}
