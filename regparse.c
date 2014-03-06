032cdd0be5bb16a17050bcd3fb41feb8fff74741
K.Takata <kentkt@csc.jp>
2014-03-06 20:04:56 +0900

    [bug] fix deadlock when pthread is used
    When onig_free_shared_cclass_table() is called from onig_end(),
    THREAD_ATOMIC_START is already called. Thus it may cause deadlock.

--- regparse.c
+++ regparse.c
@@ -5330,13 +5330,13 @@ i_free_shared_class(type_cclass_key* key, Node* node, void* arg ARG_UNUSED)
 extern int
 onig_free_shared_cclass_table(void)
 {
-  THREAD_ATOMIC_START;
+  /* THREAD_ATOMIC_START; */
   if (IS_NOT_NULL(OnigTypeCClassTable)) {
     onig_st_foreach(OnigTypeCClassTable, i_free_shared_class, 0);
     onig_st_free_table(OnigTypeCClassTable);
     OnigTypeCClassTable = NULL;
   }
-  THREAD_ATOMIC_END;
+  /* THREAD_ATOMIC_END; */
 
   return 0;
 }
