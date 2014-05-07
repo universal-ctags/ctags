5f6462e164987cd6d7226e1b6f79edcf82811511
dfishburn <dfishburn@c5d04d22-be80-434c-894e-aa346cc9e8e8>
2014-05-07 20:37:00 +0000

    Added new method findMatchingToken() to skip over blocks of code. Updated parseSwitch() to use the new method.
    Updated test\ui5.ui.controller.js with code that broke the switch statement.
    
    
    git-svn-id: https://svn.code.sf.net/p/ctags/code/trunk@809 c5d04d22-be80-434c-894e-aa346cc9e8e8

--- Test/ui5.controller.js
+++ Test/ui5.controller.js
@@ -1,5 +1,13 @@
 sap.ui.controller("app.my_form", {
 
+    successfulRequest: function(data) {
+        switch( data.mParameters.headers.SAAP_SERVICE ) {
+            case SAAP_SERVICE.APPROVAL_DETAIL:
+                if (thisForm.getController().mApproval) {
+                }
+        }
+    },
+
     onInit : function () {
         this.selectListView = null;
         sap.ui.getCore().byId("id_createButton").setEnabled(true);
