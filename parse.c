universal-ctags/ctags

commit be4a2117494ce73750cec3fd3c7cb4a60e722bcf

prepare parser running context in upper layer than each parser
In older parse.c and parser interface, the parser itself prepares
setjmp and trashBox. However, many parsers do the same.

This patch prepares experimental new interface; parse.c prepares both
setjmp and trashBox before calling actual parser. Both of the are
passed to the parser as arguments.

This new interface is applied to vhdl.c for evaluating its usefulness.

Signed-off-by: Masatake YAMATO <yamato@redhat.com>

parse.c
@@ -22,7 +22,7 @@
 #include "main.h"
 #define OPTION_WRITE
 #include "options.h"
-#include "parsers.h" 
+#include "parsers.h"
 #include "read.h"
 #include "routines.h"
 #include "vstring.h"
@@ -969,7 +969,7 @@ extern void initializeParsing (void)
 				accepted = TRUE;
 #endif
 			}
-			else if ((def->parser == NULL)  ==  (def->parser2 == NULL))
+			else if ((def->parser == NULL)  &&  (def->parser2 == NULL) && (def->parser_with_gc == NULL))
 				error (FATAL,
 		"%s parser definition must define one and only one parsing routine\n",
 					   def->name);
@@ -1450,6 +1450,19 @@ static rescanReason createTagsForFile (
 			lang->parser ();
 		else if (lang->parser2 != NULL)
 			rescan = lang->parser2 (passCount);
+		else if (lang->parser_with_gc != NULL)
+		{
+			TrashBox* trash_box = trashBoxNew ();
+			jmp_buf jbuf;
+
+			while (!setjmp (jbuf))
+			{
+				trashBoxDestroy (trash_box);
+				trash_box = trashBoxNew ();
+				rescan = lang->parser_with_gc (passCount, &jbuf, trash_box);
+			}
+			trashBoxDestroy (trash_box);
+		}
 		else if (lang->initialize != NULL)
 		{
 			parserInitialize init = lang->initialize;

parse.h
@@ -17,6 +17,7 @@
 #include "general.h"  /* must always come first */
 #include "parsers.h"  /* contains list of parsers */
 #include "strlist.h"
+#include "trashbox.h"
 
 /*
 *   MACROS
@@ -40,6 +41,9 @@ typedef enum {
 typedef void (*createRegexTag) (const vString* const name);
 typedef void (*simpleParser) (void);
 typedef rescanReason (*rescanParser) (const unsigned int passCount);
+typedef rescanReason (*rescanParserWithExceptionAndTrash) (const unsigned int passCount,
+							   jmp_buf *jbuf,
+							   TrashBox *tbox);
 typedef void (*parserInitialize) (langType language);
 typedef void (*parserFinalize) (langType language);
 
@@ -80,6 +84,7 @@ typedef struct {
 	parserFinalize finalize;       /* finalize routine, if needed */
 	simpleParser parser;           /* simple parser (common case) */
 	rescanParser parser2;          /* rescanning parser (unusual case) */
+	rescanParserWithExceptionAndTrash parser_with_gc;          /* rescanning parser (unusual case) */
 	unsigned int method;           /* See PARSE__... definitions above */
 	tgTableEntry *tg_entries;
 

vhdl.c
@@ -1,6 +1,6 @@
 /*
 *   $Id: vhdl.c 652 2008-04-18 03:51:47Z elliotth $
-* 
+*
 *   Copyright (c) 2008, Nicolas Vincent
 *
 *   This source code is released for free distribution under the terms of the
@@ -174,7 +174,7 @@ typedef struct sTokenInfo {
  *   DATA DEFINITIONS
  */
 static int Lang_vhdl;
-static jmp_buf Exception;
+static jmp_buf* Exception;
 static TrashBox* trash_box;
 
 /* Used to index into the VhdlKinds table. */
@@ -426,7 +426,7 @@ static void readToken (tokenInfo * const token)
 	switch (c)
 	{
 	case EOF:
-		longjmp (Exception, (int) ExceptionEOF);
+		longjmp (*Exception, (int) ExceptionEOF);
 		break;
 	case '(':
 		token->type = TOKEN_OPEN_PAREN;
@@ -807,20 +807,19 @@ static void parseVhdlFile (tokenInfo * const token)
 	} while (!isKeyword (token, KEYWORD_END));
 }
 
-static void findVhdlTags (void)
+static rescanReason findVhdlTags (const unsigned int passCount __unused__,
+				  jmp_buf *jbuf, TrashBox *tbox)
 {
 	tokenInfo * token;
-	exception_t exception;
 
-	trash_box = trashBoxNew ();
+	Exception = jbuf;
+	trash_box = tbox;
+
 	token = newToken ();
 
-	exception = (exception_t) (setjmp (Exception));
+	parseVhdlFile (token);
 
-	while (exception == ExceptionNone)
-		parseVhdlFile (token);
-
-	trashBoxDestroy (trash_box);
+	return RESCAN_NONE;
 }
 
 extern parserDefinition *VhdlParser (void)
@@ -830,7 +829,7 @@ extern parserDefinition *VhdlParser (void)
 	def->kinds = VhdlKinds;
 	def->kindCount = KIND_COUNT (VhdlKinds);
 	def->extensions = extensions;
-	def->parser = findVhdlTags;
+	def->parser_with_gc = findVhdlTags;
 	def->initialize = initialize;
 	return def;
 }

commit dfbc282c2e16cd6044e4181f150dbd1e2224eaa7

check multiple parser methods are not defined in a parser
This fixes a bug introduced in be4a2117494ce73750cec3fd3c7cb4a60e722bcf.

The old commit(be4a21) checked only the case that allparser, parser2 and parser_with_gc
were not set but didn't check more than one were set. This patch checks this case, too.

Signed-off-by: Masatake YAMATO <yamato@redhat.com>

parse.c
@@ -969,7 +969,7 @@ extern void initializeParsing (void)
 				accepted = TRUE;
 #endif
 			}
-			else if ((def->parser == NULL)  &&  (def->parser2 == NULL) && (def->parser_with_gc == NULL))
+			else if (((!!def->parser) + (!!def->parser2) + (!!def->parser_with_gc)) != 1)
 				error (FATAL,
 		"%s parser definition must define one and only one parsing routine\n",
 					   def->name);