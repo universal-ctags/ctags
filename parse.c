d0f70c2a1c75a69c15dc2aea7f5441c58fd1b48b
Masatake YAMATO <yamato@redhat.com>
2014-12-14 14:40:47 +0900

    use tasteLanguage before using getTwoGramLanguage
    getTwoGramLanguage is unreliable technique; it is heavily
    depends on prepared corpora and characters of input. Further
    more it takes much cost of calculation than other methods.
    
    Techniques in tasteLanguage are more reliable and take fewer
    cost. So in this change, ctags tries tasteLanguage before
    trying getTwoGramLanguage.
    
    Signed-off-by: Masatake YAMATO <yamato@redhat.com>

--- parse.c
+++ parse.c
@@ -576,6 +576,29 @@ struct getLangCtx {
     }                                                       \
 } while (0)
 
+static const struct taster {
+	vString* (* taste) (FILE *);
+        const char     *msg;
+} eager_tasters[] = {
+        {
+		.taste  = extractInterpreter,
+		.msg    = "interpreter",
+        },
+        {
+		.taste  = extracEmacsModeAtFirstLine,
+		.msg    = "emacs mode at the first line",
+        },
+        {
+		.taste  = extractEmacsModeLanguageAtEOF,
+		.msg    = "emacs mode at the EOF",
+        },
+        {
+		.taste  = extractVimFileType,
+		.msg    = "vim modeline",
+        },
+};
+static langType tasteLanguage (struct getLangCtx *glc, const struct taster *const tasters, int n_tasters);
+
 static langType getSpecLanguageCommon (const char *const spec, struct getLangCtx *glc,
 				       unsigned int nominate (const char *const, parserCandidate**))
 {
@@ -592,11 +615,18 @@ static langType getSpecLanguageCommon (const char *const spec, struct getLangCtx
 	}
 	else if (n_candidates > 1)
 	{
-		verbose ("		#candidates: %u\n", n_candidates);
         GLC_FOPEN_IF_NECESSARY(glc, fopen_error);
-		language = getTwoGramLanguage(glc->input, candidates, n_candidates);
+		language = tasteLanguage(glc,
+					 eager_tasters,
+					 ARRAY_SIZE(eager_tasters));
 		if (language == LANG_IGNORE)
-			language = candidates[0].lang;
+		{
+			verbose ("		#candidates: %u\n", n_candidates);
+			rewind(glc->input);
+			language = getTwoGramLanguage(glc->input, candidates, n_candidates);
+			if (language == LANG_IGNORE)
+				language = candidates[0].lang;
+		}
         /* At this point we are guaranteed that a language has been
          * selected:
          */
@@ -630,11 +660,6 @@ static langType getPatternLanguage (const char *const baseName,
 /* This function tries to figure out language contained in a file by
  * running a series of tests, trying to find some clues in the file.
  */
-struct taster {
-	vString* (* taste) (FILE *);
-        const char     *msg;
-};
-
 static langType
 tasteLanguage (struct getLangCtx *glc, const struct taster *const tasters, int n_tasters)
 {
@@ -655,25 +680,6 @@ tasteLanguage (struct getLangCtx *glc, const struct taster *const tasters, int n
 
     return LANG_IGNORE;
 }
-
-static const struct taster eager_tasters[] = {
-        {
-		.taste  = extractInterpreter,
-		.msg    = "interpreter",
-        },
-        {
-		.taste  = extracEmacsModeAtFirstLine,
-		.msg    = "emacs mode at the first line",
-        },
-        {
-		.taste  = extractEmacsModeLanguageAtEOF,
-		.msg    = "emacs mode at the EOF",
-        },
-        {
-		.taste  = extractVimFileType,
-		.msg    = "vim modeline",
-        },
-};
 
 static langType
 getFileLanguageInternal (const char *const fileName)
