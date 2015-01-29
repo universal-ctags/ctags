0d962bd39413088b63296c27b4b2c64305a20030
Kevin Brodsky <corax26@gmail.com>
2015-01-29 02:13:48 +0100

    C++: support C++11 enum classes
    Until now, an enum class was parsed as a class ('class'
    overrode 'enum'). As a workaround, ignore 'class' if the current
    declaration is DECL_ENUM to parse enum classes as simple enums.
    
    Also modify the cxx11enum unit test to include enum classes.

--- Units/cxx11enum.cpp.d/input.cpp
+++ Units/cxx11enum.cpp.d/input.cpp
@@ -2,8 +2,10 @@
 enum A:int {A_a, A_b, A_c};
 enum B:long {B_a, B_b, B_c};
 enum C:unsigned int {C_a, C_b, C_c};
+enum class E1:short {E1_a, E1_b, E1_c};
 
 class Foo {
   enum D:int {a, b, c};
+  enum class E2:short {E2_a, E2_b, E2_c};
   virtual void foo(enum D a);
 };
