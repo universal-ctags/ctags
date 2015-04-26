41eec84430bc5b74dcd9bb9ff648782eefbe662c
Jiří Techet <techet@gmail.com>
2015-04-26 20:37:51 +0200

    go: add function argument parsing test

---
+++ Units/go-funcs.d/input.go
@@ -0,0 +1,25 @@
+package main
+
+func f1() {
+}
+
+func f2(a int) {
+}
+
+func f3(a int) string {
+	return ""
+}
+
+func f4(a, b, c int, d, e, f string) (A, B, C int, D string) {
+	return 1, 2, 3, ""
+}
+
+type T int
+
+func (t *T) f5(/* comment */ a, /*comment*/ /*comment*/       b string, 
+	c  		[]int) (  /* */ int, string ) {
+	return 1, ""
+}
+
+func main() {
+}
