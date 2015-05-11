04a635ff39a9c728719e25103582b0ee2d392c7a
K.Takata <kentkt@csc.jp>
2015-05-11 22:57:23 +0900

    Import Ruby r49021 partly
    fix printf format conversion specifiers

--- regexec.c
+++ regexec.c
@@ -1294,7 +1294,7 @@ onig_print_statistics(FILE* f)
   int i;
   fprintf(f, "   count      prev        time\n");
   for (i = 0; OnigOpInfo[i].opcode >= 0; i++) {
-    fprintf(f, "%8d: %8d: %10ld: %s\n",
+    fprintf(f, "%8d: %8d: %10lu: %s\n",
 	    OpCounter[i], OpPrevCounter[i], OpTime[i], OnigOpInfo[i].name);
   }
   fprintf(f, "\nmax stack depth: %d\n", MaxStackDepth);
