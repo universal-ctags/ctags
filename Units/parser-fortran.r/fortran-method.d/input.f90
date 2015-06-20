80ba146caff3dba85d4330c89aa7eb5c504b0c2c
Qingming He <906459647@qq.com>
2015-06-21 00:06:09 +0800

    Fortran Units: Add non_overridable implementation test

--- Units/parser-fortran.r/fortran-method.d/input.f90
+++ Units/parser-fortran.r/fortran-method.d/input.f90
@@ -7,6 +7,7 @@ module test_method
     generic :: operator(+) => add_1d, add_2d
     generic :: assignment(=) => assigns
     procedure, deferred :: add_1d, add_2d, add_3d
+    procedure, non_overridable :: add_4d
     final clean
   end type atype
 end module test_method
