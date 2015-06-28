2842a301e0f9fd438b6ed6744d1d9c0269bf4569
Qingming He <906459647@qq.com>
2015-06-29 00:43:33 +0800

    Support Fortran file extensions .f03, .f08 and .f15

--- parsers/fortran.c
+++ parsers/fortran.c
@@ -2441,9 +2441,9 @@ static void initialize (const langType language)
 extern parserDefinition* FortranParser (void)
 {
 	static const char *const extensions [] = {
-		"f", "for", "ftn", "f77", "f90", "f95",
+		"f", "for", "ftn", "f77", "f90", "f95", "f03", "f08", "f15",
 #ifndef CASE_INSENSITIVE_FILENAMES
-		"F", "FOR", "FTN", "F77", "F90", "F95",
+		"F", "FOR", "FTN", "F77", "F90", "F95", "F03", "F08", "F15",
 #endif
 		NULL
 	};
