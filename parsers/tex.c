86e2fa27f2cae66270c509acc5197cc0fe7e7c54
Masatake YAMATO <yamato@redhat.com>
2015-07-05 20:24:30 +0900

    tex: don't make a tag for a zero length name
    ctags warns when following input gives:
    
         \section{}
    
    because a zero length name is passed to makeTagEntry.
    
    This patch makes the tex parser not pass such
    zero length names.
    
    Signed-off-by: Masatake YAMATO <yamato@redhat.com>

--- parsers/tex.c
+++ parsers/tex.c
@@ -453,8 +453,11 @@ static boolean parseTag (tokenInfo *const token, texKind kind)
 		if (useLongName)
 		{
 			vStringTerminate (fullname);
-			vStringCopy (name->string, fullname);
-			makeTexTag (name, kind);
+			if (vStringLength (fullname) > 0)
+			{
+				vStringCopy (name->string, fullname);
+				makeTexTag (name, kind);
+			}
 		}
 	}
 
