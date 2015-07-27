91b2d257ab5e0e98f7b270edcd64464bca22496f
Masatake YAMATO <yamato@redhat.com>
2015-07-27 19:17:24 +0900

    tmain: fix typos in function names
    Suggested by @k-tanaka.
    
    Signed-off-by: Masatake YAMATO <yamato@redhat.com>

--- Tmain/sorted-help-message.d/run.sh
+++ Tmain/sorted-help-message.d/run.sh
@@ -20,19 +20,19 @@ opt_sort()
     sort --ignore-case --stable
 }
 
-extrac_short_options()
+extract_short_options()
 {
     sed -n '/Usage:/,$p'  | \
     sed -n 's/\(^  -[[:alnum:]]\).*/\1/p'
 }
 
-extrac_long_options()
+extract_long_options()
 {
     sed -n '/Usage:/,$p'  | \
     sed -n 's/\(^  --[[:alnum:]_-<>]\).*/\1/p'
 }
 
-extrac_debug_options()
+extract_debug_options()
 {
     sed -n '/Usage:/,$p'  | \
     sed -n 's/\(^  --_[[:alnum:]_-<>]\).*/\1/p'
@@ -41,15 +41,15 @@ extrac_debug_options()
 
 
 diff \
-    <(print_help | extrac_short_options) \
-    <(print_help | extrac_short_options | opt_sort) &&
+    <(print_help | extract_short_options) \
+    <(print_help | extract_short_options | opt_sort) &&
 
 diff \
-    <(print_help | extrac_long_options) \
-    <(print_help | extrac_long_options | opt_sort) &&
+    <(print_help | extract_long_options) \
+    <(print_help | extract_long_options | opt_sort) &&
 
 diff \
-    <(print_help | extrac_debug_options) \
-    <(print_help | extrac_debug_options | opt_sort) &&
+    <(print_help | extract_debug_options) \
+    <(print_help | extract_debug_options | opt_sort) &&
 
 exit $?
