262835605045a9b676ae4e0253c0fc27e99cf519
Masatake YAMATO <yamato@redhat.com>
2015-08-06 18:46:06 +0900

    main, lregex: remove {optional} long flag
    {optional} long is redundant. It can be replaed with --kind-<LANG>=-
    option.
    
    Note about compatibilities:  I introduced it and exuberant ctags doesn't have it.
    
    Signed-off-by: Masatake YAMATO <yamato@redhat.com>

--- Tmain/lregex-list-kinds.d/run.sh
+++ Tmain/lregex-list-kinds.d/run.sh
@@ -2,9 +2,10 @@ CTAGS=$1
 
 ${CTAGS} --quiet --options=NONE -o - \
 	 --langdef=foo \
-	 --regex-foo=/a/\0/a/{optional} --foo-kinds=+a \
+	 --regex-foo=/a/\0/a/ --kinds-foo=-a --foo-kinds=+a \
 	 --regex-foo=/b/\0/b/ --foo-kinds=-b \
-	 --regex-foo=/c/\0/c/{optional} \
+	 --regex-foo=/c/\0/c/ \
+	 --kinds-foo=-c \
 	 --regex-foo=/d/\0/d/ \
 	 --list-kinds=foo
 
