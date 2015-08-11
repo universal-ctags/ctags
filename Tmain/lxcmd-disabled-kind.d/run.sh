26b54c23f9628b535402c4c283ff2f4074d17fa4
Masatake YAMATO <yamato@redhat.com>
2015-08-11 21:09:21 +0900

    Tmain: run xcmd related test cases only when coproc features is available
    Signed-off-by: Masatake YAMATO <yamato@redhat.com>

--- Tmain/lxcmd-disabled-kind.d/run.sh
+++ Tmain/lxcmd-disabled-kind.d/run.sh
@@ -1,4 +1,7 @@
 CTAGS=$1
 
+source ../utils.sh
+exit_if_no_coproc ${CTAGS}
+
 chmod u+x ./foo.sh
 $CTAGS --quiet --options=NONE --langdef=foo --xcmd-foo=./foo.sh --list-kinds=foo
--- Tmain/utils.sh
+++ Tmain/utils.sh
@@ -1,5 +1,23 @@
+__SKIP__=77
+
 remove_commit_id()
 {
     # Remove a commit id embedded in tags file
     sed -i -e '/!_TAG_PROGRAM_VERSION.*/s#/[^/]*/#//#' $1
+}
+
+is_feature_available()
+{
+    local ctags=$1
+    local feat=$2
+
+    if ! ${ctags} --list-features | grep -q "$feat"; then
+	echo "feature \"$feat\" is not available in $ctags"
+	exit ${__SKIP__}
+    fi
+}
+
+exit_if_no_coproc()
+{
+    is_feature_available $1 coproc
 }
