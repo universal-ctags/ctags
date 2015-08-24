universal-ctags/ctags

commit 4f11ded2206c90daa3abc95f0cfb14e00b6e6077

JavaScript: reject token other than identifier following "function"
This commit fixes following case:

    $ cat y.js
    function){
    $ ./ctags -o - y.js > /dev/null
    ctags: Warning: ignoring null tag in y.js

Signed-off-by: Masatake YAMATO <yamato@redhat.com>

parsers/jscript.c
@@ -1079,6 +1079,12 @@ static void parseFunction (tokenInfo *const token)
 	 */
 
 	readToken (name);
+	if (!isType (name, TOKEN_IDENTIFIER))
+	{
+		deleteToken (name);
+		return;
+	}
+
 	/* Add scope in case this is an INNER function */
 	addToScope(name, token->scope);
 

Tmain/js-null-tag-for-broken-input0.d/input.js
@@ -1,0 +1,1 @@
+function){

Tmain/js-null-tag-for-broken-input0.d/run.sh
@@ -1,0 +1,5 @@
+CTAGS=$1
+
+${CTAGS} --quiet --options=NONE -o - input.js > /dev/null
+
+exit $?

commit 92ee531ae5c0470771d1f98b71de350185c4ea4a

JavaScript: fix a memory leak
Fix a memory leak I introduced in 4f11ded2.

Signed-off-by: Masatake YAMATO <yamato@redhat.com>

parsers/jscript.c
@@ -1080,10 +1080,7 @@ static void parseFunction (tokenInfo *const token)
 
 	readToken (name);
 	if (!isType (name, TOKEN_IDENTIFIER))
-	{
-		deleteToken (name);
-		return;
-	}
+		goto cleanUp;
 
 	/* Add scope in case this is an INNER function */
 	addToScope(name, token->scope);
@@ -1113,6 +1110,7 @@ static void parseFunction (tokenInfo *const token)
 
 	findCmdTerm (token, FALSE, FALSE);
 
+ cleanUp:
 	vStringDelete (signature);
 	deleteToken (name);
 }