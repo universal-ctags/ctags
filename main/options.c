ab9f33f6a2417106011b197570f9ef688144c4ba
viccuad <me@viccuad.me>
2015-09-27 00:52:16 +0200

    Fixed typo that was added on main/options.c while updating the license info

--- main/options.c
+++ main/options.c
@@ -389,7 +389,7 @@ static optionDescription LongOptionDescription [] = {
 static const char* const License1 =
 "This program is free software; you can redistribute it and/or\n"
 "modify it under the terms of the GNU General Public License\n"
-"as published by the Free Software Foundation; either version 2 or (at your option) any later version.n"
+"as published by the Free Software Foundation; either version 2"
 "of the License, or (at your option) any later version.\n"
 "\n";
 static const char* const License2 =
