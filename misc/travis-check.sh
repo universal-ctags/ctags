bd95af3e24f94183ecdc92e7dec18fcfce038d8e
viccuad <me@viccuad.me>
2015-09-26 23:01:55 +0200

    Removed copyright holder from travis-check.sh

--- misc/travis-check.sh
+++ misc/travis-check.sh
@@ -1,7 +1,5 @@
 #!/bin/sh -e
 #
-#   Copyright (C) 2014 Masatake YAMATO
-#
 #   This program is free software; you can redistribute it and/or modify
 #   it under the terms of the GNU General Public License as published by
 #   the Free Software Foundation; either version 3 of the License, or
