9022fbb44a63f575ca5c5a45f089be1acf1b162c
Masatake YAMATO <yamato@redhat.com>
2015-10-06 20:44:53 +0900

    Escape control characters when writing tag fields
    This code is mostly taken from #162 authored by @b4n.
    
    This is for solving #534: css: Don't include any character below ASCII space in the tag name
    This also may solve bugs detected by afl-fuzz.
    
    This commit introduces a function for escaping unwanted character in a name.
    escaping is done in three rules.
    
    Rule 1:
    	- When a value contains a "\\t", this stands for a <Tab>.
    	- When a value contains a "\\r", this stands for a <CR>.
    	- When a value contains a "\\n", this stands for a <NL>.
    	- When a value contains a "\\\\", this stands for a single '\\' character.
    
    This rule is taken from docs/format.rst:
    
            1. The tags file is a list of lines, each line in the format::
    
                {tagname}<Tab>{tagfile}<Tab>{tagaddress}
    
               {tagname}
                Any identifier, not containing white space..
    
    Rule2:
    
    	Like rule1, 'start of heading', 'start of text', 'vertical tab' and
    	'form feed' are converted to "\\a", "\\b", "\\v", and "\\f".
    
    Rule3:
    
    	Convert the rest of character(char) between 0x00...0x20 and
    	0x7F to \x prefixed hex decimal number.

--- main/field.c
+++ main/field.c
@@ -22,54 +22,74 @@
 #include "options.h"
 #include "routines.h"
 
-#define DEFINE_FIELD_FULL(L,N, V, H, B) {			\
+static const char *renderEscapedString (const char* strval, const tagEntryInfo *const tag, vString* b);
+static const char *renderEscapedName   (const char* strval, const tagEntryInfo *const tag, vString* b);
+
+#define DEFINE_FIELD_FULL(L,N, V, H, B, F) {			\
 		.enabled       = V,				\
 		.basic         = B,				\
 		.letter        = L,				\
 		.name          = N,				\
 		.description   = H,				\
+		.renderEscaped = F,				\
+		.buffer        = NULL,  			\
 	}
 
-#define DEFINE_BASIC_FIELD(L,N,V,H) \
-    DEFINE_FIELD_FULL(L,N,V,H,TRUE)
+#define DEFINE_BASIC_FIELD(L,N,V,H,F)		\
+	DEFINE_FIELD_FULL(L,N,V,H,TRUE, F)
 
-#define DEFINE_FIELD(L,N,V,H) \
-    DEFINE_FIELD_FULL(L,N,V,H,FALSE)
+#define DEFINE_FIELD(L,N,V,H, F)		\
+	DEFINE_FIELD_FULL(L,N,V,H,FALSE, F)
 
 static fieldDesc fieldDescs [] = {
         /* BASIC FIELDS */
 	DEFINE_BASIC_FIELD ('N', "name",     TRUE,
-		      "tag name(fixed field)"),
+			    "tag name(fixed field)",
+			    renderEscapedName),
 	DEFINE_BASIC_FIELD ('F', "source",   TRUE,
-		      "source file(fixed field)"),
+			    "source file(fixed field)",
+			    renderEscapedString),
 	DEFINE_BASIC_FIELD ('P', "pattern",  TRUE,
-		      "pattern(fixed field)"),
+			    "pattern(fixed field)",
+			    NULL),
 
 	/* EXTENSION FIELDS */
 	DEFINE_FIELD ('a', "access",         FALSE,
-		      "Access (or export) of class members"),
+		      "Access (or export) of class members",
+		      NULL),
 	DEFINE_FIELD ('f', "file",           TRUE,
-		      "File-restricted scoping"),
+		      "File-restricted scoping",
+		      NULL),
 	DEFINE_FIELD ('i', "inherits",       FALSE,
-		      "Inheritance information"),
+		      "Inheritance information",
+		      renderEscapedName),
 	DEFINE_FIELD ('K', NULL,             FALSE,
-		      "Kind of tag as full name"),
+		      "Kind of tag as full name",
+		      NULL),
 	DEFINE_FIELD ('k', NULL,             TRUE,
-		      "Kind of tag as a single letter"),
+		      "Kind of tag as a single letter",
+		      NULL),
 	DEFINE_FIELD ('l', "language",       FALSE,
-			"Language of source file containing tag"),
+		      "Language of source file containing tag",
+		      NULL),
 	DEFINE_FIELD ('m', "implementation", FALSE,
-			"Implementation information"),
+		      "Implementation information",
+		      NULL),
 	DEFINE_FIELD ('n', "line",           FALSE,
-		      "Line number of tag definition"),
+		      "Line number of tag definition",
+		      NULL),
 	DEFINE_FIELD ('S', "signature",	     FALSE,
-		      "Signature of routine (e.g. prototype or parameter list)"),
+		      "Signature of routine (e.g. prototype or parameter list)",
+		      renderEscapedString),
 	DEFINE_FIELD ('s', NULL,             TRUE,/* needs sanitizing */
-		      "Scope of tag definition"),
+		      "Scope of tag definition",
+		      NULL),
 	DEFINE_FIELD ('t', "typeref",        TRUE,
-		      "Type and name of a variable or typedef"),
+		      "Type and name of a variable or typedef",
+		      renderEscapedName),
 	DEFINE_FIELD ('z', "kind",           FALSE,
-		      "Include the \"kind:\" key in kind field(use k or K)"),
+		      "Include the \"kind:\" key in kind field(use k or K)",
+		      NULL),
 };
 
 extern fieldDesc* getFieldDesc(fieldType type)
@@ -104,4 +124,92 @@ extern void printFields (void)
 
 	for (i = 0; i < sizeof (fieldDescs) / sizeof (fieldDescs [0]); i++)
 		printField (i);
+}
+
+static char valueToXDigit (int v)
+{
+	Assert (v >= 0 && v <= 0xF);
+
+	if (v >= 0xA)
+		return 'A' + (v - 0xA);
+	else
+		return '0' + v;
+}
+
+static const char *renderEscapedString (const char *s,
+					const tagEntryInfo *const tag,
+					vString* b)
+{
+	for(; *s; s++)
+	{
+		int c = *s;
+
+		/* escape control characters (incl. \t) */
+		if ((c > 0x00 && c <= 0x1F) || c == 0x7F || c == '\\')
+		{
+			vStringPut (b, '\\');
+
+			switch (c)
+			{
+				/* use a short form for known escapes */
+			case '\a':
+				c = 'a'; break;
+			case '\b':
+				c = 'b'; break;
+			case '\t':
+				c = 't'; break;
+			case '\n':
+				c = 'n'; break;
+			case '\v':
+				c = 'v'; break;
+			case '\f':
+				c = 'f'; break;
+			case '\r':
+				c = 'r'; break;
+			case '\\':
+				c = '\\'; break;
+			default:
+				vStringPut (b, 'x');
+				vStringPut (b, valueToXDigit ((c & 0xF0) >> 4));
+				vStringPut (b, valueToXDigit (c & 0x0F));
+				continue;
+			}
+		}
+		vStringPut (b, c);
+	}
+
+	return vStringValue (b);
+}
+
+static const char *renderEscapedName (const char* s,
+				      const tagEntryInfo *const tag,
+				      vString* b)
+{
+	const char* base = s;
+
+	for (; *s; s++)
+	{
+		int c = *s;
+		if ((c > 0x00 && c <= 0x1F) || c == 0x7F)
+		{
+			notice("Unexpected character (0 < *c && *c < 0x20) included in a tagEntryInfo: %s", base);
+			notice("File: %s, Line: %lu, Lang: %s, Kind: %c",
+			       tag->sourceFileName, tag->lineNumber, tag->language, tag->kind->letter);
+			if (Option.fatalWarnings)
+				error (FATAL, "Aborting");
+			notice("Escape the character");
+			break;
+		}
+		else if (c == '\\')
+			break;
+		else
+			continue;
+	}
+
+	if (*s == '\0')
+		return base;
+
+	vStringNCatS (b, base, s - base);
+
+	return renderEscapedString (s, tag, b);
 }
