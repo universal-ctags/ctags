ab86f7210d6dac7db5f47148f0715375a0d97506
Masatake YAMATO <yamato@redhat.com>
2015-10-08 11:26:03 +0900

    dts: fix a comment about the target language
    Signed-off-by: Masatake YAMATO <yamato@redhat.com>

--- parsers/dts.c
+++ parsers/dts.c
@@ -4,7 +4,7 @@
 *   This source code is released for free distribution under the terms of the
 *   GNU General Public License version 2 or (at your option) any later version.
 *
-*   This module contains functions for generating tags for YACC language files.
+*   This module contains functions for generating tags for DTS language files.
 */
 
 /*
