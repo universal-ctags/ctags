dfda3787afd4890e9d06c421196217d7e6f76067
Masatake YAMATO <yamato@redhat.com>
2015-11-23 15:49:38 +0900

    Use getInputFileName instead of accessing File.input.name directly
    Signed-off-by: Masatake YAMATO <yamato@redhat.com>

--- main/entry.c
+++ main/entry.c
@@ -965,7 +965,7 @@ static int   makePatternStringCommon (const tagEntryInfo *const tag,
 
 	line = readSourceLine (TagFile.vLine, tag->filePosition, NULL);
 	if (line == NULL)
-		error (FATAL, "bad tag in %s", vStringValue (File.input.name));
+		error (FATAL, "bad tag in %s", getInputFileName ());
 	if (tag->truncateLine)
 		truncateTagLine (line, tag->name, FALSE);
 
@@ -1210,7 +1210,7 @@ extern int makeTagEntry (const tagEntryInfo *const tag)
 	{
 		if (!getSourceLanguageAllowNullTag())
 			error (WARNING, "ignoring null tag in %s(line: %lu)",
-			       vStringValue (File.input.name), tag->lineNumber);
+			       getInputFileName (), tag->lineNumber);
 		goto out;
 	}
 
