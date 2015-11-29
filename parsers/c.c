universal-ctags/ctags

commit 4ca10085e029f630cbeac0e6ed07bc22631cbb9e

d: check blockName before building scope string
scope string is built in findScopeHierarchy.  blockName of
statementInfo is referred when building the string.

When input is broken as D syntax, blockName can hold unexpected
value. This was verified by Assert() statement.  Actually d-fha-0 test
case of fuzz-hitting-assertions made the assertion failure.

It is undesirable that broken input makes ctags stop. So in this commit
the assertion if replaced with code that just aborts building the
scope string. In addition lines expecting the scope string are also
skipped.

Removing duplicated vStringClear is suggested by @b4n.

Signed-off-by: Masatake YAMATO <yamato@redhat.com>

parsers/c.c
@@ -1278,9 +1278,10 @@ static void addOtherFields (tagEntryInfo* const tag, const tagType type,
 	}
 }
 
-static void findScopeHierarchy (vString *const string,
-								const statementInfo *const st)
+static boolean findScopeHierarchy (vString *const string, const statementInfo *const st)
 {
+	boolean found = FALSE;
+
 	vStringClear (string);
 	if (isType (st->context, TOKEN_NAME))
 		vStringCopy (string, st->context->name);
@@ -1300,9 +1301,16 @@ static void findScopeHierarchy (vString *const string,
 					continue;
 				}
 
+				found = TRUE;
 				vStringCopy (temp, string);
 				vStringClear (string);
-				Assert (isType (s->blockName, TOKEN_NAME));
+				if (! isType (s->blockName, TOKEN_NAME))
+				{
+					/* Information for building scope string
+					   is lacking. Maybe input is broken. */
+					found = FALSE;
+					break;
+				}
 				if (isType (s->context, TOKEN_NAME) &&
 					vStringLength (s->context->name) > 0)
 				{
@@ -1317,6 +1325,7 @@ static void findScopeHierarchy (vString *const string,
 		}
 		vStringDelete (temp);
 	}
+	return found;
 }
 
 static void makeExtraTagEntry (const tagType type, tagEntryInfo *const e,
@@ -1364,6 +1373,7 @@ static void makeTag (const tokenInfo *const token,
 		includeTag (type, isFileScope))
 	{
 		vString *scope = vStringNew ();
+		boolean isScopeBuilt;
 		/* Use "typeRef" to store the typename from addOtherFields() until
 		 * it's used in makeTagEntry().
 		 */
@@ -1378,11 +1388,12 @@ static void makeTag (const tokenInfo *const token,
 		e.kindName		= tagName (type);
 		e.kind			= tagLetter (type);
 
-		findScopeHierarchy (scope, st);
+		isScopeBuilt = findScopeHierarchy (scope, st);
 		addOtherFields (&e, type, st, scope, typeRef);
 
 		makeTagEntry (&e);
-		makeExtraTagEntry (type, &e, scope);
+		if (isScopeBuilt)
+			makeExtraTagEntry (type, &e, scope);
 		vStringDelete (scope);
 		vStringDelete (typeRef);
 	}

Units/fuzz-hitting-assertions.r/d-fha-0.d/README
@@ -1,0 +1,5 @@
+Because the input is invalid as D lagnauge source code.
+For this input parser cannot build proper scope information.
+
+expected.tags in this directory is just placeholder. Nothing is
+expected for invalid input.

Units/fuzz-hitting-assertions.r/d-fha-0.d/expected.tags
@@ -1,0 +1,1 @@
+T	input.d	/^template(){T;$/;"	m	file:

commit 647929b8b18a506558ec0a06a3574462261c5143

C++: reenable --extra=+q option
My commit 4ca10085e029f630cbeac0e6ed07bc22631cbb9e introduced a bug
that C++ parser cannot make class-qualified tag entries.

This commit fixes it.

Signed-off-by: Masatake YAMATO <yamato@redhat.com>

Units/parser-cpp.r/signature.cpp.d/README
@@ -1,0 +1,5 @@
+
+	int bar (a, b) int a; char b; {}
+
+I wonder how we should do with a, b.
+

Units/parser-cpp.r/signature.cpp.d/args.ctags
@@ -1,0 +1,3 @@
+--fields=+S
+--extra=+q
+--kinds-C++=+p

Units/parser-cpp.r/signature.cpp.d/expected.tags
@@ -1,0 +1,6 @@
+BAR::bar	input.cpp	/^char *BAR::bar (char *c, double d[]) const {}$/;"	f	class:BAR	signature:(char *c, double d[]) const
+a	input.cpp	/^int bar (a, b) int a; char b; {}$/;"	v
+b	input.cpp	/^int bar (a, b) int a; char b; {}$/;"	v
+bar	input.cpp	/^char *BAR::bar (char *c, double d[]) const {}$/;"	f	class:BAR	signature:(char *c, double d[]) const
+foo	input.cpp	/^void foo (int a, char b) {}$/;"	f	signature:(int a, char b)
+foobar	input.cpp	/^void foobar __ARGS ((int a, char b));$/;"	p	file:	signature:(int a, char b)

Units/review-needed.r/signature.cpp.t/expected.tags
@@ -1,4 +1,0 @@
-a	input.cpp	/^int bar (a, b) int a; char b; {}$/;"	v
-b	input.cpp	/^int bar (a, b) int a; char b; {}$/;"	v
-bar	input.cpp	/^char *BAR::bar (char *c, double d[]) const {}$/;"	f	class:BAR
-foo	input.cpp	/^void foo (int a, char b) {}$/;"	f

parsers/c.c
@@ -1345,8 +1345,13 @@ static boolean findScopeHierarchy (vString *const string, const statementInfo *c
 	boolean found = FALSE;
 
 	vStringClear (string);
+
 	if (isType (st->context, TOKEN_NAME))
+	{
 		vStringCopy (string, st->context->name);
+		found = TRUE;
+	}
+
 	if (st->parent != NULL)
 	{
 		vString *temp = vStringNew ();