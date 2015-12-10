ed38f7a3b1b8003cd1f7e18e2373cfe28bef247e
Colomban Wendling <ban@herbesfolles.org>
2015-12-10 17:39:15 +0100

    php: Recognize `php7` extension

--- parsers/php.c
+++ parsers/php.c
@@ -1470,7 +1470,7 @@ static void initializeZephirParser (const langType language)
 
 extern parserDefinition* PhpParser (void)
 {
-	static const char *const extensions [] = { "php", "php3", "php4", "php5", "phtml", NULL };
+	static const char *const extensions [] = { "php", "php3", "php4", "php5", "php7", "phtml", NULL };
 	parserDefinition* def = parserNew ("PHP");
 	def->kinds      = PhpKinds;
 	def->kindCount  = ARRAY_SIZE (PhpKinds);
