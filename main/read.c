dfe9904a879d9bff456fa829bf7630c2c50734e7
Colomban Wendling <ban@herbesfolles.org>
2016-01-24 15:26:11 +0100

    read: Avoid possible NULL dereference in getNthPrevCFromInputFile()
    Also, don't perform subtractions to check pointer bounds, to avoid
    unsigned value wraparound.  This is very unlikely as it would either
    mean a very large `nth` value or a very small value for the current
    line pointer, but better safe than sorry.

--- main/read.c
+++ main/read.c
@@ -516,9 +516,10 @@ extern int getcFromInputFile (void)
 extern int getNthPrevCFromInputFile (unsigned int nth, int def)
 {
 	const unsigned char *base = (unsigned char *) vStringValue (File.line);
+	const unsigned int offset = File.ungetchIdx + 1 + nth;
 
-	if (File.currentLine - File.ungetchIdx - 1 - nth >= base)
-		return (int) *(File.currentLine - File.ungetchIdx - 1 - nth);
+	if (File.currentLine != NULL && File.currentLine >= base + offset)
+		return (int) *(File.currentLine - offset);
 	else
 		return def;
 }
