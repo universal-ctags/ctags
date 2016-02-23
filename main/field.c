e8175bbb29f451487e7cae46e532e76bef3f0f44
Masatake YAMATO <yamato@redhat.com>
2016-02-23 15:21:30 +0900

    main, refactor: introduce new helper function vStringCatSWithEscaping
    This function is derived from functions in field.c.
    
    Signed-off-by: Masatake YAMATO <yamato@redhat.com>

--- main/field.c
+++ main/field.c
@@ -176,16 +176,6 @@ extern void printFields (void)
 		printField (i);
 }
 
-static char valueToXDigit (int v)
-{
-	Assert (v >= 0 && v <= 0xF);
-
-	if (v >= 0xA)
-		return 'A' + (v - 0xA);
-	else
-		return '0' + v;
-}
-
 static const char *renderAsIs (vString* b, const char *s)
 {
 	return s;
@@ -195,44 +185,7 @@ static const char *renderEscapedString (const char *s,
 					const tagEntryInfo *const tag,
 					vString* b)
 {
-	for(; *s; s++)
-	{
-		int c = *s;
-
-		/* escape control characters (incl. \t) */
-		if ((c > 0x00 && c <= 0x1F) || c == 0x7F || c == '\\')
-		{
-			vStringPut (b, '\\');
-
-			switch (c)
-			{
-				/* use a short form for known escapes */
-			case '\a':
-				c = 'a'; break;
-			case '\b':
-				c = 'b'; break;
-			case '\t':
-				c = 't'; break;
-			case '\n':
-				c = 'n'; break;
-			case '\v':
-				c = 'v'; break;
-			case '\f':
-				c = 'f'; break;
-			case '\r':
-				c = 'r'; break;
-			case '\\':
-				c = '\\'; break;
-			default:
-				vStringPut (b, 'x');
-				vStringPut (b, valueToXDigit ((c & 0xF0) >> 4));
-				vStringPut (b, valueToXDigit (c & 0x0F));
-				continue;
-			}
-		}
-		vStringPut (b, c);
-	}
-
+	vStringCatSWithEscaping (b, s);
 	return vStringValue (b);
 }
 
