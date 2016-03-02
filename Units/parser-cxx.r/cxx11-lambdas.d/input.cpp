6e24880bf44c5342c60461e95e3e4f656982d7da
Szymon Tomasz Stefanek <s.stefanek@gmail.com>
2016-03-02 18:46:25 +0100

    Make the lambdas test real, it now compiles.

--- Units/parser-cxx.r/cxx11-lambdas.d/input.cpp
+++ Units/parser-cxx.r/cxx11-lambdas.d/input.cpp
@@ -1,18 +1,24 @@
+// This code compiles with
+//  gcc -std=c++11 input.cpp
+
+template<typename T> void call(T x)
+{
+}
+
 int test()
 {
-	auto lambda1 = [](auto a,auto && b)
-	{
-		int x = 10;
-		return a < b + x;
+	auto l1 = [] { return 0; };
+
+	auto l2 = [](int a,int b) { return a > b ? a : b; };
+
+	auto l3 = [=](int a,int b) -> int {
+		auto l4 = [a,b](int c){ return a+b+c; };
+		return l4(5);
 	};
 	
-	[=]() -> int {
-		return [a,b](int c)
-		{
-			int y = 0;
-			return y;
-		}
-	};
-	
-	call(10,[lambda1](int a,int b) -> int { return -1; });
+	call(
+			[l1,l2,l3](int a,int b) -> int {
+				return l1() + l2(a,b) + l3(a,b);
+			}
+	);
 }
