1696a42ffe7f350294214188799e40bd67b389d9
Masatake YAMATO <yamato@redhat.com>
2016-03-13 15:31:02 +0900

    Tmain: ignore OldC and OldC++ parser in list-roles.d test case
    When introducing newly rewritten parser, we would like to provide the
    both new parser and old parser for debugging and providing migration
    period to users. In such case the prefix "Old" will be used to the
    name of old parser. The old parser should be ignored in this test
    case.
    
    Signed-off-by: Masatake YAMATO <yamato@redhat.com>

--- Tmain/list-roles.d/run.sh
+++ Tmain/list-roles.d/run.sh
@@ -16,17 +16,27 @@ ignore_xml()
     grep -v 'Glade\|Ant\|Maven2'
 }
 
+# When introducing newly rewritten parser, we would like to provide
+# the both new parser and old parser for debugging and providing
+# migration period to users. In such case the prefix "Old" will be
+# used to the name of old parser. The old parser should be ignored
+# in this test case.
+ignore_old()
+{
+    grep -v '^Old'
+}
+
 title ''
-${CTAGS} --quiet --options=NONE --_list-roles= | ignore_xml
+${CTAGS} --quiet --options=NONE --_list-roles= | ignore_xml | ignore_old
 
 title 'all:*'
-${CTAGS} --quiet --options=NONE --_list-roles='all:*' | ignore_xml
+${CTAGS} --quiet --options=NONE --_list-roles='all:*' | ignore_xml | ignore_old
 
 title 'C:*'
 ${CTAGS} --quiet --options=NONE --_list-roles='C:*'
 
 title 'all:h'
-${CTAGS} --quiet --options=NONE --_list-roles='all:h' | ignore_xml
+${CTAGS} --quiet --options=NONE --_list-roles='all:h' | ignore_xml | ignore_old
 
 title 'Sh:s'
 ${CTAGS} --quiet --options=NONE --_list-roles='Sh:s'
