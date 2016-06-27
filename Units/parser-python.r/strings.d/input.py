a0f6c0508bbba3531076d01d2347d6d4e3779a9b
Colomban Wendling <ban@herbesfolles.org>
2016-06-27 23:35:08 +0200

    Units, python: Add test for various simple strings

---
+++ Units/parser-python.r/strings.d/input.py
@@ -0,0 +1,16 @@
+
+VAR01 = ""
+VAR02 = "hello"
+VAR03 = ''
+VAR04 = 'hi'
+VAR05 = "\""
+VAR06 = '\''
+VAR07 = "'''"
+VAR08 = '"""'
+
+# also test not-too-bad behavior when strings are not terminated properly
+VAR09 = "oops
+VAR10 = 'oopsie
+VAR11 = "\"
+VAR12 = '\'
+VAR13 = 42
