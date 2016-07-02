58a4f66794969805c352aa390724a969fe309edb
Masatake YAMATO <yamato@redhat.com>
2016-07-03 03:18:24 +0900

    main: add MASTER field to --list-kinds-full option
    Signed-off-by: Masatake YAMATO <yamato@redhat.com>

--- Tmain/list-kinds-full.d/run.sh
+++ Tmain/list-kinds-full.d/run.sh
@@ -4,4 +4,5 @@
 CTAGS=$1
 
 ${CTAGS} --quiet --options=NONE --machinable --with-list-header --list-kinds-full=C
+${CTAGS} --quiet --options=NONE --machinable --with-list-header --list-kinds-full=C++
 
