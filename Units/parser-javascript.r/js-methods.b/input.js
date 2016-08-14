e066cfbc0e379928c342caf2361dfcc5c28745f5
Stefan Gojan <vollekannehoschi@gmail.com>
2016-08-14 16:56:35 +0200

    Javascript ES6 methods, test for #780

---
+++ Units/parser-javascript.r/js-methods.b/input.js
@@ -0,0 +1,9 @@
+// methods (functions of objects)
+// see: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/Method_definitions
+// http://www.ecma-international.org/ecma-262/6.0/#sec-method-definitions
+var Obj = {
+    myMethod(a, b) {
+    },
+	*myGenerator(a, b) {
+	}
+}
