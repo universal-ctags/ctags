cb587dfc019fb4d0d00ddc38f25a8935b160d435
K.Takata <kentkt@csc.jp>
2016-11-17 22:10:48 +0900

    Check return value for fopen()
    Merge kkos/oniguruma:
    * 5e66299f1a9c9879bfb393f639c5827f72282dac

--- st.c
+++ st.c
@@ -177,6 +177,8 @@ stat_col(void)
 {
     char fname[10+sizeof(long)*3];
     FILE *f = fopen((snprintf(fname, sizeof(fname), "/tmp/col%ld", (long)getpid()), fname), "w");
+    if (f == 0) return ;
+
     fprintf(f, "collision: %d / %d (%6.2f)\n", collision.all, collision.total,
 	    ((double)collision.all / (collision.total)) * 100);
     fprintf(f, "num: %d, str: %d, strcase: %d\n", collision.num, collision.str, collision.strcase);
