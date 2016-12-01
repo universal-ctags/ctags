ca8ff1893c2e4459370c0ee28665ea270bd8e995
K.Takata <kentkt@csc.jp>
2016-12-01 22:03:00 +0900

    Fix coding style
    [ci skip]

--- regparse.c
+++ regparse.c
@@ -5684,7 +5684,7 @@ node_linebreak(Node** np, ScanEnv* env)
 static int
 propname2ctype(ScanEnv* env, const char* propname)
 {
-  UChar* name = (UChar*)propname;
+  UChar* name = (UChar* )propname;
   int ctype = env->enc->property_name_to_ctype(ONIG_ENCODING_ASCII,
       name, name + strlen(propname));
   return ctype;
