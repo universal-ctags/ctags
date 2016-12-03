16322568bbd758e38279a6e2702748ed3929f1da
Masatake YAMATO <yamato@redhat.com>
2016-12-03 14:41:12 +0900

    main: use getSourceFileTagPath() when printing input file name in TAGS
    close #1217.
    
    Signed-off-by: Masatake YAMATO <yamato@redhat.com>

---
+++ Tmain/tag-relative-option-in-etags.d/run.sh
@@ -0,0 +1,27 @@
+# Copyright: 2015 Masatake YAMATO
+# License: GPL-2
+
+CTAGS=$1
+ARGS=--quiet --options=NONE
+O=TAGS.TMP
+
+for x in no yes default; do
+    touch ${x}-$O
+done
+
+(
+    cd indirect
+
+    ${CTAGS} ${ARGS} -e  -o ../no-${O}      --tag-relative=no  src/input.c
+    ${CTAGS} ${ARGS} -e  -o ../yes-${O}     --tag-relative=yes src/input.c
+    ${CTAGS} ${ARGS} -e  -o ../default-${O}                    src/input.c
+)
+
+for x in no yes default; do
+    echo '#' ${x}
+    # convert path separators in the output
+    sed -e 's|\\|/|g' ${x}-$O
+    rm ${x}-$O
+done
+
+exit $?
