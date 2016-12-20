3bf60c8a6844b9ff0c3dc1d252ce09d95816673c
Szymon Tomasz Stefanek <s.stefanek@gmail.com>
2016-12-21 00:20:34 +0100

    CXX: Improve an unit test

--- Units/parser-cxx.r/function-return-type-via-macro.d/input.cpp
+++ Units/parser-cxx.r/function-return-type-via-macro.d/input.cpp
@@ -15,4 +15,11 @@ AP_DECLARE(apr_status_t) ap_os_create_privileged_process(
 	const char * const *env,
 	apr_procattr_t *attr,
 	apr_pool_t *p);
+	
+LOCAL(struct errRecord *) handleError(z_streamp stream, size_t s) MAY_EXIT
+{
+	if (size == 0)
+		exit (1);
+	return ERR_REC;
+}
 
