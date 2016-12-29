07087623f75fa794cd83152f8ffb2dadaf570a5e
Masatake YAMATO <yamato@redhat.com>
2016-12-29 14:12:02 +0900

    etags-writer: truncate too long hints from TAIL
    Close #1247.
    
    Like javascript and css optimized by tools, there are source files that are made
    up of one very long line. Such input files can make TAGS file much larger.
    
    As we did in tags output, this commit truncates hints in TAGS output if they are
    longer than the value specified with --pattern-length-limit option.
    
    TODO: json output, xref output.
    Signed-off-by: Masatake YAMATO <yamato@redhat.com>

---
+++ Tmain/omit-long-patterns-etags.d/input.sh
@@ -0,0 +1,16 @@
+                                                                                                func96()
+{
+
+}
+                                                                                               func95()
+{
+
+}
+                                                                                                 func97()
+{
+
+}
+
+func96
+func95
+func97
