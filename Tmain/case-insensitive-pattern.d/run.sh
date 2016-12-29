c160cf6c11d9d261dd43e5bcbb79a195bb5908d6
Masatake YAMATO <yamato@redhat.com>
2016-12-29 15:46:16 +0900

    main: implement case insenstive glob matching
    ctags uses file name (glob) patterns and extensions to choose a
    proper parser for given input.
    
    extensions work well in case insenstive environment.
    However, patterns don't. This commit fixes this.
    
    GNU implementation provides FNM_CASEFOLD.
    I didn't use it to keep portability.
    
    Signed-off-by: Masatake YAMATO <yamato@redhat.com>

---
+++ Tmain/case-insensitive-pattern.d/run.sh
@@ -0,0 +1,10 @@
+# Copyright: 2016 Masatake YAMATO
+# License: GPL-2
+
+CTAGS=$1
+
+. ../utils.sh
+
+exit_if_no_case_insensitive_filenames "${CTAGS}"
+
+${CTAGS} --print-language MAKEFILE
--- Tmain/utils.sh
+++ Tmain/utils.sh
@@ -52,6 +52,11 @@ exit_if_win32()
 	is_feature_available $1 '!' win32
 }
 
+exit_if_no_case_insensitive_filenames()
+{
+	is_feature_available $1 case-insensitive-filenames
+}
+
 run_with_format()
 {
     echo '#' $*
