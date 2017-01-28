6b397033682ec355322b101f5c2d06835862e553
K.Takata <kentkt@csc.jp>
2017-01-29 05:13:13 +0900

    Add a test for (?~\S+)

--- testpy.py
+++ testpy.py
@@ -1611,6 +1611,7 @@ def main():
     x2("(?~abc|)", "abc", 0, 1)     # ???
     x2("(?~abc|def)x", "abcx", 1, 4)
     x2("(?~abc|def)x", "defx", 1, 4)
+    x2("^(?~\\S+)TEST", "TEST", 0, 4)
 
     # Perl syntax
     x2("\\Q()\\[a]\\E[b]", "()\\[a]b", 0, 7, syn=onigmo.ONIG_SYNTAX_PERL)
