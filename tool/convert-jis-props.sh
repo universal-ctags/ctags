1364ae3488cac0acbf2d61ef3ce2b62fe0c0f528
K.Takata <kentkt@csc.jp>
2017-04-03 22:14:47 +0900

    tool: Fix some scripts for gperf 3.1
    Gperf 3.1 uses size_t instead of long.

--- tool/convert-jis-props.sh
+++ tool/convert-jis-props.sh
@@ -3,9 +3,9 @@
 # Convert props.kwd to props.h using GNU gperf.
 #
 # Usage:
-#   ./tool/convert-jis-props enc/jis/props.kwd enc/jis/props.h
+#   ./tool/convert-jis-props.sh enc/jis/props.kwd enc/jis/props.h
 
 JIS_PROPS_OPTIONS='-k1,3 -7 -c -j1 -i1 -t -C -P -t --ignore-case -H onig_jis_property_hash -Q onig_jis_property_pool -N onig_jis_property'
 
 gperf $JIS_PROPS_OPTIONS $1 | \
-    sed 's/(int)(long)&((\([a-zA-Z_0-9 ]*[a-zA-Z_0-9]\) *\*)0)->\([a-zA-Z0-9_]*\),/(char)offsetof(\1, \2),/g' > $2
+    sed 's/(int)(\(long\|size_t\))&((\([a-zA-Z_0-9 ]*[a-zA-Z_0-9]\) *\*)0)->\([a-zA-Z0-9_]*\),/(char)offsetof(\2, \3),/g' > $2
