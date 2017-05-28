0cf0ddab824223a6639aa27dda790d5153a44ad1
K.Takata <kentkt@csc.jp>
2017-05-28 11:42:18 +0900

    appveyor: Create both stripped/unstripped binaries

--- win32/appveyor.bat
+++ win32/appveyor.bat
@@ -168,6 +168,8 @@ robocopy win32\license package\license > nul
 copy COPYING package\license > nul
 copy win32\mkstemp\COPYING.MinGW-w64-runtime.txt package\license > nul
 cd package
+7z a ..\ctags-%ver%-%ARCH%.debug.zip %filelist% docs license
+strip *.exe
 7z a ..\ctags-%ver%-%ARCH%.zip %filelist% docs license
 cd ..
 goto :eof
