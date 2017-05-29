991b94b4d98db5282dbd2c544314f1a9d2ccf291
K.Takata <kentkt@csc.jp>
2017-05-29 22:04:42 +0900

    tool: Support Emoji properties

--- tool/download-ucd.sh
+++ tool/download-ucd.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
 files='Blocks.txt CaseFolding.txt DerivedAge.txt DerivedCoreProperties.txt PropertyAliases.txt PropertyValueAliases.txt PropList.txt Scripts.txt SpecialCasing.txt UnicodeData.txt auxiliary/GraphemeBreakProperty.txt'
+emoji_files='emoji-data.txt'
 
-if [ -z $1 ]; then
-	echo "usage: $0 UNICODE_VERSION"
+if [ -z $1 ] || [ -z $2 ]; then
+	echo "usage: $0 UNICODE_VERSION EMOJI_VERSION"
 	exit 1
 fi
 UNICODE_VERSION=$1
+EMOJI_VERSION=$2
 
 # remove old files
 if [ -d $UNICODE_VERSION ]; then
 	cd $UNICODE_VERSION
-	rm -f $files
+	rm -f $files $emoji_files
 	rm -f GraphemeBreakProperty.txt
 	cd -
 fi
@@ -23,3 +25,6 @@ for i in $files; do
 	echo http://www.unicode.org/Public/${UNICODE_VERSION}/ucd/$i
 done | xargs wget
 mv GraphemeBreakProperty.txt auxiliary
+for i in $emoji_files; do
+	echo http://www.unicode.org/Public/emoji/${EMOJI_VERSION}/$i
+done | xargs wget
