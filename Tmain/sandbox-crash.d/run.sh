539991e13e5428ba6fb3850e2f9c7c3437f8a9f6
Masatake YAMATO <yamato@redhat.com>
2017-06-14 01:50:08 +0900

    Tmain: add cases for testing sandbox feature
    Signed-off-by: Masatake YAMATO <yamato@redhat.com>

---
+++ Tmain/sandbox-crash.d/run.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# Copyright: 2017 Masatake YAMATO
+# License: GPL-2
+
+CTAGS=$1
+
+. ../utils.sh
+is_feature_available $CTAGS debug
+is_feature_available $CTAGS sandbox
+is_feature_available ${CTAGS} '!' gcov
+
+{
+    echo '{"command":"generate-tags", "filename":"input.ctst", "size": 1}'
+    echo 'P'
+} | $CTAGS --quiet --options=NONE  --language-force=CTagsSelfTest --_interactive=sandbox
+
+exit $?
