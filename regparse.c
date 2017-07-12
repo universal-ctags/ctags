40945546578004bf40e6f884834bcad4054c70f7
K.Takata <kentkt@csc.jp>
2017-07-12 22:48:14 +0900

    Byte value expressed in octal must be smaller than 256
    CVE-2017-9226
    https://github.com/kkos/oniguruma/issues/55

--- regparse.c
+++ regparse.c
@@ -3644,7 +3644,7 @@ fetch_token(OnigToken* tok, UChar** src, UChar* end, ScanEnv* env)
       if (IS_SYNTAX_OP(syn, ONIG_SYN_OP_ESC_OCTAL3)) {
 	prev = p;
 	num = scan_unsigned_octal_number(&p, end, (c == '0' ? 2:3), enc);
-	if (num < 0) return ONIGERR_TOO_BIG_NUMBER;
+	if (num < 0 || 0xff < num) return ONIGERR_TOO_BIG_NUMBER;
 	if (p == prev) {  /* can't read nothing. */
 	  num = 0; /* but, it's not error */
 	}
