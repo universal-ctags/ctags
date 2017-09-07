e6f6f34fc9d79a813bfb187675dc768b9e57439c
Masatake YAMATO <yamato@redhat.com>
2017-09-07 21:48:09 +0900

    main: simplify conditions(reported by cppcheck)
    Signed-off-by: Masatake YAMATO <yamato@redhat.com>

--- main/parse.c
+++ main/parse.c
@@ -3634,8 +3634,7 @@ extern subparser *getNextSubparser(subparser *last,
 	t = getSubparserLanguage(r);
 	if (isLanguageEnabled (t) &&
 		(includingNoneCraftedParser
-		 || (!includingNoneCraftedParser &&
-			 ((((LanguageTable + t)->def->method) && METHOD_NOT_CRAFTED) == 0))))
+		 || ((((LanguageTable + t)->def->method) && METHOD_NOT_CRAFTED) == 0)))
 		return r;
 	else
 		return getNextSubparser (r, includingNoneCraftedParser);
