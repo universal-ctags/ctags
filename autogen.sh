ded3b0156ba6412a72b94b92a17a488047371018
Mahmoud Al-Qudsi <mqudsi@neosmart.net>
2017-09-16 13:27:16 -0500

    Fixed build script for BSD systems
    In autogen.sh, don't assume that the correct Makefile interpreter is
    `make`. The syntax of the Makefile generated by autotools is not
    compatible with BSD Make, and must be built with GNU Make (gmake)
    instead.
    
    In addition to detecting BSD platforms (via uname) and using gmake
    instead of make, autogen.sh also generates a BSDmakefile in the
    top-level directory. In its presence, BSD Make will use it instead of a
    generic Makefile, and BSD users will be informed that they should use
    gmake instead of make to buil the project.
    
    Tested on FreeBSD.

--- autogen.sh
+++ autogen.sh
@@ -5,7 +5,31 @@ set -xe
 type autoreconf || exit 1
 type pkg-config || exit 1
 
-ctags_files=`make -f makefiles/list-translator-input.mak --no-print-directory`
+gen_bsdmakefile() {
+	set +xe
+	echo -e '.DONE:\n\t@echo "Please use GNU Make (gmake) to build this project"\n.DEFAULT:\n\t@echo "Please use GNU Make (gmake) to build this project"' > BSDmakefile
+	set -xe
+}
+
+is_bsd() {
+	UNAME=`uname`
+	if echo $UNAME | grep -i bsd >/dev/null; then
+		#return true
+		return 0
+	else
+		#return false
+		return 1
+	fi
+}
+
+if is_bsd; then
+	MAKE="gmake"
+	gen_bsdmakefile
+else
+	MAKE="make"
+fi
+
+ctags_files=`${MAKE} -f makefiles/list-translator-input.mak --no-print-directory`
 misc/dist-test-cases > makefiles/test-cases.mak && \
     if autoreconf -vfi; then
 	if type perl > /dev/null; then
