563bf9756f60a95aced9f9b843ec5ac2f007a9cc
Masatake YAMATO <yamato@redhat.com>
2017-10-03 19:44:13 +0900

    go: put receiver type of function to scope
    Following changes by @techee are merged in this commit:
    simplify parseReceiver() a little
    
    There should be no functional difference for valid code. The difference
    in unit tests is for an invalid receiver and comes from the fact that
    the code now works with the token passed from the caller and takes into
    account the very first token too.
    
    Simplyfy the condtion about receiver_type_token local variable.
    Suggsted by @techee.

--- Units/parser-go.r/go-scope.d/input.go
+++ Units/parser-go.r/go-scope.d/input.go
@@ -13,3 +13,20 @@ func main() {
 	p := &Point{X: 5, Y: 12}
 	p.Render()
 }
+
+func (*Point) doNothing0() {
+}
+
+func (Point) doNothing1() {
+}
+
+func () wrongSyntax0() {
+}
+
+func (+) wrongSyntax1() {
+}
+
+func (a-) wrongSyntax2() {
+}
+
+func (brokenInput
--- parsers/go.c
+++ parsers/go.c
@@ -88,8 +88,17 @@ typedef enum {
 	GOTAG_VAR,
 	GOTAG_STRUCT,
 	GOTAG_INTERFACE,
-	GOTAG_MEMBER
+	GOTAG_MEMBER,
+	GOTAG_UNKNOWN,
 } goKind;
+
+typedef enum {
+	R_GOTAG_UNKNOWN_RECEIVER,
+} GoUnknownRole;
+
+static roleDesc GoUnknownRoles [] = {
+	{ true, "receiverType", "receiver type" },
+};
 
 static kindDefinition GoKinds[] = {
 	{true, 'p', "package", "packages"},
@@ -99,7 +108,9 @@ static kindDefinition GoKinds[] = {
 	{true, 'v', "var", "variables"},
 	{true, 's', "struct", "structs"},
 	{true, 'i', "interface", "interfaces"},
-	{true, 'm', "member", "struct members"}
+	{true, 'm', "member", "struct members"},
+	{true, 'u', "unknown", "unknown",
+	 .referenceOnly = false, ATTACH_ROLES (GoUnknownRoles)},
 };
 
 static const keywordTable GoKeywordTable[] = {
@@ -506,7 +517,12 @@ static int makeTag (tokenInfo *const token, const goKind kind,
 	const char *const name = vStringValue (token->string);
 
 	tagEntryInfo e;
-	initTagEntry (&e, name, &(GoKinds [kind]));
+
+	if (kind == GOTAG_UNKNOWN)
+		initRefTagEntry (&e, name, &(GoKinds [kind]),
+						 R_GOTAG_UNKNOWN_RECEIVER);
+	else
+		initTagEntry (&e, name, &(GoKinds [kind]));
 
 	if (!GoKinds [kind].enabled)
 		return CORK_NIL;
@@ -531,8 +547,42 @@ static int parsePackage (tokenInfo *const token)
 		return CORK_NIL;
 }
 
+static tokenInfo * parseReceiver (tokenInfo *const token)
+{
+	tokenInfo *receiver_type_token = NULL;
+	int nest_level = 1;
+
+	/* Looking for an identifier before ')'. */
+	while (nest_level > 0 && !isType (token, TOKEN_EOF))
+	{
+		if (isType (token, TOKEN_IDENTIFIER))
+		{
+			if (!receiver_type_token)
+				receiver_type_token = newToken ();
+			copyToken (receiver_type_token, token);
+		}
+
+		readToken (token);
+		if (isType (token, TOKEN_OPEN_PAREN))
+			nest_level++;
+		else if (isType (token, TOKEN_CLOSE_PAREN))
+			nest_level--;
+	}
+
+	if (nest_level > 0 && receiver_type_token)
+	{
+		deleteToken (receiver_type_token);
+		receiver_type_token = NULL;
+	}
+
+	readToken (token);
+	return receiver_type_token;
+}
+
 static void parseFunctionOrMethod (tokenInfo *const token, const int scope)
 {
+	tokenInfo *receiver_type_token = NULL;
+
 	// FunctionDecl = "func" identifier Signature [ Body ] .
 	// Body         = Block.
 	//
@@ -540,14 +590,15 @@ static void parseFunctionOrMethod (tokenInfo *const token, const int scope)
 	// Receiver     = "(" [ identifier ] [ "*" ] BaseTypeName ")" .
 	// BaseTypeName = identifier .
 
-	// Skip over receiver.
+	// Pick up receiver type.
 	readToken (token);
 	if (isType (token, TOKEN_OPEN_PAREN))
-		skipToMatched (token);
+		receiver_type_token = parseReceiver (token);
 
 	if (isType (token, TOKEN_IDENTIFIER))
 	{
 		tokenInfo *functionToken = newToken ();
+		int func_scope;
 
 		copyToken (functionToken, token);
 
@@ -560,7 +611,12 @@ static void parseFunctionOrMethod (tokenInfo *const token, const int scope)
 
 		vStringStripLeading (signature);
 		vStringStripTrailing (signature);
-		makeTag (functionToken, GOTAG_FUNCTION, scope, signature->buffer);
+		if (receiver_type_token)
+			func_scope = makeTag(receiver_type_token, GOTAG_UNKNOWN,
+								 scope, NULL);
+		else
+			func_scope = scope;
+		makeTag (functionToken, GOTAG_FUNCTION, func_scope, signature->buffer);
 		deleteToken (functionToken);
 		vStringDelete(signature);
 
@@ -576,6 +632,9 @@ static void parseFunctionOrMethod (tokenInfo *const token, const int scope)
 		if (isType (token, TOKEN_OPEN_CURLY))
 			skipToMatched (token);
 	}
+
+	if (receiver_type_token)
+		deleteToken(receiver_type_token);
 }
 
 static void parseStructMembers (tokenInfo *const token, const int scope)
