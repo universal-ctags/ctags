ff535c82127a0a354fd07e80bc0ca582cc2e2087
Colomban Wendling <ban@herbesfolles.org>
2017-10-05 14:04:03 -0700

    jscript: Add test case for CR/LF line ends handling

---
+++ Units/parser-javascript.r/js-crlf.d/input.js
@@ -0,0 +1,25 @@
+
+var v1, v2
+var v3 = 1
+,   v4 = 2
+,   v5 = 3
+function f1() {
+  var f1_c1 = {
+    f1_c1_m1:
+    function() {
+    }
+    ,
+    f1_c1_m2
+    :
+    function
+    (
+    )
+    {
+    }
+  }
+}
+
+var v6 = "hello\
+var bug1 = world;"
+var v7 = "hello\var bug2 = world"
+var v8;
