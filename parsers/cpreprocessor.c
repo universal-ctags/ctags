universal-ctags/ctags

commit 270d31260b3a89227ec420b05cb69ce04846aa66

CPreProcessor,refactor: specify field name explicitly when initializing Cpp
Signed-off-by: Masatake YAMATO <yamato@redhat.com>

parsers/cpreprocessor.c
@@ -157,26 +157,33 @@ void cppPopExternalParserBlock()
 
 
 static cppState Cpp = {
-	LANG_IGNORE,
-	NULL,        /* ungetBuffer */
-	0,           /* ungetBufferSize */
-	NULL,        /* ungetPointer */
-	0,           /* ungetDataSize */
-	false,       /* resolveRequired */
-	false,       /* hasAtLiteralStrings */
-	false,       /* hasCxxRawLiteralStrings */
-	false,	     /* hasSingleQuoteLiteralNumbers */
-	NULL,        /* defineMacroKind */
-	.macroUndefRoleIndex   = ROLE_INDEX_DEFINITION,
-	NULL,	     /* headerKind */
+	.lang = LANG_IGNORE,
+	.ungetBuffer = NULL,
+	.ungetBufferSize = 0,
+	.ungetPointer = NULL,
+	.ungetDataSize = 0,
+	.resolveRequired = false,
+	.hasAtLiteralStrings = false,
+	.hasCxxRawLiteralStrings = false,
+	.hasSingleQuoteLiteralNumbers = false,
+	.defineMacroKind = NULL,
+	.macroUndefRoleIndex = ROLE_INDEX_DEFINITION,
+	.headerKind = NULL,
 	.headerSystemRoleIndex = ROLE_INDEX_DEFINITION,
 	.headerLocalRoleIndex = ROLE_INDEX_DEFINITION,
-	{
-		DRCTV_NONE,  /* state */
-		false,       /* accept */
-		NULL,        /* tag name */
-		0,           /* nestLevel */
-		{ {false,false,false,false} }  /* ifdef array */
+	.directive = {
+		.state = DRCTV_NONE,
+		.accept = false,
+		.name = NULL,
+		.nestLevel = 0,
+		.ifdef = {
+			{
+				.ignoreAllBranches = false,
+				.singleBranch = false,
+				.singleBranch = false,
+				.ignoring = false,
+			}
+		}
 	}  /* directive */
 };
 

commit d4c747a0c6bbe7a263772870af0a1bd135e62cfe

Fix a typo introduced in 270d31260b3a89227ec420b05cb69ce04846aa66

parsers/cpreprocessor.c
@@ -188,7 +188,7 @@ static cppState Cpp = {
 			{
 				.ignoreAllBranches = false,
 				.singleBranch = false,
-				.singleBranch = false,
+				.branchChosen = false,
 				.ignoring = false,
 			}
 		}