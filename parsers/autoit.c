cb0261b2412b931e25c4f880f8f98c778cfaec20
Skif-off <Skif-off@users.noreply.github.com>
2017-11-19 22:55:40 +0400

    Upd AutoIt parser: Add #include & #region, some optimizations

--- parsers/autoit.c
+++ parsers/autoit.c
@@ -1,12 +1,16 @@
 /*
-*   Copyright (c) 2017, S
+*   Copyright (c) 2017, Alexey Olenich
 *
 *   This source code is released for free distribution under the terms of the
 *   GNU General Public License version 2 or (at your option) any later version.
 *
 *   This module contains functions for generating tags for AutoIt functions.
-*   Homepage https://www.autoitscript.com/site/autoit/
+*   Homepage             https://www.autoitscript.com/site/autoit/
 *   Online Documentation https://www.autoitscript.com/autoit3/docs/
+*
+*   Functions:     (?i)^func[ \t]{1,}([a-z0-9_]{1,}) >> \1
+*   Include Files: (?i)^#include(.*?)(<|")(.*?)("|>) >> \3
+*   Regions:       (?i)^#region[ \t]*(.*?)           >> \1
 */
 
 /*
@@ -25,11 +29,15 @@
 *   DATA DEFINITIONS
 */
 typedef enum {
-	K_FUNCTION
+	K_FUNCTION,
+	K_INCLUDE,
+	K_REGION
 } AutoItKind;
 
-static kindDefinition AutoItKinds[] = {
-	{ true, 'f', "func", "functions" }
+static kindDefinition AutoItKinds [] = {
+	{ true, 'f', "func", "functions" },
+	{ true, 'i', "include", "include files" },
+	{ true, 'r', "region", "regions" }
 };
 
 /*
@@ -42,30 +50,82 @@ static void findAutoItTags (void)
 
 	while ((line = readLineFromInputFile ()) != NULL)
 	{
-		while (isspace ((int) *line))
-			++line;
-		const unsigned char* cp = line;
-		/* Min. string "func a()" == 8 */
-		if (strlen ((const char *) line) >= 8 &&
-			(line[0] == 'F' || line[0] == 'f') &&
-			(line[1] == 'U' || line[1] == 'u') &&
-			(line[2] == 'N' || line[2] == 'n') &&
-			(line[3] == 'C' || line[3] == 'c') &&
-			isspace ((int) line[4]))
+		const unsigned char* p = line;
+		if (p [0] == '#')
 		{
-			cp += 4;
-			while (isspace ((int) *cp))
-				++cp;
-			while (isalnum ((int) *cp) || *cp == '_')
+			/* min. string "#include<a> == 11 */
+			if ((p [1] == 'I' || p [1] == 'i') &&
+				strlen ((const char *) p) >= 11 &&
+				(p [2] == 'N' || p [2] == 'n') &&
+				(p [3] == 'C' || p [3] == 'c') &&
+				(p [4] == 'L' || p [4] == 'l') &&
+				(p [5] == 'U' || p [5] == 'u') &&
+				(p [6] == 'D' || p [6] == 'd') &&
+				(p [7] == 'E' || p [7] == 'e') &&
+				(isspace ((int) p [8]) || p [8] == '"' || p [8] == '<'))
 			{
-				vStringPut (name, (int) *cp);
-				++cp;
+				p += 9;
+				while (isspace ((int) *p) || *p == '"' || *p == '<')
+					++p;
+				while (*p != '\0')
+				{
+					if (*p != '"' && *p != '>')
+						vStringPut (name, (int) *p);
+					++p;
+				}
+				makeSimpleTag (name, K_INCLUDE);
+				vStringClear (name);
 			}
-			while (isspace ((int) *cp))
-				++cp;
-			if (*cp == '(')
-				makeSimpleTag (name, K_FUNCTION);
-			vStringClear (name);
+			/* min. string "#region" > 7 */
+			else if ((p [1] == 'R' || p [1] == 'r') &&
+				strlen ((const char *) p) > 8 &&
+				(p [2] == 'E' || p [2] == 'e') &&
+				(p [3] == 'G' || p [3] == 'g') &&
+				(p [4] == 'I' || p [4] == 'i') &&
+				(p [5] == 'O' || p [5] == 'o') &&
+				(p [6] == 'N' || p [6] == 'n'))
+			{
+				p += 7;
+				while (isspace ((int) *p))
+					++p;
+				while (*p != '\0')
+				{
+					vStringPut (name, (int) *p);
+					++p;
+				}
+				makeSimpleTag (name, K_REGION);
+				vStringClear (name);
+			}
+		}
+		else
+		{
+			/* skip white space */
+			while (isspace ((int) *p))
+				++p;
+			/* min. string "func a()" == 8 */
+			if ((p [0] == 'F' || p [0] == 'f') &&
+				strlen ((const char *) p) >= 8 &&
+				(p [1] == 'U' || p [1] == 'u') &&
+				(p [2] == 'N' || p [2] == 'n') &&
+				(p [3] == 'C' || p [3] == 'c') &&
+				isspace ((int) p [4]))
+			{
+				p += 5;
+				while (isspace ((int) *p))
+					++p;
+				while (isalnum ((int) *p) || *p == '_')
+				{
+					vStringPut (name, (int) *p);
+					++p;
+				}
+				while (isspace ((int) *p))
+					++p;
+				if (*p == '(')
+				{
+					makeSimpleTag (name, K_FUNCTION);
+					vStringClear (name);
+				}
+			}
 		}
 	}
 	vStringDelete (name);
@@ -73,7 +133,7 @@ static void findAutoItTags (void)
 
 parserDefinition *AutoItParser (void)
 {
-	static char const *extensions[] = { "au3", "AU3", NULL };
+	static char const *extensions[] = { "au3", "AU3", "aU3", "Au3", NULL };
 	parserDefinition* def = parserNew ("AutoIt");
 	def->kindTable      = AutoItKinds;
 	def->kindCount  = ARRAY_SIZE (AutoItKinds);
