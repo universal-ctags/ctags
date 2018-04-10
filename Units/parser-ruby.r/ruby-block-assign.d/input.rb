68ea0f84f69fecaebb27ce11461cb27427eb3640
Tambet Arak <tambet.arak+nospam@gmail.com>
2018-04-10 11:51:14 +0300

    Ruby: allow shortcut assignment ops (in block assignment)
    Extend block assignment parsing by allowing "shortcut assignment"
    operators, e.g. `||=`, `+=`.
    
    (The commit log is edited by @masatake.)

--- Units/parser-ruby.r/ruby-block-assign.d/input.rb
+++ Units/parser-ruby.r/ruby-block-assign.d/input.rb
@@ -1,6 +1,9 @@
 m = module Foo
 end
 
+m += module Quux
+end
+
 c = class Bar
   x = def method_a
     if 1
@@ -9,17 +12,32 @@ c = class Bar
   end
 
   def method_b
-    x = if 1
-    else
-    end
-  end
-
-  def method_c
     x = while 1 do
       break
     end
   end
 
-  def method_d
+  def method_c
+    x = if 1
+    else
+    end
   end
+
+  def method_d
+    x += if 1
+    else
+    end
+  end
+
+  def method_e
+    x ||= if 1
+    else
+    end
+  end
+
+  def method_f
+  end
+end
+
+c ||= class Zook
 end
