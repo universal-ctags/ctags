e8b65bbd241fee713683b6486f779bb4566101d9
Björn Andersson <andebjor@gmail.com>
2018-07-11 15:30:56 +0200

    Rework `Units/review-needed.r/matlab_test.m.t`
    Make it a complete, but silly, functional Matlab class without warnings
    issued by the built-in linter in the Matlab editor.
    
    Add some comments and arguments to functions to stress the parser even
    more.

--- Units/review-needed.r/matlab_test.m.t/input.m
+++ Units/review-needed.r/matlab_test.m.t/input.m
@@ -1,15 +1,33 @@
-classdef class1 < handle
-    methods
+classdef input < handle
+    methods(Static)
         function [x,y,z] = classfunc1
-        function x = classfunc2
-        function classfunc3
+            [x, y, z] = func1();
+        end
+        function x = classfunc2()
+            x = func2(1);
+        end
+        function classfunc3;
+            func3();
         end
     end
 end
 
-function [x,y,z] = func1 
-function x = func2 
-function func3 
+function [x,y,z] = func1() % Silly function with multiple outputs
+    x = 1;
+    y = 2;
+    z = 3;
+end
+function x = func2(arg) 
+    x = arg;
+end
+function func3() 
+    % Silly function without any arguments
+    A = magic(4);
+    R = randn(3,4,5);
 
-A = magic(4);
-R = randn(3,4,5);
+    disp('A:');
+    disp(A);
+
+    disp('R:');
+    disp(R);
+end
