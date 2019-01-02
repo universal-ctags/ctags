33bc04825df8b3e6bf94af12e7ffed9dfc3fad19
Karol Samborski <karol.samborski@espeo.eu>
2019-01-02 23:44:56 +0100

    TypeScript: new parser
    @masatake does following in merging process:
    * squash the original commits into this one,
    * adapt to unwind input stream API,
    * add --sort=no to all new test cases, and
    * replace __inline with CTAGS_INLINE

---
+++ Units/parser-typescript.r/ts-type.d/input.ts
@@ -0,0 +1,12 @@
+type MyString = "mystring";
+type MyUnion = MyString | "other" | 99;
+export type MyExportedUnion = MyUnion | "default" //;
+;
+type StringOrNumber = string | number;
+type Text = string | { text: string };
+type NameLookup = Dictionary<string, Person>;
+type ObjectStatics = typeof Object;
+type Callback<T> = (data: T) => void;
+type Pair<T> = [T, T];
+type Coordinates = Pair<number>;
+type Tree<T> = T | { left: Tree<T>, right: Tree<T> };
