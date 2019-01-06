b50d191f9e7dc60093d242b63880727a622f8f3b
Szymon Tomasz Stefanek <s.stefanek@gmail.com>
2019-01-07 00:23:04 +0100

    CXX: Add unit tests for default values in function signatures
    @masatake adds params7 in the test case.

--- Units/parser-cxx.r/signature.cpp.d/input.cpp
+++ Units/parser-cxx.r/signature.cpp.d/input.cpp
@@ -8,3 +8,15 @@ void foo (int a, char b) {}
 char *BAR::bar (char *c, double d[]) const {}
 
 void foobar __ARGS ((int a, char b));
+
+
+void params1(const char * c = "blah");
+void params2(char x = 'x');
+void params3(char x = ' ');
+void params4(char x = ',');
+void params5(char x = '\n');
+void params6(char x = '\t',int n = 10,const char * v = "a string with\na newline");
+void params7(char x = '	',	// This is  tab char
+	     float p = 3.14,
+	     const char * v = "a string with a tab char:	"
+	     );
