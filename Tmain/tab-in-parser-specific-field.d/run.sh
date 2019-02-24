cd909ce07b6ea53425d05bc940653be9adc85ca7
Masatake YAMATO <yamato@redhat.com>
2019-02-24 10:33:27 +0900

    Tmain: add case for testing "--output-format=e-ctags -o - --sort=no" option combination
    Signed-off-by: Masatake YAMATO <yamato@redhat.com>

--- Tmain/tab-in-parser-specific-field.d/run.sh
+++ Tmain/tab-in-parser-specific-field.d/run.sh
@@ -5,9 +5,19 @@ CTAGS=$1
 
 . ../utils.sh
 
-echo "# Universal-ctags output format"
-$CTAGS --options=NONE --options=./foo.ctags --output-format=u-ctags  -o - input.foo
+run()
+{
+	echo '#'
+	echo '#' with $1
+	echo '#'
 
-echo "# Exuberant-ctags output format"
-echo "# fun1 has a tab char in fprop fields. So ctags drops it silent."
-$CTAGS --options=NONE --options=./foo.ctags --output-format=e-ctags  -o - input.foo
+	echo "# Universal-ctags output format"
+	$CTAGS --options=NONE --options=./foo.ctags "$1" --output-format=u-ctags  -o - input.foo
+
+	echo "# Exuberant-ctags output format"
+	echo "# 'a' has a tab char in fprop fields. So ctags drops it silent."
+	$CTAGS --options=NONE --options=./foo.ctags "$1" --output-format=e-ctags  -o - input.foo
+}
+
+run "--sort=yes"
+run "--sort=no"
