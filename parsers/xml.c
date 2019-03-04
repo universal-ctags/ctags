b06676716e9d37e14562b10acae24f056a75abbc
Masatake YAMATO <yamato@redhat.com>
2019-03-04 19:30:49 +0900

    XML: capture namespace prefix
    Known issue: Line numbers for nsprefix are incorrect.
    
    Signed-off-by: Masatake YAMATO <yamato@redhat.com>

--- parsers/xml.c
+++ parsers/xml.c
@@ -17,20 +17,52 @@
 #include "routines.h"
 #include "selectors.h"
 
+#ifndef HAVE_LIBXML
+typedef void *xmlNsPtr;
+#endif
+
 typedef enum {
 	K_ID,
+	K_NSPREFIX,
 } xmlKind;
 
 static kindDefinition XmlKinds [] = {
 	{ true, 'i', "id", "id attributes" },
+	{ true, 'n', "nsprefix", "namespace prefixes" },
 };
 
 enum xmlXpathTables {
 	TABLE_MAIN,
+	TABLE_ID,
 };
 
+typedef enum {
+	F_NS_URI,
+} xmlField;
+
+static fieldDefinition XmlFields [] = {
+	{
+		.name = "uri",
+		.description = "uri associated with name prefix",
+		.enabled = true,
+	}
+};
+
+static void findNsPrefix (xmlNode *node,
+					   const char *xpath,
+					   const struct sTagXpathRecurSpec *spec,
+					   xmlXPathContext *ctx,
+					   void *userData);
+
 static tagXpathTable XmlXpathMainTable [] = {
-	{ "///@id",
+	{ "//*",
+	  LXPATH_TABLE_DO_RECUR,
+	  { .recurSpec = { .enter = findNsPrefix, .nextTable = TABLE_ID } }
+	},
+};
+
+static tagXpathTable XmlXpathIdTable [] = {
+	{ "./@id",
 	  LXPATH_TABLE_DO_MAKE,
 	  { .makeTagSpec = { K_ID, ROLE_INDEX_DEFINITION } }
 	},
@@ -38,7 +70,57 @@ static tagXpathTable XmlXpathMainTable [] = {
 
 static tagXpathTableTable xmlXpathTableTable[] = {
 	[TABLE_MAIN] = { ARRAY_AND_SIZE (XmlXpathMainTable) },
+	[TABLE_ID]   = { ARRAY_AND_SIZE (XmlXpathIdTable) },
 };
+
+static int makeNsPrefixTag (const char *name, xmlNode *node, xmlNsPtr ns)
+{
+	int n = CORK_NIL;
+
+#ifdef HAVE_LIBXML
+	tagEntryInfo tag;
+
+	initTagEntry (&tag, name, K_NSPREFIX);
+	/* TODO
+	 * - move this code block to lxpath.c.
+	 * - adjust the line number for nsprefixes forward. */
+	tag.lineNumber = XML_GET_LINE (node);
+	tag.filePosition = getInputFilePositionForLine (tag.lineNumber);
+	char *p = (char *)xmlGetNodePath (node);
+	if (ns->href && *ns->href)
+		attachParserField (&tag, XmlFields [F_NS_URI].ftype, (char *)ns->href);
+
+	n = makeTagEntry (&tag);
+	if (p)
+		xmlFree (p);
+#endif
+
+	return n;
+}
+
+static void findNsPrefix (xmlNode *node,
+					   const char *xpath,
+					   const struct sTagXpathRecurSpec *spec,
+					   xmlXPathContext *ctx,
+					   void *userData)
+{
+#ifdef HAVE_LIBXML
+	if (node->ns && node->ns->prefix == NULL)
+	{
+		vString *prefix = anonGenerateNew ("ns", K_NSPREFIX);
+		makeNsPrefixTag (vStringValue (prefix), node, node->ns);
+		vStringDelete (prefix);
+	}
+
+	for (xmlNsPtr ns = node->nsDef; ns; ns = ns->next)
+	{
+		if (ns->prefix)
+			makeNsPrefixTag ((char *)ns->prefix, node, ns);
+	}
+
+	findXMLTags (ctx, node, spec->nextTable, userData);
+#endif
+}
 
 static void
 findXmlTags (void)
@@ -60,5 +142,8 @@ XmlParser (void)
 	def->tagXpathTableTable = xmlXpathTableTable;
 	def->tagXpathTableCount = ARRAY_SIZE (xmlXpathTableTable);
 	def->selectLanguage = selectors;
+	def->fieldTable = XmlFields;
+	def->fieldCount = ARRAY_SIZE (XmlFields);
+
 	return def;
 }
