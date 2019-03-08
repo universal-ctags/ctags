f8534cf29bd4146f69cc31917b22099d4ba53f36
K.Takata <kentkt@csc.jp>
2019-03-08 19:51:18 +0900

    Add a script for updating packcc

---
+++ misc/pull-packcc.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# Update to the latest packcc
+#
+# Copyright (C) 2019 Universal Ctags Team
+# License: GPL 2 or later
+
+cd $(git rev-parse --show-toplevel)
+git subtree pull --prefix misc/packcc https://github.com/universal-ctags/packcc.git master --squash
