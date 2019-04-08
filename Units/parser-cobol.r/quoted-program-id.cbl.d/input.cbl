259669afa7d78ec904542638bb66c862cff3db7b
Colomban Wendling <ban@herbesfolles.org>
2019-04-08 18:34:22 +0200

    cobol: Fix parsing literal program name

---
+++ Units/parser-cobol.r/quoted-program-id.cbl.d/input.cbl
@@ -0,0 +1,7 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. 'Test program name'.
+       PROCEDURE DIVISION.
+       MAIN.
+           DISPLAY 'This is a test program'.
+       STOP RUN.
+       END PROGRAM 'Test program name'.
