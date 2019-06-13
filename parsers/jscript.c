50c63a4e83a57ff2b580c72b5515ece36c0200b0
Masatake YAMATO <yamato@redhat.com>
2019-06-14 04:08:04 +0900

    JavaScript: delete an unused structure field
    Signed-off-by: Masatake YAMATO <yamato@redhat.com>

--- parsers/jscript.c
+++ parsers/jscript.c
@@ -137,7 +137,6 @@ typedef struct sTokenInfo {
 	unsigned long 	lineNumber;
 	MIOPos 			filePosition;
 	int				nestLevel;
-	bool			ignoreTag;
 	bool			dynamicProp;
 } tokenInfo;
 
@@ -247,7 +246,6 @@ static void clearPoolToken (void *data)
 	token->type			= TOKEN_UNDEFINED;
 	token->keyword		= KEYWORD_NONE;
 	token->nestLevel	= 0;
-	token->ignoreTag	= false;
 	token->dynamicProp  = false;
 	token->lineNumber   = getInputLineNumber ();
 	token->filePosition = getInputFilePosition ();
@@ -294,7 +292,7 @@ static void makeJsTagCommon (const tokenInfo *const token, const jsKind kind,
 							 vString *const signature, vString *const inheritance,
 							 bool anonymous)
 {
-	if (JsKinds [kind].enabled && ! token->ignoreTag )
+	if (JsKinds [kind].enabled )
 	{
 		const char *name = vStringValue (token->string);
 		vString *fullscope = vStringNewCopy (token->scope);
@@ -369,11 +367,10 @@ static void makeJsTag (const tokenInfo *const token, const jsKind kind,
 static void makeClassTagCommon (tokenInfo *const token, vString *const signature,
                           vString *const inheritance, bool anonymous)
 {
-	vString *	fulltag;
 
-	if ( ! token->ignoreTag )
+
 	{
-		fulltag = vStringNew ();
+		vString *	fulltag = vStringNew ();
 		if (vStringLength (token->scope) > 0)
 		{
 			vStringCopy(fulltag, token->scope);
@@ -403,11 +400,8 @@ static void makeClassTag (tokenInfo *const token, vString *const signature,
 static void makeFunctionTagCommon (tokenInfo *const token, vString *const signature, bool generator,
 								   bool anonymous)
 {
-	vString *	fulltag;
-
-	if ( ! token->ignoreTag )
 	{
-		fulltag = vStringNew ();
+		vString *	fulltag = vStringNew ();
 		if (vStringLength (token->scope) > 0)
 		{
 			vStringCopy(fulltag, token->scope);
@@ -2049,7 +2043,6 @@ nextVar:
 							 * Find to the end of the statement
 							 */
 							findCmdTerm (token, false, false);
-							token->ignoreTag = false;
 							is_terminated = true;
 							goto cleanUp;
 						}
