67536c859276765b3404c077ca54adb99c8a72fd
Mirco Schoenfeld <mirco.schoenfeld@tum.de>
2019-07-12 16:55:44 +0200

    BibTeX: add recognition of @string entries
    (This log message is edited by @masatake.)

--- parsers/bibtex.c
+++ parsers/bibtex.c
@@ -55,6 +55,7 @@ enum eKeywordId {
 	KEYWORD_misc,
 	KEYWORD_phdthesis,
 	KEYWORD_proceedings,
+	KEYWORD_string,
 	KEYWORD_techreport,
 	KEYWORD_unpublished
 };
@@ -97,6 +98,7 @@ typedef enum {
 	BIBTAG_MISC,
 	BIBTAG_PHDTHESIS,
 	BIBTAG_PROCEEDINGS,
+	BIBTAG_STRING,
 	BIBTAG_TECHREPORT,
 	BIBTAG_UNPUBLISHED,
 	BIBTAG_COUNT
@@ -115,6 +117,7 @@ static kindDefinition BibKinds [] = {
 	{ true,  'n', "misc",						"misc"					},
 	{ true,  'p', "phdthesis",			"phdthesis"			},
 	{ true,  'P', "proceedings",		"proceedings"		},
+	{ true,  's', "string",					"string"				},
 	{ true,  't', "techreport",			"techreport"		},
 	{ true,  'u', "unpublished",		"unpublished"		}
 };
@@ -133,6 +136,7 @@ static const keywordTable BibKeywordTable [] = {
 	{ "misc",	        KEYWORD_misc				  },
 	{ "phdthesis",	  KEYWORD_phdthesis			},
 	{ "proceedings",	KEYWORD_proceedings		},
+	{ "string",				KEYWORD_string				},
 	{ "techreport",	  KEYWORD_techreport		},
 	{ "unpublished",	KEYWORD_unpublished		}
 };
@@ -375,6 +379,9 @@ static void parseBibFile (tokenInfo *const token)
 					break;
 				case KEYWORD_proceedings:
 					eof = parseTag (token, BIBTAG_PROCEEDINGS);
+					break;
+				case KEYWORD_string:
+					eof = parseTag (token, BIBTAG_STRING);
 					break;
 				case KEYWORD_techreport:
 					eof = parseTag (token, BIBTAG_TECHREPORT);
