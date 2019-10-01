4cce3135b026467dc7be3c2658b0dc5ad4d364b1
Masatake YAMATO <yamato@redhat.com>
2019-10-02 08:46:38 +0900

    main: update the help message for --use-slash-as-filename-separator
    Signed-off-by: Masatake YAMATO <yamato@redhat.com>

--- main/options.c
+++ main/options.c
@@ -421,7 +421,7 @@ static optionDescription LongOptionDescription [] = {
  {1,"       Print statistics about input and tag files [no]."},
 #ifdef WIN32
  {1,"  --use-slash-as-filename-separator"},
- {1,"       Use slash as filename separator [no]."},
+ {1,"       Use slash as filename separator [yes] for u-ctags output format."},
 #endif
  {1,"  --verbose=[yes|no]"},
  {1,"       Enable verbose messages describing actions on each input file."},
