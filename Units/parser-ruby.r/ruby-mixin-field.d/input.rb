205052c4584eec60c22ca9ff5ea6063c8fd2bd48
Masatake YAMATO <yamato@redhat.com>
2020-03-31 16:12:24 +0900

    Units,Ruby: add a case for mixin field

---
+++ Units/parser-ruby.r/ruby-mixin-field.d/input.rb
@@ -0,0 +1,21 @@
+# Taken from a comment in #2476 submitted by @AmaiKinono
+
+module X
+  def hi
+    p "Calling 'hi' in X."
+  end
+end
+
+module Y
+  def hoi
+    p "Calling 'hoi' in Y."
+  end
+end
+
+class A
+  include X
+  def hi
+    p "Calling 'hi' in A."
+  end
+  include Y
+end
