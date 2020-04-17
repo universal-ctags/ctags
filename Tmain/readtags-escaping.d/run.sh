6d65c066195a49b94bfb93d7839adcef36734dcb
Masatake YAMATO <yamato@redhat.com>
2020-04-17 14:09:53 +0900

    readtags: rename --escape option to --escape-output
    Signed-off-by: Masatake YAMATO <yamato@redhat.com>

--- Tmain/readtags-escaping.d/run.sh
+++ Tmain/readtags-escaping.d/run.sh
@@ -20,8 +20,8 @@ ${V} ${READTAGS} -en -t ./output.tags -l &&
 echo '# -enE -l' &&
 ${V} ${READTAGS} -enE -t ./output.tags -l &&
 
-echo '# -en --escape -l' &&
-${V} ${READTAGS} -en --escape -t ./output.tags -l &&
+echo '# -en --escape-output -l' &&
+${V} ${READTAGS} -en --escape-output -t ./output.tags -l &&
 
 echo '# -en -D' &&
 ${V} ${READTAGS} -en -t ./output.tags -D &&
@@ -29,12 +29,12 @@ ${V} ${READTAGS} -en -t ./output.tags -D &&
 echo '# -en -E -D' &&
 ${V} ${READTAGS} -en -E -t ./output.tags -D &&
 
-echo '# -en --escape -D' &&
-${V} ${READTAGS} -en --escape -t ./output.tags -D &&
+echo '# -en --escape-output -D' &&
+${V} ${READTAGS} -en --escape-output -t ./output.tags -D &&
 
 echo '# -en -l (output2)' &&
 ${V} ${READTAGS} -en -t ./output2.tags -l &&
 
-echo '# -en --escape -l (output2)' &&
-${V} ${READTAGS} -en --escape -t ./output2.tags -l &&
+echo '# -en --escape-output -l (output2)' &&
+${V} ${READTAGS} -en --escape-output -t ./output2.tags -l &&
 :
