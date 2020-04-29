universal-ctags/ctags

commit dc4e754e0a00e90a4f57bc959a8345e629c110d1

CXX: Emit types of template parameters

parsers/cxx/cxx_parser_template.c
@@ -40,10 +40,9 @@ typedef enum _CXXParserParseTemplateAngleBracketsResult
 } CXXParserParseTemplateAngleBracketsResult;
 
 
-static bool cxxTemplateTokenCheckIsNonTypeAndCompareWord(const void *pToken,void * szWord)
+static bool cxxTemplateTokenCheckIsNonTypeAndCompareWord(const CXXToken *t,void * szWord)
 {
 	// To be non type the token must NOT be preceeded by class/struct/union
-	CXXToken * t = (CXXToken *)pToken;
 	if(!t->pPrev)
 		return false;
 	if(cxxTokenTypeIs(t->pPrev,CXXTokenTypeKeyword))
@@ -64,17 +63,23 @@ static bool cxxTokenIsPresentInTemplateParametersAsNonType(CXXToken * t)
 			"Token must be identifier"
 		);
 
-	return ptrArrayHasTest(
-			g_cxx.pTemplateParameters,
-			cxxTemplateTokenCheckIsNonTypeAndCompareWord,
-			vStringValue(t->pszWord)
-		);
+	for(unsigned int u=0;u<g_cxx.oTemplateParameters.uCount;u++)
+	{
+		if(
+			cxxTemplateTokenCheckIsNonTypeAndCompareWord(
+					t,
+					vStringValue(g_cxx.oTemplateParameters.aIdentifiers[u]->pszWord)
+				)
+			)
+			return true;
+	}
+
+	return false;
 }
 
-static bool cxxTemplateTokenCheckIsTypeAndCompareWord(const void *pToken,void * szWord)
+static bool cxxTemplateTokenCheckIsTypeAndCompareWord(const CXXToken * t,void * szWord)
 {
 	// To be non type the token must be preceeded by class/struct/union
-	CXXToken * t = (CXXToken *)pToken;
 	if(!t->pPrev)
 		return false;
 	if(!cxxTokenTypeIs(t->pPrev,CXXTokenTypeKeyword))
@@ -92,11 +97,18 @@ static bool cxxTokenIsPresentInTemplateParametersAsType(CXXToken * t)
 			"Token must be identifier"
 		);
 
-	return ptrArrayHasTest(
-			g_cxx.pTemplateParameters,
-			cxxTemplateTokenCheckIsTypeAndCompareWord,
-			vStringValue(t->pszWord)
-		);
+	for(unsigned int u=0;u<g_cxx.oTemplateParameters.uCount;u++)
+	{
+		if(
+			cxxTemplateTokenCheckIsTypeAndCompareWord(
+					t,
+					vStringValue(g_cxx.oTemplateParameters.aIdentifiers[u]->pszWord)
+				)
+			)
+			return true;
+	}
+
+	return false;
 }
 
 // Attempt to capture a template parameter that is between the
@@ -111,6 +123,12 @@ static void cxxParserParseTemplateAngleBracketsCaptureTypeParameter(
 	)
 {
 	CXX_DEBUG_ENTER();
+
+	if(g_cxx.oTemplateParameters.uCount >= CXX_TYPED_VARIABLE_SET_ITEM_COUNT)
+	{
+		CXX_DEBUG_LEAVE_TEXT("No space for more parameters");
+		return;
+	}
 
 	CXX_DEBUG_ASSERT(
 			pParameterStart &&
@@ -212,7 +230,13 @@ static void cxxParserParseTemplateAngleBracketsCaptureTypeParameter(
 			vStringValue(t->pPrev->pszWord)
 		);
 
-	ptrArrayAdd(g_cxx.pTemplateParameters,t->pPrev);
+	unsigned int c = g_cxx.oTemplateParameters.uCount;
+
+	g_cxx.oTemplateParameters.aIdentifiers[c] = t->pPrev;
+	g_cxx.oTemplateParameters.aTypeStarts[c] = pParameterStart;
+	g_cxx.oTemplateParameters.aTypeEnds[c] = t->pPrev->pPrev;
+
+	g_cxx.oTemplateParameters.uCount++;
 
 	CXX_DEBUG_LEAVE();
 }
@@ -715,10 +739,7 @@ bool cxxParserParseTemplateAngleBracketsToTemplateChain(void)
 {
 	CXX_DEBUG_ENTER();
 
-	if(g_cxx.pTemplateParameters)
-		ptrArrayClear(g_cxx.pTemplateParameters);
-	else
-		g_cxx.pTemplateParameters = ptrArrayNew(NULL);
+	g_cxx.oTemplateParameters.uCount = 0;
 
 	CXXTokenChain * pOut = cxxParserParseTemplateAngleBracketsToSeparateChain(true);
 
@@ -732,6 +753,8 @@ bool cxxParserParseTemplateAngleBracketsToTemplateChain(void)
 		cxxTokenChainDestroy(g_cxx.pTemplateTokenChain);
 
 	g_cxx.pTemplateTokenChain = pOut;
+
+	g_cxx.oTemplateParameters.pChain = pOut;
 
 	// make sure we have no stale specializations
 	// (note that specializations always come AFTER the main template)
@@ -790,26 +813,31 @@ void cxxParserEmitTemplateParameterTags(void)
 			g_cxx.pTemplateTokenChain &&
 			(g_cxx.pTemplateTokenChain->iCount > 0) &&
 			cxxParserCurrentLanguageIsCPP() &&
-			g_cxx.pTemplateParameters && // this should be ensured by chain existence
 			cxxTagKindEnabled(CXXTagCPPKindTEMPLATEPARAM),
 			"Template existence must be checked before calling this function"
 		);
 
-	int c = ptrArrayCount(g_cxx.pTemplateParameters);
+	unsigned int c = g_cxx.oTemplateParameters.uCount;
 
-	for(int i=0;i<c;i++)
+	for(unsigned int i=0;i<c;i++)
 	{
-		CXXToken * t = (CXXToken *)ptrArrayItem(g_cxx.pTemplateParameters,i);
-
-		CXX_DEBUG_PRINT("Emitting template param tag for '%s'",vStringValue(t->pszWord));
+		CXX_DEBUG_PRINT(
+				"Emitting template param tag for '%s'",
+				vStringValue(g_cxx.oTemplateParameters.aIdentifiers[i]->pszWord)
+			);
 
 		tagEntryInfo * tag = cxxTagBegin(
 				CXXTagCPPKindTEMPLATEPARAM,
-				t
+				g_cxx.oTemplateParameters.aIdentifiers[i]
 			);
 
 		if(!tag)
 			continue;
+
+		cxxTagCheckAndSetTypeField(
+				g_cxx.oTemplateParameters.aTypeStarts[i],
+				g_cxx.oTemplateParameters.aTypeEnds[i]
+			);
 
 		cxxTagCommit();
 	}

Units/parser-cxx.r/bug-github-2263.cpp.d/expected.tags
@@ -1,42 +1,42 @@
 C0	input.cc	/^template <class T> class C0 {$/;"	c	file:
-T	input.cc	/^template <class T> class C0 {$/;"	Z	class:C0
+T	input.cc	/^template <class T> class C0 {$/;"	Z	class:C0	typeref:meta:class
 t	input.cc	/^  T t;$/;"	m	class:C0	typeref:typename:T	file:
 C1	input.cc	/^template <class T, typename T0 = C0<const T>> class C1 {$/;"	c	file:
-T	input.cc	/^template <class T, typename T0 = C0<const T>> class C1 {$/;"	Z	class:C1
-T0	input.cc	/^template <class T, typename T0 = C0<const T>> class C1 {$/;"	Z	class:C1
+T	input.cc	/^template <class T, typename T0 = C0<const T>> class C1 {$/;"	Z	class:C1	typeref:meta:class
+T0	input.cc	/^template <class T, typename T0 = C0<const T>> class C1 {$/;"	Z	class:C1	typeref:meta:typename
 t0	input.cc	/^  T0 t0;$/;"	m	class:C1	typeref:typename:T0	file:
 c1rehash	input.cc	/^  void c1rehash() {}$/;"	f	class:C1	typeref:typename:void	file:
 C2	input.cc	/^template <class T, typename T1 = C0<C0<const T>>> class C2 {$/;"	c	file:
-T	input.cc	/^template <class T, typename T1 = C0<C0<const T>>> class C2 {$/;"	Z	class:C2
-T1	input.cc	/^template <class T, typename T1 = C0<C0<const T>>> class C2 {$/;"	Z	class:C2
+T	input.cc	/^template <class T, typename T1 = C0<C0<const T>>> class C2 {$/;"	Z	class:C2	typeref:meta:class
+T1	input.cc	/^template <class T, typename T1 = C0<C0<const T>>> class C2 {$/;"	Z	class:C2	typeref:meta:typename
 t1	input.cc	/^  T1 t1;$/;"	m	class:C2	typeref:typename:T1	file:
 c2rehash	input.cc	/^  void c2rehash() {}$/;"	f	class:C2	typeref:typename:void	file:
 C3	input.cc	/^template <class T, typename T2 = C0<C0<C0<const T>>>> class C3 {$/;"	c	file:
-T	input.cc	/^template <class T, typename T2 = C0<C0<C0<const T>>>> class C3 {$/;"	Z	class:C3
-T2	input.cc	/^template <class T, typename T2 = C0<C0<C0<const T>>>> class C3 {$/;"	Z	class:C3
+T	input.cc	/^template <class T, typename T2 = C0<C0<C0<const T>>>> class C3 {$/;"	Z	class:C3	typeref:meta:class
+T2	input.cc	/^template <class T, typename T2 = C0<C0<C0<const T>>>> class C3 {$/;"	Z	class:C3	typeref:meta:typename
 t2	input.cc	/^  T2 t2;$/;"	m	class:C3	typeref:typename:T2	file:
 c3rehash	input.cc	/^  void c3rehash() {}$/;"	f	class:C3	typeref:typename:void	file:
 C4	input.cc	/^template <class T, typename T3 = C0<C0<C0<C0<const T>>>>> class C4 {$/;"	c	file:
-T	input.cc	/^template <class T, typename T3 = C0<C0<C0<C0<const T>>>>> class C4 {$/;"	Z	class:C4
-T3	input.cc	/^template <class T, typename T3 = C0<C0<C0<C0<const T>>>>> class C4 {$/;"	Z	class:C4
+T	input.cc	/^template <class T, typename T3 = C0<C0<C0<C0<const T>>>>> class C4 {$/;"	Z	class:C4	typeref:meta:class
+T3	input.cc	/^template <class T, typename T3 = C0<C0<C0<C0<const T>>>>> class C4 {$/;"	Z	class:C4	typeref:meta:typename
 t3	input.cc	/^  T3 t3;$/;"	m	class:C4	typeref:typename:T3	file:
 c4rehash	input.cc	/^  void c4rehash() {}$/;"	f	class:C4	typeref:typename:void	file:
 C5	input.cc	/^template <class T, typename T4 = C0<C0<C0<C0<C0<const T>>>>>> class C5 {$/;"	c	file:
-T	input.cc	/^template <class T, typename T4 = C0<C0<C0<C0<C0<const T>>>>>> class C5 {$/;"	Z	class:C5
-T4	input.cc	/^template <class T, typename T4 = C0<C0<C0<C0<C0<const T>>>>>> class C5 {$/;"	Z	class:C5
+T	input.cc	/^template <class T, typename T4 = C0<C0<C0<C0<C0<const T>>>>>> class C5 {$/;"	Z	class:C5	typeref:meta:class
+T4	input.cc	/^template <class T, typename T4 = C0<C0<C0<C0<C0<const T>>>>>> class C5 {$/;"	Z	class:C5	typeref:meta:typename
 t4	input.cc	/^  T4 t4;$/;"	m	class:C5	typeref:typename:T4	file:
 c5rehash	input.cc	/^  void c5rehash() {}$/;"	f	class:C5	typeref:typename:void	file:
 C4x1	input.cc	/^template <class T, typename T3 = C0<C0<C0<C0<const T>>>>, class S = int> class C4x1 {$/;"	c	file:
-T	input.cc	/^template <class T, typename T3 = C0<C0<C0<C0<const T>>>>, class S = int> class C4x1 {$/;"	Z	class:C4x1
-T3	input.cc	/^template <class T, typename T3 = C0<C0<C0<C0<const T>>>>, class S = int> class C4x1 {$/;"	Z	class:C4x1
-S	input.cc	/^template <class T, typename T3 = C0<C0<C0<C0<const T>>>>, class S = int> class C4x1 {$/;"	Z	class:C4x1
+T	input.cc	/^template <class T, typename T3 = C0<C0<C0<C0<const T>>>>, class S = int> class C4x1 {$/;"	Z	class:C4x1	typeref:meta:class
+T3	input.cc	/^template <class T, typename T3 = C0<C0<C0<C0<const T>>>>, class S = int> class C4x1 {$/;"	Z	class:C4x1	typeref:meta:typename
+S	input.cc	/^template <class T, typename T3 = C0<C0<C0<C0<const T>>>>, class S = int> class C4x1 {$/;"	Z	class:C4x1	typeref:meta:class
 t3	input.cc	/^  T3 t3;$/;"	m	class:C4x1	typeref:typename:T3	file:
 s	input.cc	/^  S  s;$/;"	m	class:C4x1	typeref:typename:S	file:
 c4x1rehash	input.cc	/^  void c4x1rehash() {}$/;"	f	class:C4x1	typeref:typename:void	file:
 C4x2	input.cc	/^template <class T, typename T3 = C0<C0<C0<C0<const T>>>>, class S = C0<const T>> class C4x2 {$/;"	c	file:
-T	input.cc	/^template <class T, typename T3 = C0<C0<C0<C0<const T>>>>, class S = C0<const T>> class C4x2 {$/;"	Z	class:C4x2
-T3	input.cc	/^template <class T, typename T3 = C0<C0<C0<C0<const T>>>>, class S = C0<const T>> class C4x2 {$/;"	Z	class:C4x2
-S	input.cc	/^template <class T, typename T3 = C0<C0<C0<C0<const T>>>>, class S = C0<const T>> class C4x2 {$/;"	Z	class:C4x2
+T	input.cc	/^template <class T, typename T3 = C0<C0<C0<C0<const T>>>>, class S = C0<const T>> class C4x2 {$/;"	Z	class:C4x2	typeref:meta:class
+T3	input.cc	/^template <class T, typename T3 = C0<C0<C0<C0<const T>>>>, class S = C0<const T>> class C4x2 {$/;"	Z	class:C4x2	typeref:meta:typename
+S	input.cc	/^template <class T, typename T3 = C0<C0<C0<C0<const T>>>>, class S = C0<const T>> class C4x2 {$/;"	Z	class:C4x2	typeref:meta:class
 t3	input.cc	/^  T3 t3;$/;"	m	class:C4x2	typeref:typename:T3	file:
 s	input.cc	/^  S  s;$/;"	m	class:C4x2	typeref:typename:S	file:
 c4x2rehash	input.cc	/^  void c4x2rehash() {}$/;"	f	class:C4x2	typeref:typename:void	file:

Units/parser-cxx.r/bug1563476.cpp.d/expected.tags
@@ -1,4 +1,4 @@
 g	input.cpp	/^int g() {$/;"	f	typeref:typename:int
 IntroduceBitDef	input.cpp	/^struct IntroduceBitDef< Accessor, typename$/;"	s	file:
-Accessor	input.cpp	/^template< typename Accessor >$/;"	Z	struct:IntroduceBitDef
+Accessor	input.cpp	/^template< typename Accessor >$/;"	Z	struct:IntroduceBitDef	typeref:meta:typename
 f	input.cpp	/^ int f() { }$/;"	f	struct:IntroduceBitDef	typeref:typename:int	file:

Units/parser-cxx.r/template-nested-triangle-brackets.d/expected.tags
@@ -1,75 +1,75 @@
 Test	input.hpp	/^class Test : public set<int> {$/;"	c	template:<int,class _Comp0=less<int>,class _Comp1=less<pair<int,int>>>
-_Comp0	input.hpp	/^         class _Comp0=less<int>,$/;"	Z	class:Test
-_Comp1	input.hpp	/^         class _Comp1=less<pair<int, int> > >$/;"	Z	class:Test
+_Comp0	input.hpp	/^         class _Comp0=less<int>,$/;"	Z	class:Test	typeref:meta:class
+_Comp1	input.hpp	/^         class _Comp1=less<pair<int, int> > >$/;"	Z	class:Test	typeref:meta:class
 xxx	input.hpp	/^  typedef int xxx;$/;"	t	class:Test	typeref:typename:int
 Foo	input-0.hpp	/^class Foo {};$/;"	c	template:<typename X>
-X	input-0.hpp	/^template <typename X>$/;"	Z	class:Foo
+X	input-0.hpp	/^template <typename X>$/;"	Z	class:Foo	typeref:meta:typename
 bar	input-0.hpp	/^constexpr Foo<Y> bar {};$/;"	v	typeref:typename:Foo<Y>	template:<typename Y=Foo<int>>
-Y	input-0.hpp	/^template <typename Y = Foo<int>>$/;"	Z	variable:bar
+Y	input-0.hpp	/^template <typename Y = Foo<int>>$/;"	Z	variable:bar	typeref:meta:typename
 i	input-0.hpp	/^const int i = 3;$/;"	v	typeref:typename:const int
 failure	input-1.hpp	/^class failure {};$/;"	c	template:<class T,class Hash=std::hash<T>>
-T	input-1.hpp	/^template <class T, class Hash=std::hash<T>>$/;"	Z	class:failure
-Hash	input-1.hpp	/^template <class T, class Hash=std::hash<T>>$/;"	Z	class:failure
+T	input-1.hpp	/^template <class T, class Hash=std::hash<T>>$/;"	Z	class:failure	typeref:meta:class
+Hash	input-1.hpp	/^template <class T, class Hash=std::hash<T>>$/;"	Z	class:failure	typeref:meta:class
 V	input-2.hpp	/^const int V = 10;$/;"	v	typeref:typename:const int
 funcA	input-2.hpp	/^template<typename T,int I = V << 1 > void funcA(T t)$/;"	f	typeref:typename:void	template:<typename T,int I=V<<1>
-T	input-2.hpp	/^template<typename T,int I = V << 1 > void funcA(T t)$/;"	Z	function:funcA
-I	input-2.hpp	/^template<typename T,int I = V << 1 > void funcA(T t)$/;"	Z	function:funcA
+T	input-2.hpp	/^template<typename T,int I = V << 1 > void funcA(T t)$/;"	Z	function:funcA	typeref:meta:typename
+I	input-2.hpp	/^template<typename T,int I = V << 1 > void funcA(T t)$/;"	Z	function:funcA	typeref:typename:int
 K	input-2.hpp	/^typedef int K;$/;"	t	typeref:typename:int
 funcB	input-2.hpp	/^template<typename T,int I,int J = I < V> void funcB(T t)$/;"	f	typeref:typename:void	template:<typename T,int I,int J=I<V>
-T	input-2.hpp	/^template<typename T,int I,int J = I < V> void funcB(T t)$/;"	Z	function:funcB
-I	input-2.hpp	/^template<typename T,int I,int J = I < V> void funcB(T t)$/;"	Z	function:funcB
-J	input-2.hpp	/^template<typename T,int I,int J = I < V> void funcB(T t)$/;"	Z	function:funcB
+T	input-2.hpp	/^template<typename T,int I,int J = I < V> void funcB(T t)$/;"	Z	function:funcB	typeref:meta:typename
+I	input-2.hpp	/^template<typename T,int I,int J = I < V> void funcB(T t)$/;"	Z	function:funcB	typeref:typename:int
+J	input-2.hpp	/^template<typename T,int I,int J = I < V> void funcB(T t)$/;"	Z	function:funcB	typeref:typename:int
 funcC	input-2.hpp	/^template<typename T,int I,int J = I < V> K funcC(T t)$/;"	f	typeref:typename:K	template:<typename T,int I,int J=I<V>
-T	input-2.hpp	/^template<typename T,int I,int J = I < V> K funcC(T t)$/;"	Z	function:funcC
-I	input-2.hpp	/^template<typename T,int I,int J = I < V> K funcC(T t)$/;"	Z	function:funcC
-J	input-2.hpp	/^template<typename T,int I,int J = I < V> K funcC(T t)$/;"	Z	function:funcC
+T	input-2.hpp	/^template<typename T,int I,int J = I < V> K funcC(T t)$/;"	Z	function:funcC	typeref:meta:typename
+I	input-2.hpp	/^template<typename T,int I,int J = I < V> K funcC(T t)$/;"	Z	function:funcC	typeref:typename:int
+J	input-2.hpp	/^template<typename T,int I,int J = I < V> K funcC(T t)$/;"	Z	function:funcC	typeref:typename:int
 funcD	input-2.hpp	/^template<typename T,int I,int J = I >> 2> K funcD(T t)$/;"	f	typeref:typename:K	template:<typename T,int I,int J=I>>2>
-T	input-2.hpp	/^template<typename T,int I,int J = I >> 2> K funcD(T t)$/;"	Z	function:funcD
-I	input-2.hpp	/^template<typename T,int I,int J = I >> 2> K funcD(T t)$/;"	Z	function:funcD
-J	input-2.hpp	/^template<typename T,int I,int J = I >> 2> K funcD(T t)$/;"	Z	function:funcD
+T	input-2.hpp	/^template<typename T,int I,int J = I >> 2> K funcD(T t)$/;"	Z	function:funcD	typeref:meta:typename
+I	input-2.hpp	/^template<typename T,int I,int J = I >> 2> K funcD(T t)$/;"	Z	function:funcD	typeref:typename:int
+J	input-2.hpp	/^template<typename T,int I,int J = I >> 2> K funcD(T t)$/;"	Z	function:funcD	typeref:typename:int
 funcE	input-2.hpp	/^template<typename T,int I,int J = 1 >> I> K funcE(T t)$/;"	f	typeref:typename:K	template:<typename T,int I,int J=1>> I>
-T	input-2.hpp	/^template<typename T,int I,int J = 1 >> I> K funcE(T t)$/;"	Z	function:funcE
-I	input-2.hpp	/^template<typename T,int I,int J = 1 >> I> K funcE(T t)$/;"	Z	function:funcE
-J	input-2.hpp	/^template<typename T,int I,int J = 1 >> I> K funcE(T t)$/;"	Z	function:funcE
+T	input-2.hpp	/^template<typename T,int I,int J = 1 >> I> K funcE(T t)$/;"	Z	function:funcE	typeref:meta:typename
+I	input-2.hpp	/^template<typename T,int I,int J = 1 >> I> K funcE(T t)$/;"	Z	function:funcE	typeref:typename:int
+J	input-2.hpp	/^template<typename T,int I,int J = 1 >> I> K funcE(T t)$/;"	Z	function:funcE	typeref:typename:int
 funcF	input-2.hpp	/^template<typename T,int I,int L,int J = I >> L> K funcF(T t)$/;"	f	typeref:typename:K	template:<typename T,int I,int L,int J=I>> L>
-T	input-2.hpp	/^template<typename T,int I,int L,int J = I >> L> K funcF(T t)$/;"	Z	function:funcF
-I	input-2.hpp	/^template<typename T,int I,int L,int J = I >> L> K funcF(T t)$/;"	Z	function:funcF
-L	input-2.hpp	/^template<typename T,int I,int L,int J = I >> L> K funcF(T t)$/;"	Z	function:funcF
-J	input-2.hpp	/^template<typename T,int I,int L,int J = I >> L> K funcF(T t)$/;"	Z	function:funcF
+T	input-2.hpp	/^template<typename T,int I,int L,int J = I >> L> K funcF(T t)$/;"	Z	function:funcF	typeref:meta:typename
+I	input-2.hpp	/^template<typename T,int I,int L,int J = I >> L> K funcF(T t)$/;"	Z	function:funcF	typeref:typename:int
+L	input-2.hpp	/^template<typename T,int I,int L,int J = I >> L> K funcF(T t)$/;"	Z	function:funcF	typeref:typename:int
+J	input-2.hpp	/^template<typename T,int I,int L,int J = I >> L> K funcF(T t)$/;"	Z	function:funcF	typeref:typename:int
 funcG	input-2.hpp	/^template<typename T,int I,int J = 1 >> V> K funcG(T t)$/;"	f	typeref:typename:K	template:<typename T,int I,int J=1>> V>
-T	input-2.hpp	/^template<typename T,int I,int J = 1 >> V> K funcG(T t)$/;"	Z	function:funcG
-I	input-2.hpp	/^template<typename T,int I,int J = 1 >> V> K funcG(T t)$/;"	Z	function:funcG
-J	input-2.hpp	/^template<typename T,int I,int J = 1 >> V> K funcG(T t)$/;"	Z	function:funcG
+T	input-2.hpp	/^template<typename T,int I,int J = 1 >> V> K funcG(T t)$/;"	Z	function:funcG	typeref:meta:typename
+I	input-2.hpp	/^template<typename T,int I,int J = 1 >> V> K funcG(T t)$/;"	Z	function:funcG	typeref:typename:int
+J	input-2.hpp	/^template<typename T,int I,int J = 1 >> V> K funcG(T t)$/;"	Z	function:funcG	typeref:typename:int
 funcH	input-2.hpp	/^template<typename T,int I,int J = I >> V> K funcH(T t)$/;"	f	typeref:typename:K	template:<typename T,int I,int J=I>> V>
-T	input-2.hpp	/^template<typename T,int I,int J = I >> V> K funcH(T t)$/;"	Z	function:funcH
-I	input-2.hpp	/^template<typename T,int I,int J = I >> V> K funcH(T t)$/;"	Z	function:funcH
-J	input-2.hpp	/^template<typename T,int I,int J = I >> V> K funcH(T t)$/;"	Z	function:funcH
+T	input-2.hpp	/^template<typename T,int I,int J = I >> V> K funcH(T t)$/;"	Z	function:funcH	typeref:meta:typename
+I	input-2.hpp	/^template<typename T,int I,int J = I >> V> K funcH(T t)$/;"	Z	function:funcH	typeref:typename:int
+J	input-2.hpp	/^template<typename T,int I,int J = I >> V> K funcH(T t)$/;"	Z	function:funcH	typeref:typename:int
 funcI	input-2.hpp	/^template<typename T,int I,int J = 1 >> (1+2)> K funcI(T t)$/;"	f	typeref:typename:K	template:<typename T,int I,int J=1>> (1+2)>
-T	input-2.hpp	/^template<typename T,int I,int J = 1 >> (1+2)> K funcI(T t)$/;"	Z	function:funcI
-I	input-2.hpp	/^template<typename T,int I,int J = 1 >> (1+2)> K funcI(T t)$/;"	Z	function:funcI
-J	input-2.hpp	/^template<typename T,int I,int J = 1 >> (1+2)> K funcI(T t)$/;"	Z	function:funcI
+T	input-2.hpp	/^template<typename T,int I,int J = 1 >> (1+2)> K funcI(T t)$/;"	Z	function:funcI	typeref:meta:typename
+I	input-2.hpp	/^template<typename T,int I,int J = 1 >> (1+2)> K funcI(T t)$/;"	Z	function:funcI	typeref:typename:int
+J	input-2.hpp	/^template<typename T,int I,int J = 1 >> (1+2)> K funcI(T t)$/;"	Z	function:funcI	typeref:typename:int
 funcJ	input-2.hpp	/^template<typename T,int I,int J = I > 2> K funcJ(T t)$/;"	f	typeref:typename:K	template:<typename T,int I,int J=I>2>
-T	input-2.hpp	/^template<typename T,int I,int J = I > 2> K funcJ(T t)$/;"	Z	function:funcJ
-I	input-2.hpp	/^template<typename T,int I,int J = I > 2> K funcJ(T t)$/;"	Z	function:funcJ
-J	input-2.hpp	/^template<typename T,int I,int J = I > 2> K funcJ(T t)$/;"	Z	function:funcJ
+T	input-2.hpp	/^template<typename T,int I,int J = I > 2> K funcJ(T t)$/;"	Z	function:funcJ	typeref:meta:typename
+I	input-2.hpp	/^template<typename T,int I,int J = I > 2> K funcJ(T t)$/;"	Z	function:funcJ	typeref:typename:int
+J	input-2.hpp	/^template<typename T,int I,int J = I > 2> K funcJ(T t)$/;"	Z	function:funcJ	typeref:typename:int
 funcK	input-2.hpp	/^template<typename T,int I,int J = 1 > I> K funcK(T t)$/;"	f	typeref:typename:K	template:<typename T,int I,int J=1> I>
-T	input-2.hpp	/^template<typename T,int I,int J = 1 > I> K funcK(T t)$/;"	Z	function:funcK
-I	input-2.hpp	/^template<typename T,int I,int J = 1 > I> K funcK(T t)$/;"	Z	function:funcK
-J	input-2.hpp	/^template<typename T,int I,int J = 1 > I> K funcK(T t)$/;"	Z	function:funcK
+T	input-2.hpp	/^template<typename T,int I,int J = 1 > I> K funcK(T t)$/;"	Z	function:funcK	typeref:meta:typename
+I	input-2.hpp	/^template<typename T,int I,int J = 1 > I> K funcK(T t)$/;"	Z	function:funcK	typeref:typename:int
+J	input-2.hpp	/^template<typename T,int I,int J = 1 > I> K funcK(T t)$/;"	Z	function:funcK	typeref:typename:int
 funcL	input-2.hpp	/^template<typename T,int I,int L,int J = I > L> K funcL(T t)$/;"	f	typeref:typename:K	template:<typename T,int I,int L,int J=I> L>
-T	input-2.hpp	/^template<typename T,int I,int L,int J = I > L> K funcL(T t)$/;"	Z	function:funcL
-I	input-2.hpp	/^template<typename T,int I,int L,int J = I > L> K funcL(T t)$/;"	Z	function:funcL
-L	input-2.hpp	/^template<typename T,int I,int L,int J = I > L> K funcL(T t)$/;"	Z	function:funcL
-J	input-2.hpp	/^template<typename T,int I,int L,int J = I > L> K funcL(T t)$/;"	Z	function:funcL
+T	input-2.hpp	/^template<typename T,int I,int L,int J = I > L> K funcL(T t)$/;"	Z	function:funcL	typeref:meta:typename
+I	input-2.hpp	/^template<typename T,int I,int L,int J = I > L> K funcL(T t)$/;"	Z	function:funcL	typeref:typename:int
+L	input-2.hpp	/^template<typename T,int I,int L,int J = I > L> K funcL(T t)$/;"	Z	function:funcL	typeref:typename:int
+J	input-2.hpp	/^template<typename T,int I,int L,int J = I > L> K funcL(T t)$/;"	Z	function:funcL	typeref:typename:int
 funcO	input-2.hpp	/^template<typename T,int I,unsigned int J = 1 > (1+2)> K funcO(T t)$/;"	f	typeref:typename:K	template:<typename T,int I,unsigned int J=1> (1+2)>
-T	input-2.hpp	/^template<typename T,int I,unsigned int J = 1 > (1+2)> K funcO(T t)$/;"	Z	function:funcO
-I	input-2.hpp	/^template<typename T,int I,unsigned int J = 1 > (1+2)> K funcO(T t)$/;"	Z	function:funcO
-J	input-2.hpp	/^template<typename T,int I,unsigned int J = 1 > (1+2)> K funcO(T t)$/;"	Z	function:funcO
+T	input-2.hpp	/^template<typename T,int I,unsigned int J = 1 > (1+2)> K funcO(T t)$/;"	Z	function:funcO	typeref:meta:typename
+I	input-2.hpp	/^template<typename T,int I,unsigned int J = 1 > (1+2)> K funcO(T t)$/;"	Z	function:funcO	typeref:typename:int
+J	input-2.hpp	/^template<typename T,int I,unsigned int J = 1 > (1+2)> K funcO(T t)$/;"	Z	function:funcO	typeref:typename:unsigned int
 TestStruct	input-3.hpp	/^template <typename T> struct TestStruct {};$/;"	s	template:<typename T>
-T	input-3.hpp	/^template <typename T> struct TestStruct {};$/;"	Z	struct:TestStruct
+T	input-3.hpp	/^template <typename T> struct TestStruct {};$/;"	Z	struct:TestStruct	typeref:meta:typename
 TemplateParameterStruct	input-3.hpp	/^template <typename T> struct TemplateParameterStruct {};$/;"	s	template:<typename T>
-T	input-3.hpp	/^template <typename T> struct TemplateParameterStruct {};$/;"	Z	struct:TemplateParameterStruct
+T	input-3.hpp	/^template <typename T> struct TemplateParameterStruct {};$/;"	Z	struct:TemplateParameterStruct	typeref:meta:typename
 ParameterStruct	input-3.hpp	/^struct ParameterStruct {};$/;"	s
 TestStruct	input-3.hpp	/^template <> struct TestStruct<ParameterStruct> {};$/;"	s	template:<>	specialization:<ParameterStruct>
 TestStruct	input-3.hpp	/^template <typename P> struct TestStruct<TemplateParameterStruct<P>> {};$/;"	s	template:<typename P>	specialization:<TemplateParameterStruct<P>>
-P	input-3.hpp	/^template <typename P> struct TestStruct<TemplateParameterStruct<P>> {};$/;"	Z	struct:TestStruct
+P	input-3.hpp	/^template <typename P> struct TestStruct<TemplateParameterStruct<P>> {};$/;"	Z	struct:TestStruct	typeref:meta:typename

commit a3b1e1b0758d45c84dd10d1a8aab4139afcffdb3

Cxx: destroy the token returned from cxxTagCheckAndSetTypeField() for fixing memory leak
The token returned from cxxTagCheckAndSetTypeField() must be freed after
calling cxxTagCommit().

The memory leak was introduced in dc4e754e0a00e90a4f57bc959a8345e629c110d1.
Close #2525.

Signed-off-by: Masatake YAMATO <yamato@redhat.com>

parsers/cxx/cxx_parser_template.c
@@ -834,11 +834,13 @@ void cxxParserEmitTemplateParameterTags(void)
 		if(!tag)
 			continue;
 
-		cxxTagCheckAndSetTypeField(
+		CXXToken * pTypeToken = cxxTagCheckAndSetTypeField(
 				g_cxx.oTemplateParameters.aTypeStarts[i],
 				g_cxx.oTemplateParameters.aTypeEnds[i]
 			);
 
 		cxxTagCommit();
+		if (pTypeToken)
+			cxxTokenDestroy(pTypeToken);
 	}
 }