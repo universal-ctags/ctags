universal-ctags/ctags

commit fcc4853ec2f2b7a2992d8b64a77eb60d148b3c0d

CXX: Add template specification field. Improve angle bracked matching. Add some unit tests.

parsers/cxx/cxx_tag.c
@@ -120,7 +120,12 @@ static fieldDefinition g_aCXXCPPFields [] = {
 		.name = "name",
 		.description = "aliased names",
 		.enabled = true
-	}
+	},
+	{
+		.name = "specialization",
+		.description = "template specialization parameters",
+		.enabled = false,
+	},
 };
 
 static fieldDefinition g_aCXXCUDAFields [] = {
@@ -329,7 +334,7 @@ vString * cxxTagSetProperties(unsigned int uProperties)
 	if(uProperties & CXXTagPropertyFunctionTryBlock)
 		ADD_PROPERTY("fntryblock");
 
-	cxxTagSetField(CXXTagFieldProperties,vStringValue(pszProperties));
+	cxxTagSetField(CXXTagFieldProperties,vStringValue(pszProperties),false);
 
 	return pszProperties;
 }
@@ -501,7 +506,7 @@ CXXToken * cxxTagCheckAndSetTypeField(
 	return pTypeName;
 }
 
-void cxxTagSetField(unsigned int uField,const char * szValue)
+void cxxTagSetField(unsigned int uField,const char * szValue,bool bCopyValue)
 {
 	CXX_DEBUG_ASSERT(
 			uField < g_cxx.uFieldOptionCount,
@@ -511,7 +516,7 @@ void cxxTagSetField(unsigned int uField,const char * szValue)
 	if(!g_cxx.pFieldOptions[uField].enabled)
 		return;
 
-	attachParserField(&g_oCXXTag,false,g_cxx.pFieldOptions[uField].ftype,szValue);
+	attachParserField(&g_oCXXTag,bCopyValue,g_cxx.pFieldOptions[uField].ftype,szValue);
 }
 
 void cxxTagSetCorkQueueField(
@@ -530,29 +535,54 @@ void cxxTagSetCorkQueueField(
 	attachParserFieldToCorkEntry(iIndex,g_cxx.pFieldOptions[uField].ftype,szValue);
 }
 
-void cxxTagHandleTemplateField()
+void cxxTagHandleTemplateFields()
 {
 	CXX_DEBUG_ASSERT(
 		g_cxx.pTemplateTokenChain &&
 		(g_cxx.pTemplateTokenChain->iCount > 0) &&
-		cxxParserCurrentLanguageIsCPP() &&
-		cxxTagFieldEnabled(CXXTagCPPFieldTemplate),
+		cxxParserCurrentLanguageIsCPP(),
 		"Template existence must be checked before calling this function"
 	);
 
-	cxxTokenChainNormalizeTypeNameSpacing(g_cxx.pTemplateTokenChain);
-
-	CXXToken * t = cxxTokenChainCondenseIntoToken(g_cxx.pTemplateTokenChain,0);
-
-	if(t)
+	if(cxxTagFieldEnabled(CXXTagCPPFieldTemplate))
 	{
-		cxxTagSetField(
-				CXXTagCPPFieldTemplate,
-				vStringValue(t->pszWord)
-			);
+		cxxTokenChainNormalizeTypeNameSpacing(g_cxx.pTemplateTokenChain);
 
-		cxxTokenDestroy(t);
+		CXXToken * t = cxxTokenChainCondenseIntoToken(g_cxx.pTemplateTokenChain,0);
+
+		if(t)
+		{
+			cxxTagSetField(
+					CXXTagCPPFieldTemplate,
+					vStringValue(t->pszWord),
+					true
+				);
+
+			cxxTokenDestroy(t);
+		}
 	}
+
+	if(
+			g_cxx.pTemplateSpecializationTokenChain &&
+			cxxTagFieldEnabled(CXXTagCPPFieldTemplateSpecialization)
+		)
+	{
+		cxxTokenChainNormalizeTypeNameSpacing(g_cxx.pTemplateSpecializationTokenChain);
+
+		CXXToken * tx = cxxTokenChainCondenseIntoToken(g_cxx.pTemplateSpecializationTokenChain,0);
+
+		if(tx)
+		{
+			cxxTagSetField(
+					CXXTagCPPFieldTemplateSpecialization,
+					vStringValue(tx->pszWord),
+					true
+				);
+
+			cxxTokenDestroy(tx);
+		}
+	}
+
 }
 
 int cxxTagCommit(void)

Units/parser-cxx.r/template-nested-triangle-brackets.d/args.ctags
@@ -1,3 +1,3 @@
 --sort=no
 --kinds-c++=+Z
---fields-c++={template}
+--fields-c++={template}{specialization}

Units/parser-cxx.r/template-nested-triangle-brackets.d/expected.tags
@@ -11,14 +11,27 @@ failure	input-1.hpp	/^class failure {};$/;"	c	template:<class T,class Hash=std::
 T	input-1.hpp	/^template <class T, class Hash=std::hash<T>>$/;"	Z	class:failure
 Hash	input-1.hpp	/^template <class T, class Hash=std::hash<T>>$/;"	Z	class:failure
 V	input-2.hpp	/^const int V = 10;$/;"	v	typeref:typename:const int
-func	input-2.hpp	/^template<typename T,int I = V << 1 > void func(T t)$/;"	f	typeref:typename:void	template:<typename T,int I=V<<1>
-T	input-2.hpp	/^template<typename T,int I = V << 1 > void func(T t)$/;"	Z	function:func
-I	input-2.hpp	/^template<typename T,int I = V << 1 > void func(T t)$/;"	Z	function:func
+funcA	input-2.hpp	/^template<typename T,int I = V << 1 > void funcA(T t)$/;"	f	typeref:typename:void	template:<typename T,int I=V<<1>
+T	input-2.hpp	/^template<typename T,int I = V << 1 > void funcA(T t)$/;"	Z	function:funcA
+I	input-2.hpp	/^template<typename T,int I = V << 1 > void funcA(T t)$/;"	Z	function:funcA
+K	input-2.hpp	/^typedef int K;$/;"	t	typeref:typename:int
+funcB	input-2.hpp	/^template<typename T,int I,int J = I < V> void funcB(T t)$/;"	f	typeref:typename:void	template:<typename T,int I,int J=I<V>
+T	input-2.hpp	/^template<typename T,int I,int J = I < V> void funcB(T t)$/;"	Z	function:funcB
+I	input-2.hpp	/^template<typename T,int I,int J = I < V> void funcB(T t)$/;"	Z	function:funcB
+J	input-2.hpp	/^template<typename T,int I,int J = I < V> void funcB(T t)$/;"	Z	function:funcB
+funcC	input-2.hpp	/^template<typename T,int I,int J = I < V> K funcC(T t)$/;"	f	typeref:typename:K	template:<typename T,int I,int J=I<V>
+T	input-2.hpp	/^template<typename T,int I,int J = I < V> K funcC(T t)$/;"	Z	function:funcC
+I	input-2.hpp	/^template<typename T,int I,int J = I < V> K funcC(T t)$/;"	Z	function:funcC
+J	input-2.hpp	/^template<typename T,int I,int J = I < V> K funcC(T t)$/;"	Z	function:funcC
+funcD	input-2.hpp	/^template<typename T,int I,int J = I >> 2> K funcD(T t)$/;"	f	typeref:typename:>2> K	template:<typename T,int I,int J=I>
+T	input-2.hpp	/^template<typename T,int I,int J = I >> 2> K funcD(T t)$/;"	Z	function:funcD
+I	input-2.hpp	/^template<typename T,int I,int J = I >> 2> K funcD(T t)$/;"	Z	function:funcD
+J	input-2.hpp	/^template<typename T,int I,int J = I >> 2> K funcD(T t)$/;"	Z	function:funcD
 TestStruct	input-3.hpp	/^template <typename T> struct TestStruct {};$/;"	s	template:<typename T>
 T	input-3.hpp	/^template <typename T> struct TestStruct {};$/;"	Z	struct:TestStruct
 TemplateParameterStruct	input-3.hpp	/^template <typename T> struct TemplateParameterStruct {};$/;"	s	template:<typename T>
 T	input-3.hpp	/^template <typename T> struct TemplateParameterStruct {};$/;"	Z	struct:TemplateParameterStruct
 ParameterStruct	input-3.hpp	/^struct ParameterStruct {};$/;"	s
-TestStruct	input-3.hpp	/^template <> struct TestStruct<ParameterStruct> {};$/;"	s	template:<>
-TestStruct	input-3.hpp	/^template <typename P> struct TestStruct<TemplateParameterStruct<P>> {};$/;"	s	template:<typename P>
+TestStruct	input-3.hpp	/^template <> struct TestStruct<ParameterStruct> {};$/;"	s	template:<>	specialization:<ParameterStruct>
+TestStruct	input-3.hpp	/^template <typename P> struct TestStruct<TemplateParameterStruct<P>> {};$/;"	s	template:<typename P>	specialization:<TemplateParameterStruct<P>>
 P	input-3.hpp	/^template <typename P> struct TestStruct<TemplateParameterStruct<P>> {};$/;"	Z	struct:TestStruct

Units/parser-cxx.r/template-nested-triangle-brackets.d/input-2.hpp
@@ -1,5 +1,22 @@
 const int V = 10;
 
-template<typename T,int I = V << 1 > void func(T t)
+template<typename T,int I = V << 1 > void funcA(T t)
 {
+}
+
+typedef int K;
+
+template<typename T,int I,int J = I < V> void funcB(T t)
+{
+}
+
+template<typename T,int I,int J = I < V> K funcC(T t)
+{
+	return 0;
+}
+
+// This is allowed by C++03
+template<typename T,int I,int J = I >> 2> K funcD(T t)
+{
+	return 0;
 }

Units/parser-cxx.r/template-specializations.d/args.ctags
@@ -1,4 +1,4 @@
 --sort=no
 --kinds-C++=*
 --fields=+sS
---fields-c++=+{template}{properties}
+--fields-c++=+{template}{properties}{specialization}

Units/parser-cxx.r/template-specializations.d/expected.tags
@@ -12,10 +12,30 @@ g1	input.cpp	/^template<class X1> void A<T>::g1(T, X1) { }$/;"	f	class:A	typeref
 X1	input.cpp	/^template<class X1> void A<T>::g1(T, X1) { }$/;"	Z	function:A::g1
 g1	input.cpp	/^template<class X1> void A<int>::g1(int, X1);$/;"	p	class:A	typeref:typename:void	file:	signature:(int,X1)	template:<class X1>	properties:scopespecialization,specialization
 X1	input.cpp	/^template<class X1> void A<int>::g1(int, X1);$/;"	Z	function:A::g1
-g2	input.cpp	/^template<> void A<int>::g2<char>(int, char); \/\/ for X2 = char$/;"	p	class:A	typeref:typename:void	file:	signature:(int,char)	template:<>	properties:scopespecialization,specialization
+g2	input.cpp	/^template<> void A<int>::g2<char>(int, char); \/\/ for X2 = char$/;"	p	class:A	typeref:typename:void	file:	signature:(int,char)	template:<>	specialization:<char>	properties:scopespecialization,specialization
 g1	input.cpp	/^template<> void A<int>::g1(int, char);$/;"	p	class:A	typeref:typename:void	file:	signature:(int,char)	template:<>	properties:scopespecialization,specialization
 m	input.cpp	/^template<typename X> void m(X)$/;"	f	typeref:typename:void	signature:(X)	template:<typename X>
 X	input.cpp	/^template<typename X> void m(X)$/;"	Z	function:m
-m	input.cpp	/^template<> void m<int>(int)$/;"	f	typeref:typename:void	signature:(int)	template:<>	properties:specialization
-m	input.cpp	/^template<> void m<A>(A)$/;"	f	typeref:typename:void	signature:(A)	template:<>	properties:specialization
+m	input.cpp	/^template<> void m<int>(int)$/;"	f	typeref:typename:void	signature:(int)	template:<>	specialization:<int>	properties:specialization
+m	input.cpp	/^template<> void m<A<int>>(A<int> a)$/;"	f	typeref:typename:void	signature:(A<int> a)	template:<>	specialization:<A<int>>	properties:specialization
+a	input.cpp	/^template<> void m<A<int>>(A<int> a)$/;"	z	function:m	typeref:typename:A<int>	file:
+B	input.cpp	/^struct B { };$/;"	s	file:
+m	input.cpp	/^template<> void m<B>(B)$/;"	f	typeref:typename:void	signature:(B)	template:<>	specialization:<B>	properties:specialization
 m	input.cpp	/^template<> void m(char)$/;"	f	typeref:typename:void	signature:(char)	template:<>	properties:specialization
+C	input.cpp	/^class C {};$/;"	c	file:	template:<class T1,class T2,int I>
+T1	input.cpp	/^template<class T1, class T2, int I>$/;"	Z	class:C
+T2	input.cpp	/^template<class T1, class T2, int I>$/;"	Z	class:C
+I	input.cpp	/^template<class T1, class T2, int I>$/;"	Z	class:C
+C	input.cpp	/^class C<T, T*, I> {};$/;"	c	file:	template:<class T,int I>	specialization:<T,T *,I>
+T	input.cpp	/^template<class T, int I>$/;"	Z	class:C
+I	input.cpp	/^template<class T, int I>$/;"	Z	class:C
+C	input.cpp	/^class C<T*, T2, I> {};$/;"	c	file:	template:<class T,class T2,int I>	specialization:<T *,T2,I>
+T	input.cpp	/^template<class T, class T2, int I>$/;"	Z	class:C
+T2	input.cpp	/^template<class T, class T2, int I>$/;"	Z	class:C
+I	input.cpp	/^template<class T, class T2, int I>$/;"	Z	class:C
+C	input.cpp	/^class C<int, T*, 5> {};$/;"	c	file:	template:<class T>	specialization:<int,T *,5>
+T	input.cpp	/^template<class T>$/;"	Z	class:C
+C	input.cpp	/^class C<X, T*, I> {};$/;"	c	file:	template:<class X,class T,int I>	specialization:<X,T *,I>
+X	input.cpp	/^template<class X, class T, int I>$/;"	Z	class:C
+T	input.cpp	/^template<class X, class T, int I>$/;"	Z	class:C
+I	input.cpp	/^template<class X, class T, int I>$/;"	Z	class:C

Units/parser-cxx.r/template-specializations.d/input.cpp
@@ -5,25 +5,25 @@ struct A {
     template<class X1> void g1(T, X1); // member template
     template<class X2> void g2(T, X2); // member template
 };
- 
+
 // specialization of a member
 template<> void A<int>::f(int);
 // member specialization OK even if defined in-class
 template<> void A<int>::h(int) {}
- 
+
 // out of class member template definition
 template<class T>
 template<class X1> void A<T>::g1(T, X1) { }
- 
+
 // member template specialization
 template<>
 template<class X1> void A<int>::g1(int, X1);
- 
+
 // member template specialization
 template<>
 template<> void A<int>::g2<char>(int, char); // for X2 = char
 // same, using template argument deduction (X1 = char)
-template<> 
+template<>
 template<> void A<int>::g1(int, char);
 
 template<typename X> void m(X)
@@ -34,8 +34,13 @@ template<> void m<int>(int)
 {
 }
 
+template<> void m<A<int>>(A<int> a)
+{
+}
+
 // bug #2181
-template<> void m<A>(A)
+struct B { };
+template<> void m<B>(B)
 {
 }
 
@@ -47,3 +52,26 @@ template<> void m(char)
 	// This is broken input. Should *not* be extracted.
 	template <> void int<int>(int a);
 #endif
+
+
+// Examples from the "manual".
+
+// primary template
+template<class T1, class T2, int I>
+class C {};
+
+//partial specialization where T2 is a pointer to T1
+template<class T, int I>
+class C<T, T*, I> {};
+
+// partial specialization where T1 is a pointer
+template<class T, class T2, int I>
+class C<T*, T2, I> {};
+
+// partial specialization where T1 is int, I is 5, and T2 is a pointer
+template<class T>
+class C<int, T*, 5> {};
+
+// partial specialization where T2 is a pointer
+template<class X, class T, int I>
+class C<X, T*, I> {};

parsers/cxx/cxx_parser.c
@@ -51,6 +51,9 @@ void cxxParserNewStatement(void)
 	{
 		cxxTokenChainDestroy(g_cxx.pTemplateTokenChain);
 		g_cxx.pTemplateTokenChain = NULL;
+	} else {
+		// we don't care about stale specializations as they
+		// are destroyed wen the base template prefix is extracted
 	}
 	g_cxx.uKeywordState = 0;
 
@@ -1002,18 +1005,16 @@ static bool cxxParserParseClassStructOrUnionInternal(
 		// {
 		// }
 
-		CXXTokenChain * pSpecChain = cxxParserParseTemplateAngleBracketsToSeparateChain(false);
-		if(!pSpecChain)
+		if(g_cxx.pTemplateSpecializationTokenChain)
+			cxxTokenChainDestroy(g_cxx.pTemplateSpecializationTokenChain);
+
+		g_cxx.pTemplateSpecializationTokenChain = cxxParserParseTemplateAngleBracketsToSeparateChain(false);
+		if(!g_cxx.pTemplateSpecializationTokenChain)
 		{
 			cxxKeywordEnableFinal(false);
 			CXX_DEBUG_LEAVE_TEXT("Could not parse class/struct/union name");
 			return false;
 		}
-
-		// FIXME: We currently just discard the specialization chain.
-		//        Should we add it as a new field?
-
-		cxxTokenChainDestroy(pSpecChain);
 	}
 
 	// Once we reached the terminator, "final" is not a keyword anymore.
@@ -1291,11 +1292,8 @@ static bool cxxParserParseClassStructOrUnionInternal(
 			}
 		}
 
-		if(
-				bGotTemplate &&
-				cxxTagFieldEnabled(CXXTagCPPFieldTemplate)
-			)
-			cxxTagHandleTemplateField();
+		if(bGotTemplate)
+			cxxTagHandleTemplateFields();
 
 		tag->isFileScope = !isInputHeaderFile();
 
@@ -1849,6 +1847,8 @@ static rescanReason cxxParserMain(const unsigned int passCount)
 	cxxTokenChainClear(g_cxx.pTokenChain);
 	if(g_cxx.pTemplateTokenChain)
 		cxxTokenChainClear(g_cxx.pTemplateTokenChain);
+	if(g_cxx.pTemplateSpecializationTokenChain)
+		cxxTokenChainClear(g_cxx.pTemplateSpecializationTokenChain);
 	// Restart coveralls: LCOV_EXCL_END
 
 	if(!bRet && (passCount == 1))
@@ -1969,6 +1969,8 @@ void cxxParserCleanup(langType language CTAGS_ATTR_UNUSED,bool initialized CTAGS
 		cxxTokenChainDestroy(g_cxx.pTokenChain);
 	if(g_cxx.pTemplateTokenChain)
 		cxxTokenChainDestroy(g_cxx.pTemplateTokenChain);
+	if(g_cxx.pTemplateSpecializationTokenChain)
+		cxxTokenChainDestroy(g_cxx.pTemplateSpecializationTokenChain);
 	if(g_cxx.pTemplateParameters)
 		ptrArrayDelete(g_cxx.pTemplateParameters);
 	// Restart coveralls: LCOV_EXCL_END

commit 9f8e4f24303ab6d4528999ba576abfb69f292fd7

Cxx: fix the memory leak related to field attaching
Close #2524.
The memory leak was introduced in fcc4853e.

When attaching a field to a tag, the original code calls
attachParserField() wrong way. The caller must own the field value
passed to attachParserField(); the caller must free the memory
object for the value after calling makeTagEntry().
However, the original code didn't do so. As the result, the memory
object for the value was leaked.

This change introduces the parser trash box. After calling
attachParserField(), the caller puts the memory object to the trash
box; the memory object is freed when parsing the current input file is
completed.

Signed-off-by: Masatake YAMATO <yamato@redhat.com>

parsers/cxx/cxx_tag.c
@@ -16,6 +16,7 @@
 #include "entry.h"
 #include "../cpreprocessor.h"
 #include "routines.h"
+#include "trashbox.h"
 #include "xtag.h"
 
 #define CXX_COMMON_MACRO_ROLES(__langPrefix) \
@@ -527,7 +528,11 @@ void cxxTagSetField(unsigned int uField,const char * szValue,bool bCopyValue)
 	if(!g_cxx.pFieldOptions[uField].enabled)
 		return;
 
-	attachParserField(&g_oCXXTag,bCopyValue,g_cxx.pFieldOptions[uField].ftype,szValue);
+	/* If we make a copy for the value, the copy must be freed after
+	 * calling cxxTagCommit() for g_oCXXTag. The parser trash box
+	 * allows us to delay freeing the copy. */
+	attachParserField(&g_oCXXTag,false,g_cxx.pFieldOptions[uField].ftype,
+					  bCopyValue?parserTrashBoxPut(eStrdup(szValue),eFree):szValue);
 }
 
 void cxxTagSetCorkQueueField(