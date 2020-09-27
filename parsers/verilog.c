612ee9340d91511b9506827a267354653446d224
Hiroo HAYASHI <24754036+hirooih@users.noreply.github.com>
2020-09-27 15:47:27 +0900

    SystemVerilog: mark unspecified Verilog/SystemVerilog keywords as K_IGNORE
    This is required to distinguish an identifier from a keyword.
    define verilogKeywords[] and systemVerilogKeywords[].
    systemverilog-net-var.d/expected.tags is changed because "packed" is
    ignored properly.

--- parsers/verilog.c
+++ parsers/verilog.c
@@ -47,7 +47,7 @@
  * code related to the symbol table. */
 typedef enum {
 	/* parser private items */
-	K_IGNORE = -16,
+	K_IGNORE = -16,	/* Verilog/SystemVerilog keywords to be ignored */
 	K_DEFINE,
 	K_IFDEF,
 	K_BEGIN,
@@ -237,6 +237,73 @@ static const keywordAssoc KeywordTable [] = {
 static tokenInfo *currentContext = NULL;
 static tokenInfo *tagContents = NULL;
 
+// IEEE Std 1364-2005 LRM, Appendix B "List of Keywords"
+static const char *verilogKeywords[] = {
+	"always", "and", "assign", "automatic", "begin", "buf", "bufif0",
+	"bufif1", "case", "casex", "casez", "cell", "cmos", "config",
+	"deassign", "default", "defparam", "design", "disable", "edge",
+	"else", "end", "endcase", "endconfig", "endfunction", "endgenerate",
+	"endmodule", "endprimitive", "endspecify", "endtable", "endtask",
+	"event", "for", "force", "forever", "fork", "function", "generate",
+	"genvar", "highz0", "highz1", "if", "ifnone", "incdir", "include",
+	"initial", "inout", "input", "instance", "integer", "join", "large",
+	"liblist", "library", "localparam", "macromodule", "medium", "module",
+	"nand", "negedge", "nmos", "nor", "noshowcancelled", "not", "notif0",
+	"notif1", "or", "output", "parameter", "pmos", "posedge", "primitive",
+	"pull0", "pull1", "pulldown", "pullup", "pulsestyle_onevent",
+	"pulsestyle_ondetect", "rcmos", "real", "realtime", "reg", "release",
+	"repeat", "rnmos", "rpmos", "rtran", "rtranif0", "rtranif1",
+	"scalared", "showcancelled", "signed", "small", "specify",
+	"specparam", "strong0", "strong1", "supply0", "supply1", "table",
+	"task", "time", "tran", "tranif0", "tranif1", "tri", "tri0", "tri1",
+	"triand", "trior", "trireg", "unsigned1", "use", "uwire", "vectored",
+	"wait", "wand", "weak0", "weak1", "while", "wire", "wor", "xnor", "xor"
+};
+// IEEE Std 1800-2017 LRM, Annex B "Keywords"
+static const char *systemVerilogKeywords[] = {
+	"accept_on", "alias", "always", "always_comb", "always_ff",
+	"always_latch", "and", "assert", "assign", "assume", "automatic",
+	"before", "begin", "bind", "bins", "binsof", "bit", "break", "buf",
+	"bufif0", "bufif1", "byte", "case", "casex", "casez", "cell",
+	"chandle", "checker", "class", "clocking", "cmos", "config", "const",
+	"constraint", "context", "continue", "cover", "covergroup",
+	"coverpoint", "cross", "deassign", "default", "defparam", "design",
+	"disable", "dist", "do", "edge", "else", "end", "endcase",
+	"endchecker", "endclass", "endclocking", "endconfig", "endfunction",
+	"endgenerate", "endgroup", "endinterface", "endmodule", "endpackage",
+	"endprimitive", "endprogram", "endproperty", "endspecify",
+	"endsequence", "endtable", "endtask", "enum", "event", "eventually",
+	"expect", "export", "extends", "extern", "final", "first_match",
+	"for", "force", "foreach", "forever", "fork", "forkjoin", "function",
+	"generate", "genvar", "global", "highz0", "highz1", "if", "iff",
+	"ifnone", "ignore_bins", "illegal_bins", "implements", "implies",
+	"import", "incdir", "include", "initial", "inout", "input", "inside",
+	"instance", "int", "integer", "interconnect", "interface",
+	"intersect", "join", "join_any", "join_none", "large", "let",
+	"liblist", "library", "local", "localparam", "logic", "longint",
+	"macromodule", "matches", "medium", "modport", "module", "nand",
+	"negedge", "nettype", "new", "nexttime", "nmos", "nor",
+	"noshowcancelled", "not", "notif0", "notif1", "null", "or", "output",
+	"package", "packed", "parameter", "pmos", "posedge", "primitive",
+	"priority", "program", "property", "protected", "pull0", "pull1",
+	"pulldown", "pullup", "pulsestyle_ondetect", "pulsestyle_onevent",
+	"pure", "rand", "randc", "randcase", "randsequence", "rcmos", "real",
+	"realtime", "ref", "reg", "reject_on", "release", "repeat",
+	"restrict", "return", "rnmos", "rpmos", "rtran", "rtranif0",
+	"rtranif1", "s_always", "s_eventually", "s_nexttime", "s_until",
+	"s_until_with", "scalared", "sequence", "shortint", "shortreal",
+	"showcancelled", "signed", "small", "soft", "solve", "specify",
+	"specparam", "static", "string", "strong", "strong0", "strong1",
+	"struct", "super", "supply0", "supply1", "sync_accept_on",
+	"sync_reject_on", "table", "tagged", "task", "this", "throughout",
+	"time", "timeprecision", "timeunit", "tran", "tranif0", "tranif1",
+	"tri", "tri0", "tri1", "triand", "trior", "trireg", "type", "typedef",
+	"union", "unique", "unique0", "unsigned", "until", "until_with",
+	"untyped", "use", "uwire", "var", "vectored", "virtual", "void",
+	"wait", "wait_order", "wand", "weak", "weak0", "weak1", "while",
+	"wildcard", "wire", "with", "within", "wor", "xnor", "xor"
+};
+
 /*
  *   PROTOTYPE DEFINITIONS
  */
@@ -381,7 +448,7 @@ static char kindEnabled (const verilogKind kind)
 		return VerilogKinds[kind].enabled;
 }
 
-static void buildKeywordHash (const langType language, unsigned int idx)
+static void buildKeywordHash (const langType language, unsigned int idx, const char *keywords[], const unsigned int size)
 {
 	size_t i;
 	const size_t count = ARRAY_SIZE (KeywordTable);
@@ -391,18 +458,23 @@ static void buildKeywordHash (const langType language, unsigned int idx)
 		if (p->isValid [idx])
 			addKeyword (p->keyword, language, (int) p->kind);
 	}
+	/* mark unspecified keywords as K_IGNORE */
+	for (i = 0  ;  i < size  ;  ++i) {
+		if (lookupKeyword(keywords[i], language) == KEYWORD_NONE)
+			addKeyword (keywords[i], language, K_IGNORE);
+	}
 }
 
 static void initializeVerilog (const langType language)
 {
 	Lang_verilog = language;
-	buildKeywordHash (language, IDX_VERILOG);
+	buildKeywordHash (language, IDX_VERILOG, verilogKeywords, sizeof(verilogKeywords) / sizeof(char *));
 }
 
 static void initializeSystemVerilog (const langType language)
 {
 	Lang_systemverilog = language;
-	buildKeywordHash (language, IDX_SYSTEMVERILOG);
+	buildKeywordHash (language, IDX_SYSTEMVERILOG, systemVerilogKeywords, sizeof(systemVerilogKeywords) / sizeof(char *));
 }
 
 static void vUngetc (int c)
