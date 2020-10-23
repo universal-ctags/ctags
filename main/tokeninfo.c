9fb1721d84fb8ff9e268aef62460b173c77b7780
Masatake YAMATO <yamato@redhat.com>
2020-10-24 03:15:05 +0900

    main,comment: fix wrong comments
    Signed-off-by: Masatake YAMATO <yamato@redhat.com>

--- main/tokeninfo.c
+++ main/tokeninfo.c
@@ -5,8 +5,6 @@
 *   This source code is released for free distribution under the terms of the
 *   GNU General Public License version 2 or (at your option) any later version.
 *
-*   This module contains functions for generating tags for Python language
-*   files.
 */
 
 #include "general.h"
