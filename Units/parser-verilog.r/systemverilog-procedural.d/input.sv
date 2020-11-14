128d6ff19b204256eed9bd96978a2d19106f4411
Hiroo HAYASHI <24754036+hirooih@users.noreply.github.com>
2020-11-14 17:20:39 +0900

    Units(SystemVerilog): systemverilog-procedural.d: add tests

--- Units/parser-verilog.r/systemverilog-procedural.d/input.sv
+++ Units/parser-verilog.r/systemverilog-procedural.d/input.sv
@@ -1,29 +1,55 @@
 //
-// LRM 12. Procedural programming statements
+// LRM 12. Procedural programming Statements
+//    These construct does not generate tags basically.
+//    But they must be ignored properly.
 //
 
 module procedural;
 
+  int index, rega, regb, result;
+
+  // 12.4 Conditional if–else statement
+  always_comb begin
+    if (index > 0)
+      if (rega > regb) result = rega;
+      else  // else applies to preceding if
+        result = regb;
+
+    if (index > 0)
+      begin
+        if (rega > regb)
+          result = rega;
+      end
+    else result = regb;
+  end
+
   string s;
   always_comb begin
-    s = "foo bar ( { [ ";
+    if (index > 0)
+      s = "foo bar ) } ";
   end
 
+  // 
+  int value, a[3];
+  int offset = 10;
   parameter N = 8;
   always_comb begin
-    int a;
-    for (int i = 0; i < N; i++) begin : outer1
-      int b;
-      for (int j = 0; j < N; j++) begin:inner1
-        int c;
+    for ( int count = 0; count < 3; count++ )
+      value = value +((a[count]) * (count+1));
+
+    for ( int count = 0, done = 0, j = 0; j * count < 125; j++, count++)
+      $display("Value j = %d\n", j );
+
+    for (int i = 0, j = i+offset; i < N; i++,j++)
+      $display("i = %d, j = %d\n", i, j );
+  end
+
+  always_comb begin
+    for (int i = 0; i < N; i++) begin:outer
+      for (int j = 0; j < N; j++) begin : inner
+        // ...
       end
     end
-
-    for (int i = 0; i < N; i++) begin : outer2
-      int d;
-      for (int j = 0; j < N; j++) begin:inner2
-        int e;
-      end:inner2
-    end : outer2
   end
+
 endmodule
