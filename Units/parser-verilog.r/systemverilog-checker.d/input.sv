c4ca26762fa410ea0eb4b8403696af6913312475
Hiroo HAYASHI <24754036+hirooih@users.noreply.github.com>
2020-11-27 21:44:07 +0900

    Units(SystemVerilog): fix unmatched parens in input.sv

--- Units/parser-verilog.r/systemverilog-checker.d/input.sv
+++ Units/parser-verilog.r/systemverilog-checker.d/input.sv
@@ -16,7 +16,7 @@ endchecker : my_check1
 
 // Simple checker containing deferred assertions
 checker my_check2 (logic a, b);
-    a1: assert #0 ($onehot0({a, b});
+    a1: assert #0 ($onehot0({a, b}));
     c1: cover #0 (a == 0 && b == 0);
     c2: cover #0 (a == 1);
     c3: cover #0 (b == 1);
@@ -25,8 +25,8 @@ endchecker : my_check2
 // Simple checker with output arguments
 checker my_check3 (logic a, b, event clock, output bit failure, undef);
     default clocking @clock; endclocking
-    a1: assert property ($onehot0({a, b}) failure = 1'b0; else failure = 1'b1;
-    a2: assert property ($isunknown({a, b}) undef = 1'b0; else undef = 1'b1;
+    a1: assert property ($onehot0({a, b})) failure = 1'b0; else failure = 1'b1;
+    a2: assert property ($isunknown({a, b})) undef = 1'b0; else undef = 1'b1;
 endchecker : my_check3
 
 // Checker with default input and initialized output arguments
