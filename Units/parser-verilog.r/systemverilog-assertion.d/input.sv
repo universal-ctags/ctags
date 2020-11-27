66c00fcac9d4b219b5d65e6f4e358839508d17e0
Hiroo HAYASHI <24754036+hirooih@users.noreply.github.com>
2020-11-27 21:52:19 +0900

    Units(SystemVerilog): rename task() to myTask()
    "task" is a reserved word.

--- Units/parser-verilog.r/systemverilog-assertion.d/input.sv
+++ Units/parser-verilog.r/systemverilog-assertion.d/input.sv
@@ -1,13 +1,13 @@
 initial begin : deferred_immediate_assertions
-    immediate_assertion : assert () task();
-    immediate_cover     : cover () task();
-    immediate_assume    : assume () task();
-    deferred_assertion1 : assert #0 () task();
-    deferred_cover1     : cover #0 () task();
-    deferred_assume1    : assume #0 () task();
-    deferred_assertion2 : assert final () task();
-    deferred_cover2     : cover final () task();
-    deferred_assume2    : assume final () task();
+    immediate_assertion : assert () myTask();
+    immediate_cover     : cover () myTask();
+    immediate_assume    : assume () myTask();
+    deferred_assertion1 : assert #0 () myTask();
+    deferred_cover1     : cover #0 () myTask();
+    deferred_assume1    : assume #0 () myTask();
+    deferred_assertion2 : assert final () myTask();
+    deferred_cover2     : cover final () myTask();
+    deferred_assume2    : assume final () myTask();
 end
 
 property prop1 (
