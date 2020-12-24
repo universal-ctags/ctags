e1ab236ac40a1978c466bf8deb2e0e85f63b2db8
Jan Dolinar <jan.dolinar@firma.seznam.cz>
2020-12-24 12:54:08 +0100

    kotlin: improve units

--- Units/parser-kotlin.r/kotlin-destructuring.d/input.kt
+++ Units/parser-kotlin.r/kotlin-destructuring.d/input.kt
@@ -1,4 +1,4 @@
 val (a, b) = functionReturningPair()
 val (c: Int, d: String) = functionReturningPair()
 var (e, f) = functionReturningPair()
-var (g: Int, h: String) = functionReturningPair()
+var (g: Int, h: Pair<String, Int>) = functionReturningPair()
