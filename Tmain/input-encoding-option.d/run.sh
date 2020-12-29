e5d7cedc9e41aa734dda2d8b60f49ce73bd02db5
Masatake YAMATO <yamato@redhat.com>
2020-12-30 06:16:48 +0900

    main: enable TAG_{KIND,EXTRA,FIELD,ROLE}_DESCRIPTION pseudo tags
    Signed-off-by: Masatake YAMATO <yamato@redhat.com>

--- Tmain/input-encoding-option.d/run.sh
+++ Tmain/input-encoding-option.d/run.sh
@@ -11,6 +11,10 @@ BUILDDIR=$2
 if ${CTAGS} --quiet --options=NONE --list-features | grep -q iconv; then
   if ${CTAGS} --quiet --options=NONE \
 	      --pseudo-tags=-TAG_PROC_CWD \
+	      --pseudo-tags=-TAG_KIND_DESCRIPTION \
+	      --pseudo-tags=-TAG_EXTRA_DESCRIPTION \
+	      --pseudo-tags=-TAG_FIELD_DESCRIPTION \
+	      --pseudo-tags=-TAG_ROLE_DESCRIPTION \
 	      --input-encoding=utf-8 --input-encoding-java=shift_jis --input-encoding-javascript=euc-jp \
 	      -o ${BUILDDIR}/tags \
 	      input.js input.java ; then
