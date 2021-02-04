1c6cb518d6aa06758178ba2a92cfee32ca6dc668
Masatake YAMATO <yamato@redhat.com>
2021-02-04 13:22:04 +0900

    Units: add a test case for FunctionParameters subparser
    Signed-off-by: Masatake YAMATO <yamato@redhat.com>

---
+++ Units/simple-function-parameters.d/input.pl
@@ -0,0 +1,40 @@
+# Taken from https://metacpan.org/pod/Function::Parameters
+use Function::Parameters;
+ 
+# plain function
+fun foo($x, $y, $z = 5) {
+    return $x + $y + $z;
+}
+print foo(1, 2), "\n";  # 8
+ 
+# method with implicit $self
+method bar($label, $n) {
+    return "$label: " . ($n * $self->scale);
+}
+ 
+# named arguments: order doesn't matter in the call
+fun create_point(:$x, :$y, :$color) {
+    print "creating a $color point at ($x, $y)\n";
+}
+create_point(
+    color => "red",
+    x     => 10,
+    y     => 5,
+);
+ 
+package Derived {
+    use Function::Parameters qw(:std :modifiers);
+    use Moo;
+ 
+    extends 'Base';
+ 
+    has 'go_big' => (
+        is => 'ro',
+    );
+ 
+    # "around" method with implicit $orig and $self
+    around size() {
+        return $self->$orig() * 2 if $self->go_big;
+        return $self->$orig();
+    }
+}
