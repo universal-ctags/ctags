70677d3b475d9a598a4231a6ecc03167b559bf1b
jimmygoogle <jim@jimandmeg.com>
2021-02-15 20:08:59 -0500

    Units: added tests for Function::Parameters parser (#2868)
    * Added tests for Function::Parameters parser
    
    * Removed Python tests
    
    * Removed whitespace
    
    Co-authored-by: Jim Butler <jim.butler@endurance.com>

---
+++ Units/function-parameters.r/c-fp-test.d/input.c
@@ -0,0 +1,14 @@
+int method(void)
+{
+  return 0;
+}
+
+int sub(void)
+{
+  return 0;
+}
+
+int fun(void)
+{
+  return 0;
+}
---
+++ Units/function-parameters.r/perl-fp-test.d/input.pl
@@ -0,0 +1,11 @@
+package Jim::Butler;
+
+use Function::Parameters;
+
+method my_method() {}
+
+fun my_function() {}
+
+sub my_subroutine {}
+
+1;
