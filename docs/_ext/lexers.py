7c260c669e2d66c83d37822cc738598ae1db04be
Masatake YAMATO <yamato@redhat.com>
2021-05-13 16:30:28 +0900

    docs: as a highlighter, specify CtagsOtplibLexer class in sphinx-4
    Close #3012.
    
    Signed-off-by: Masatake YAMATO <yamato@redhat.com>
    Tested-by: fpliu <leleliu008@gmail.com>

--- docs/_ext/lexers.py
+++ docs/_ext/lexers.py
@@ -1,5 +1,13 @@
 from ctags_optlib_highlighter import CtagsOtplibLexer
+import sphinx
 
 def setup(app):
-    app.add_lexer('ctags', CtagsOtplibLexer())
-
+    v = sphinx.__version__.split('.')
+    if int(v[0]) > 3:
+        c = CtagsOtplibLexer
+    else:
+        if (int(v[1]) > 3):
+            c = CtagsOtplibLexer
+        else:
+            c = CtagsOtplibLexer()
+    app.add_lexer('ctags', c)
