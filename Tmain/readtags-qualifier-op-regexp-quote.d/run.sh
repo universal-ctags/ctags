b38af5142b702e9217b7a77b860b1284b60f9a05
Masatake YAMATO <yamato@redhat.com>
2021-06-18 23:14:13 +0900

    Tmain: delete redundant lines
    Signed-off-by: Masatake YAMATO <yamato@redhat.com>

--- Tmain/readtags-qualifier-op-regexp-quote.d/run.sh
+++ Tmain/readtags-qualifier-op-regexp-quote.d/run.sh
@@ -14,22 +14,6 @@ if ! [ -x "${READTAGS}" ]; then
     skip "no readtags"
 fi
 
-#!/bin/sh
-
-# Copyright: 2021 Masatake YAMATO
-# License: GPL-2
-
-READTAGS=$3
-
-. ../utils.sh
-
-#V="valgrind --leak-check=full -v"
-V=
-
-if ! [ -x "${READTAGS}" ]; then
-    skip "no readtags"
-fi
-
 if ! ( "${READTAGS}" -h | grep -q -e -S ); then
     skip "no qualifier function in readtags"
 fi
