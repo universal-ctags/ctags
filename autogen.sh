a150bcb2c7b16b680e6745654998957f049827e6
Hiroo HAYASHI <24754036+hirooih@users.noreply.github.com>
2021-07-25 10:50:10 +0900

    build-sys: autogen.sh: enable xtrace when CI is set.

--- autogen.sh
+++ autogen.sh
@@ -11,7 +11,10 @@ for t in autoreconf aclocal pkg-config; do
 done
 echo '##################################################################'
 
-set -xe
+set -e	# errexit (exit on error)
+if [ ! -z "${CI}" ]; then
+	set -x	# xtrace (execution trace)
+fi
 
 type autoreconf > /dev/null 2>&1 || {
 	echo "No autotools (autoconf and automake) found" 1>&2
