1db733082523f4a59bf605a4ba4f1df59f5630fd
Masatake YAMATO <yamato@redhat.com>
2021-10-28 08:05:45 +0900

    docs: introduce stub highlighters to suppress warning
    Signed-off-by: Masatake YAMATO <yamato@redhat.com>

--- docs/_ext/lexers.py
+++ docs/_ext/lexers.py
@@ -1,12 +1,19 @@
 from ctags_optlib_highlighter import CtagsOptlibLexer
+from pygments.lexers.special import TextLexer
 import sphinx
 
-def setup(app):
+def install(app, h, name):
     v = sphinx.__version__.split('.')
     if int(v[0]) > 3:
-        c = CtagsOptlibLexer
+        c = h
     elif (int(v[0]) == 3) and (int(v[1]) > 3):
-        c = CtagsOptlibLexer
+        c = h
     else:
-        c = CtagsOptlibLexer()
-    app.add_lexer('ctags', c)
+        c = h()
+    app.add_lexer(name, c)
+
+def setup(app):
+    install(app, CtagsOptlibLexer, 'ctags')
+    # Setup stubs
+    for h in ['git', 'yacc', 'EmacsLisp', 'tags', 'Autoconf', 'SystemTap', 'RMarkdown']:
+        install(app, TextLexer, h)
