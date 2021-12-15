57319efb30df0b8cd4482c24252cd8559875dfd7
Masatake YAMATO <yamato@redhat.com>
2021-12-16 07:13:52 +0900

    optimize the crash test patterns more

--- Tmain/errors-about-parser-own-extras.d/run.sh
+++ Tmain/errors-about-parser-own-extras.d/run.sh
@@ -1,5 +1,2 @@
 set -x
-$1 --with-list-header=no --extras-NOSUCHLANG=-'{whitespaceSwapped}' --list-extras; echo $? 2>&1
-$1 --with-list-header=no --list-extras; echo $? 2>&1
-$1 --extras-NOSUCHLANG=-'{whitespaceSwapped}' --list-extras; echo $? 2>&1
-$1 --list-extras; echo $? 2>&1
+$1 --verbose --list-extras; echo $? 2>&1
