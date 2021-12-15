d0771cba8960a8f9bc648367f57c59ca0bbd5dcb
Masatake YAMATO <yamato@redhat.com>
2021-12-16 07:04:32 +0900

    optimize the crash test patterns

--- Tmain/errors-about-parser-own-extras.d/run.sh
+++ Tmain/errors-about-parser-own-extras.d/run.sh
@@ -1,22 +1,5 @@
 set -x
-$1 --quiet --options=NONE --with-list-header=no --extras-NOSUCHLANG=-'{whitespaceSwapped}' --list-extras; echo $? 2>&1
-$1 --quiet --options=NONE --with-list-header=no --extras-NOSUCHLANG=-'{whitespaceSwapped}'; echo $? 2>&1
-$1 --quiet --options=NONE --with-list-header=no; echo $? 2>&1
-$1 --quiet --options=NONE; echo $? 2>&1
-$1 --quiet; echo $? 2>&1
-$1 --options=NONE --with-list-header=no --extras-NOSUCHLANG=-'{whitespaceSwapped}' --list-extras; echo $? 2>&1
-$1 --options=NONE --with-list-header=no --extras-NOSUCHLANG=-'{whitespaceSwapped}'; echo $? 2>&1
-$1 --options=NONE --with-list-header=no; echo $? 2>&1
-$1 --options=NONE; echo $? 2>&1
 $1 --with-list-header=no --extras-NOSUCHLANG=-'{whitespaceSwapped}' --list-extras; echo $? 2>&1
-$1 --with-list-header=no --extras-NOSUCHLANG=-'{whitespaceSwapped}'; echo $? 2>&1
-$1 --with-list-header=no; echo $? 2>&1
-$1 --verbose --options=NONE --with-list-header=no --extras-NOSUCHLANG=-'{whitespaceSwapped}' --list-extras; echo $? 2>&1
-$1 --verbose --options=NONE --with-list-header=no --extras-NOSUCHLANG=-'{whitespaceSwapped}'; echo $? 2>&1
-$1 --verbose --options=NONE --with-list-header=no; echo $? 2>&1
-$1 --verbose --options=NONE; echo $? 2>&1
-$1 --verbose --with-list-header=no --extras-NOSUCHLANG=-'{whitespaceSwapped}' --list-extras; echo $? 2>&1
-$1 --verbose --with-list-header=no --extras-NOSUCHLANG=-'{whitespaceSwapped}'; echo $? 2>&1
-$1 --verbose --with-list-header=no; echo $? 2>&1
-$1 --verbose; echo $? 2>&1
-$1; echo $? 2>&1
+$1 --with-list-header=no --list-extras; echo $? 2>&1
+$1 --extras-NOSUCHLANG=-'{whitespaceSwapped}' --list-extras; echo $? 2>&1
+$1 --list-extras; echo $? 2>&1
