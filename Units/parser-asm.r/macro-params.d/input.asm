12ab91a0a1ac44a3153cca0ffd57c7a266832f83
Masatake YAMATO <yamato@redhat.com>
2021-12-27 17:20:56 +0900

    Asm: extract macro parameters
    Signed-off-by: Masatake YAMATO <yamato@redhat.com>

---
+++ Units/parser-asm.r/macro-params.d/input.asm
@@ -0,0 +1,16 @@
+	;; Taken from "7.63 '.macro'", the section of the info document for Gas
+
+	.macro comm
+	.endm
+
+	.macro plus1 p, p1
+	.endm
+
+	.macro plus2 p p1
+	.endm
+
+	.macro reserve_str p1=0 p2
+	.endm
+
+	.macro m p1:req, p2=0, p3:vararg
+	.endm
