24dc869cf53abcc4e3824ebe762d1cb4b8595f01
Masatake YAMATO <yamato@redhat.com>
2022-03-18 23:48:59 +0900

    Tmain: skip list-subparsers-all if yaml feature is not available
    Signed-off-by: Masatake YAMATO <yamato@redhat.com>

--- Tmain/list-subparsers-all.d/run.sh
+++ Tmain/list-subparsers-all.d/run.sh
@@ -5,7 +5,7 @@ CTAGS="$1"
 
 . ../utils.sh
 is_feature_available $CTAGS xpath
+is_feature_available $CTAGS yaml
 
 ${CTAGS} --quiet --options=NONE \
-		 --list-subparsers=all \
-	| grep -v AnsiblePlaybook
+		 --list-subparsers=all
