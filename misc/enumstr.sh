universal-ctags/ctags

commit daefdc4a4e7a7b7be8f3cf2a2832f14faee7a08b

misc/enumstr: assume .h is for C header file
Signed-off-by: Masatake YAMATO <yamato@redhat.com>

misc/enumstr.sh
@@ -33,7 +33,7 @@ printf '{ /* Generated by misc/enumstr.sh with cmdline:\n'
 printf '     %s */\n' "$*"
 echo '	switch (e)'
 echo '	{'
-./ctags --quiet --options=NONE --sort=no -o - --languages=C --kinds-C=e -x --_xformat="%N enum:%s" $1 | grep $2 | while read N S; do
+./ctags --quiet --options=NONE --sort=no -o - --languages=C --kinds-C=e --map-C=.h -x --_xformat="%N enum:%s" $1 | grep $2 | while read N S; do
 	n=$N
 	if [ -n "$4" ]; then
 		n=${N#$4}

commit a1b970706acfa669d78ba11d584894ddaa3a5d09

misc/enumstr: assume both .h AND .c is for C header file
This fixes a bug I introduced in daefdc4a4e.

Signed-off-by: Masatake YAMATO <yamato@redhat.com>

misc/enumstr.sh
@@ -33,7 +33,7 @@ printf '{ /* Generated by misc/enumstr.sh with cmdline:\n'
 printf '     %s */\n' "$*"
 echo '	switch (e)'
 echo '	{'
-./ctags --quiet --options=NONE --sort=no -o - --languages=C --kinds-C=e --map-C=.h -x --_xformat="%N enum:%s" $1 | grep $2 | while read N S; do
+./ctags --quiet --options=NONE --sort=no -o - --languages=C --kinds-C=e --map-C=+.h -x --_xformat="%N enum:%s" $1 | grep $2 | while read N S; do
 	n=$N
 	if [ -n "$4" ]; then
 		n=${N#$4}