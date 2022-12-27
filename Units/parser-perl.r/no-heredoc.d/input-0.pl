6f5b873fd2014a813402bfb595c98bcf8d52e45d
Masatake YAMATO <yamato@redhat.com>
2022-12-28 04:53:16 +0900

    Units(Perl): add more cases for testing heredoc recognition
    Signed-off-by: Masatake YAMATO <yamato@redhat.com>

---
+++ Units/parser-perl.r/no-heredoc.d/input-0.pl
@@ -0,0 +1,31 @@
+print 3 + 2 <<5;
+5
+    ;
+
+sub five_sub() {
+    return 5
+}
+print 3 + 2 << five_sub;
+five
+    ;
+
+print 3 + 2 . <<five_mark0;
+a
+five_mark0
+
+print 3 + 2 . <<~five_mark1;
+a
+five_mark1
+
+print 3 + 2 << ~five_sub;
+five_sub;
+
+print 3 + 2 . << "five_mark2";
+	ox
+five_mark2
+
+
+
+
+
+
