a674e97ae49266402ea7aaaff941a167490c4ad2
Masatake YAMATO <yamato@redhat.com>
2023-01-11 05:46:11 +0900

    CPreProcessor: skip a newline after a backslash character in string or char literals
    Signed-off-by: Masatake YAMATO <yamato@redhat.com>

---
+++ Units/parser-cpreprocessor.r/skip-newlines-in-literals.d/input-1.c
@@ -0,0 +1,8 @@
+#define J 'aaaaaalastSHOULDNOTBETAGGED'
+#define K 'aaaaaalast\n'
+#define L 'aaaaaalas\nt'
+#define M 'aaaaaala\nxx'
+#define N 'aaaaaalas\
+t'
+#define O 'aaaaaala\
+xx'
---
+++ Units/parser-cpreprocessor.r/skip-newlines-in-literals.d/input.c
@@ -0,0 +1,9 @@
+#define X "abc \n\t\
+	efg"
+#define Y "abc"
+
+#define A '\
+a'
+
+#define B 'b'
+#define C '\n'
