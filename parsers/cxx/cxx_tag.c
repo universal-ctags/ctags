universal-ctags/ctags

commit b9249a8fc960f751bbae0430ac711f21af771eb0

Cxx: don't set "nth:" fields in upper layers of parsers
The lower layer of tagging sets the fields.

Signed-off-by: Masatake YAMATO <yamato@redhat.com>

parsers/cxx/cxx_tag.c
@@ -298,7 +298,8 @@ tagEntryInfo * cxxTagBegin(unsigned int uKind,CXXToken * pToken)
 		g_oCXXTag.extensionFields.scopeIndex = cxxScopeGetDefTag();
 		if (g_oCXXTag.extensionFields.scopeIndex != CORK_NIL)
 		{
-			if (uKind == CXXTagKindMEMBER || uKind == CXXTagKindENUMERATOR)
+			if (uKind == CXXTagKindMEMBER || uKind == CXXTagKindENUMERATOR
+				|| uKind == CXXTagKindPARAMETER || CXXTagCPPKindTEMPLATEPARAM)
 				g_oCXXTag.extensionFields.nth =
 					(short) countEntriesInScope(g_oCXXTag.extensionFields.scopeIndex,
 												true,

parsers/cxx/cxx_parser_function.c
@@ -1925,7 +1925,6 @@ void cxxParserEmitFunctionParameterTags(CXXTypedVariableSet * pInfo)
 		} else {
 			pTypeName = NULL;
 		}
-		tag->extensionFields.nth = i;
 
 		tag->isFileScope = true;
 

parsers/cxx/cxx_parser_template.c
@@ -823,8 +823,6 @@ void cxxParserEmitTemplateParameterTags(void)
 		if(!tag)
 			continue;
 
-		tag->extensionFields.nth = (short)i;
-
 		CXXToken * pTypeToken = cxxTagCheckAndSetTypeField(
 				g_cxx.oTemplateParameters.aTypeStarts[i],
 				g_cxx.oTemplateParameters.aTypeEnds[i]

parsers/cxx/cxx_parser_typedef.c
@@ -442,7 +442,8 @@ skip_to_comma_or_end:
 			if(bGotTemplate)
 				cxxTagHandleTemplateFields();
 
-			cxxTagCommit(NULL);
+			int iCorkQueueIndex = cxxTagCommit(NULL);
+			cxxTagUseTokenAsPartOfDefTag(iCorkQueueIndex, t);
 
 			if (
 					bGotTemplate &&

parsers/cxx/cxx_parser_variable.c
@@ -767,6 +767,7 @@ got_identifier:
 				cxxTagHandleTemplateFields();
 
 			iCorkIndex = cxxTagCommit(&iCorkIndexFQ);
+			cxxTagUseTokenAsPartOfDefTag(iCorkIndexFQ, pIdentifier);
 
 			if(pTypeToken)
 				cxxTokenDestroy(pTypeToken);

commit c34c579ba61a3ab70079842f15093fac80d09664

Cxx: Fix typo in a conditional
Introduced in b9249a8fc960f751bbae0430ac711f21af771eb0

parsers/cxx/cxx_tag.c
@@ -314,7 +314,7 @@ tagEntryInfo * cxxTagBegin(unsigned int uKind,CXXToken * pToken)
 		if (g_oCXXTag.extensionFields.scopeIndex != CORK_NIL)
 		{
 			if (uKind == CXXTagKindMEMBER || uKind == CXXTagKindENUMERATOR
-				|| uKind == CXXTagKindPARAMETER || CXXTagCPPKindTEMPLATEPARAM)
+				|| uKind == CXXTagKindPARAMETER || uKind == CXXTagCPPKindTEMPLATEPARAM)
 				g_oCXXTag.extensionFields.nth =
 					(short) countEntriesInScope(g_oCXXTag.extensionFields.scopeIndex,
 												true,