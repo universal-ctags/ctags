4bcabdc8c8d938719ec1118efe3f8604bd158ceb
Masatake YAMATO <yamato@redhat.com>
2023-05-14 16:52:16 +0900

    main,comment: write about the value returned from getFieldOwner()
    Signed-off-by: Masatake YAMATO <yamato@redhat.com>

--- main/field_p.h
+++ main/field_p.h
@@ -46,7 +46,10 @@ extern fieldType getFieldTypeForName (const char *name);
 extern fieldType getFieldTypeForNameAndLanguage (const char *fieldName, langType language);
 extern bool enableField (fieldType type, bool state);
 extern bool isCommonField (fieldType type);
+
+/* Return LANG_IGNORE if the field is a common field.*/
 extern langType getFieldOwner (fieldType type);
+
 extern const char* getFieldDescription (fieldType type);
 extern const char* getFieldName (fieldType type);
 extern unsigned char getFieldLetter (fieldType type);
