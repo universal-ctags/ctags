8e218a37719a1711340a929072704fac5172b6e8
Colomban Wendling <ban@herbesfolles.org>
2023-05-15 14:23:09 +0200

    erlang: Fix crash parsing directives longer than 31 characters
    Even if they might not exist, fix the code not to crash on them.
    
    The issue was that the pointer to the vString buffer holding the
    directive name was cached at its value right after the vString
    creation, yet that buffer might get realloc()ated if it grows past the
    initial length of 32, possibly making the initial pointer invalid.
    
    Fix this by simply getting the pointer to the value later on.

---
+++ Units/parser-erlang.r/crash1.d/input.erl
@@ -0,0 +1,5 @@
+% long directives used to possibly crash the parser
+% (and always make Valgrind angry)
+-some_extremely_loooong_directive
+-even_longer_might_crash_more_often_than_not_but_anyway_valgrind_would_not_be_hayyp_either_way
+
