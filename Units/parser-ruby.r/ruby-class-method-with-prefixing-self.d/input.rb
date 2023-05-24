692a024dfe05884fb737a2f1ccec54c5da46c0a1
Masatake YAMATO <yamato@redhat.com>
2023-05-25 07:51:57 +0900

    Units(Ruby): add a case for extracting a singleton method starting with self.
    Signed-off-by: Masatake YAMATO <yamato@redhat.com>

---
+++ Units/parser-ruby.r/ruby-class-method-with-prefixing-self.d/input.rb
@@ -0,0 +1,23 @@
+# Derrived from gitlab/app/models/integration.rb
+class Integration < ApplicationRecord
+  UnknownType = Class.new(StandardError)
+  self.inheritance_column = :type_new
+
+  def self.field(name, storage: field_storage, **attrs)
+    fields << ::Integrations::Field.new(name: name, integration_class: self, **attrs)
+
+    case storage
+    when :attribute
+      # noop
+    when :properties
+      prop_accessor(name)
+    when :data_fields
+      data_field(name)
+    else
+      raise ArgumentError, "Unknown field storage: #{storage}"
+    end
+
+    boolean_accessor(name) if attrs[:type] == 'checkbox' && storage != :attribute
+  end
+end
+  
