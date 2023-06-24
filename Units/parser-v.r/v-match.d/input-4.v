6f6a4302330527189e2d6f85993bbef2020ece73
Tim Marston <tim@ed.am>
2023-06-24 17:30:08 +0100

    V: added compile-time kws; fix: match, arr, r-str, type-fns, chains

--- Units/parser-v.r/v-match.d/input-4.v
+++ Units/parser-v.r/v-match.d/input-4.v
@@ -2,5 +2,11 @@ fn main() {
     return match mut x {
         StructA { 0 }
         StructB { 0 }
+        int, u16 { 0 }
+        &a.b.c.Foo { 0 }
+        ?map[string]f32 { 0 }
+        []string { 0 }
+        &a.b.c.Foo, ?map[string]f32, []string { 0 }
+        else { 0 }
     }
 }
