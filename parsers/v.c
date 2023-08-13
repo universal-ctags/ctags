1c8e8acccfe3ed1f91559296e697844801560dbb
Tim Marston <tim@ed.am>
2023-08-13 10:26:03 +0100

    V: added parser docs and copyight

--- parsers/v.c
+++ parsers/v.c
@@ -1,4 +1,6 @@
 /*
+*   Copyright (c) 2023, Tim Marston
+*
 *   This source code is released for free distribution under the terms of the
 *   GNU General Public License version 2 or (at your option) any later version.
 *
