ed36fd1b5054eab1efca923df75aa9fa8a0b8895
Masatake YAMATO <yamato@redhat.com>
2023-12-13 22:34:32 +0900

    misc/news.bash: add copyright notice
    Signed-off-by: Masatake YAMATO <yamato@redhat.com>

--- misc/news.bash
+++ misc/news.bash
@@ -1,4 +1,22 @@
 #!/bin/bash
+#
+# news.bash - enumerate the titles of issues and pull-requests closed between FROM..TO
+#
+# Copyright (C) 2023 Masatake YAMATO
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
 FROM=v6.0.0
 TO=.
 PROJ=universal-ctags
