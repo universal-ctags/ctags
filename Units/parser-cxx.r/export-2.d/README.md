ab9e02107b282eb9c336ca139688a3a4a746708e
Masatake YAMATO <yamato@redhat.com>
2024-02-17 10:39:52 +0900

    Units: add a case testing the export properties of C++
    Signed-off-by: Masatake YAMATO <yamato@redhat.com>

---
+++ Units/parser-cxx.r/export-2.d/README.md
@@ -0,0 +1,3 @@
+The test case testing parts of the syntax (2) in https://en.cppreference.com/w/cpp/language/modules.
+
+	export declaration  (2)
---
+++ Units/parser-cxx.r/export-2.d/input.cpp
@@ -0,0 +1,15 @@
+// Taken from https://en.cppreference.com/w/cpp/language/modules
+
+export module A; // declares the primary module interface unit for named module 'A'
+
+// hello() will be visible by translations units importing 'A'
+export char const* hello() { return "hello"; }
+
+// world() will NOT be visible.
+char const* world() { return "world"; }
+
+export namespace hi
+{
+    char const* english() { return "Hi!"; }
+    char const* french()  { return "Salut!"; }
+}
