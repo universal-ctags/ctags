e98c847573d56bce7474007389545148b017d42c
Alfonso Fernando Álvarez <ponchoalv@gmail.com>
2024-02-24 09:49:13 +0000

    terraform: switch to multi-table ctags regex
    This release will fix two know bugs:
    
    - [x] The parser doesn't ignore strings inside a block comment (``/* ... */``).
    
    - [x] The parser doesn't extract variables defined with ``locals`` keyword.
    
    Update the parser version to 1.1

---
+++ Units/parser-terraform.r/local.d/input.tf
@@ -0,0 +1,3 @@
+locals {
+  this_is_a_local_variable_1 = "this is the value of local variable 1"
+}
--- Units/parser-terraform.r/simple-terraform.d/input.tf
+++ Units/parser-terraform.r/simple-terraform.d/input.tf
@@ -25,3 +25,35 @@ data "aws_ssm_parameter" "example_database_password" {
 output "password" {
   value = data.aws_ssm_parameter.example_database_password.value
 }
+
+locals {
+  this_is_a_local_variable_1 = "this is the value of local variable 1"
+  this_is_a_local_variable_2 = var.events_bus_name == local.this_is_a_local_variable_1 ## var.events_bus_name == shouldn't be picked up as a local
+
+  /* THE DEFINITIONS IN THIS BLOCK SHOULDN'T BE PICKED UP
+variable "no_parse_super_aws_thing" {
+  type = string
+  default = "hello-world"
+}
+resource "aws_cloudwatch_event_bus" "no_parse_example_events_bus_1" {
+  name = var.events_bus_name
+}
+
+module "no_parse_database_1" {
+  source = "../../modules/database"
+}
+
+data "aws_ssm_parameter" "no_parse_example_database_password_1" {
+  name = "example-database-password"
+}
+
+output "no_parse_password_1" {
+  value = data.aws_ssm_parameter.example_database_password.value
+}
+
+locals {
+  no_parse_this_is_a_local_variable_1 = "this is the value of local variable 1"
+
+}
+*/
+}
