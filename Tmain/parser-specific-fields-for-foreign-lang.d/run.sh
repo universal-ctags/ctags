844b0ef930d6c202525d6078c16e9dbb605e203b
Masatake YAMATO <yamato@redhat.com>
2025-01-03 00:31:11 +0900

    Tmain: add cases for testing parser specific fields with data types defined in optlib
    Signed-off-by: Masatake YAMATO <yamato@redhat.com>

--- Tmain/parser-specific-fields-for-foreign-lang.d/run.sh
+++ Tmain/parser-specific-fields-for-foreign-lang.d/run.sh
@@ -8,8 +8,17 @@ CTAGS=$1
 V=
 # V=valgrind
 
-${V} ${CTAGS} --options=NONE --options=./knownz.ctags --options=./unknownx.ctags \
+echo "# output: tags"
+${V} ${CTAGS} --options=NONE --options=./knownz.ctags --sort=no --options=./unknownx.ctags \
 	 --fields=+l \
 	 --fields-unknownx=+'{protection}{signature}' \
 	 --fields-knownz=+'{owner}' \
+	 -o - input.unknownx
+
+echo "# output: xref"
+${V} ${CTAGS} --options=NONE --options=./knownz.ctags --sort=no --options=./unknownx.ctags \
+	 --fields=+l \
+	 --fields-unknownx=+'{protection}{signature}' \
+	 --fields-knownz=+'{owner}' \
+	 -x --_xformat="%N %l / owner:%{knownz.owner},len:%{knownz.len},lenplus:%{knownz.lenplus},exported:%{knownz.exported},stb:%{knownz.stb} / %{unknownx.protection}%{unknownx.signature}" \
 	 -o - input.unknownx
