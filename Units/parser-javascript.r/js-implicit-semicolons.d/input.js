ebb6eb09d9b49da7d9ebb06426be5b1248db2148
Colomban Wendling <ban@herbesfolles.org>
2025-02-17 01:43:51 +0100

    JavaScript: Fix held tokens breaking implicit semicolon insertion
    When emitting a held token, the previous token type was not properly
    updated, breaking the implicit semicolon emission logic for the next
    token.
    
    Fixes #4192.

--- Units/parser-javascript.r/js-implicit-semicolons.d/input.js
+++ Units/parser-javascript.r/js-implicit-semicolons.d/input.js
@@ -14,3 +14,9 @@ var k = new Function('a','b','return a+b')
 function l() {}
 var m = 0 // a single comment doesn't eat the newline
 function n() {}
+/* test from https://github.com/universal-ctags/ctags/issues/4192 */
+var o = () => {
+  return 0
+}
+var p
+function q() {}
