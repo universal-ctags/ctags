45388e0df097a19fd789c40503b9640edeba36bd
Masatake YAMATO <yamato@redhat.com>
2025-05-04 01:26:24 +0900

    Units: add "with BOM" variants of test cases related to the location bugs
    Signed-off-by: Masatake YAMATO <yamato@redhat.com>

---
+++ Units/parser-sh.r/sh-heredoc-run-guest-parser-with-external-libs-and-bom.d/input-0.sh
@@ -0,0 +1,23 @@
+﻿h()
+{
+	:
+}
+
+cat > bar.rmd <<EOFB
+---
+title: example for u-ctags
+---
+# S1
+
+...
+
+# S2
+
+...
+
+EOFB
+
+i()
+{
+	:
+}
---
+++ Units/parser-sh.r/sh-heredoc-run-guest-parser-with-external-libs-and-bom.d/input.sh
@@ -0,0 +1,20 @@
+﻿f()
+{
+	:
+}
+
+cat > foo.xml <<EOFA
+<input xmlns="https://ctags.io/root"
+       xmlns:abc="https://ctags.io/nosuchthing"
+       xmlns:unused="https://ctags.io/unused"
+       >
+<abc:UnKnownTag id="a" xmlns:efg="https://ctags.io/nosuchthing2">
+  <efg:UnKnownTag2 id="b"/>
+</abc:UnKnownTag>
+</input>
+EOFA
+
+g()
+{
+	:
+}
