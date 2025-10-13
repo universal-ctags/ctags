24c6869d6d1d49b655ea9654695fc5e4fb9fe722
Masatake YAMATO <yamato@redhat.com>
2025-10-13 18:25:42 +0900

    Tmain: consider broken version strings in the versioning test case
    Signed-off-by: Masatake YAMATO <yamato@redhat.com>

--- Tmain/versioning.d/run.sh
+++ Tmain/versioning.d/run.sh
@@ -15,3 +15,21 @@ ${CTAGS} --quiet --options=NONE --options=./test.ctags --list-roles=TEST
 ${CTAGS} --quiet --options=NONE --options=./test.ctags --list-fields=TEST
 ${CTAGS} --quiet --options=NONE --options=./test.ctags --list-extras=TEST
 ${CTAGS} --quiet --options=NONE --options=./test.ctags --describe-language=TEST
+
+${CTAGS} --quiet --options=NONE --options=./broken-kind.ctags --_force-quit=0
+echo broken kind: $? 1>&2
+${CTAGS} --quiet --options=NONE --options=./broken-role.ctags --_force-quit=0
+echo broken role: $? 1>&2
+${CTAGS} --quiet --options=NONE --options=./broken-field.ctags --_force-quit=0
+echo broken field: $? 1>&2
+${CTAGS} --quiet --options=NONE --options=./broken-extra.ctags --_force-quit=0
+echo broken extra: $? 1>&2
+
+${CTAGS} --quiet --options=NONE --options=./warning-kind.ctags --_force-quit=0
+echo warning kind: $? 1>&2
+${CTAGS} --quiet --options=NONE --options=./warning-role.ctags --_force-quit=0
+echo warning role: $? 1>&2
+${CTAGS} --quiet --options=NONE --options=./warning-field.ctags --_force-quit=0
+echo warning field: $? 1>&2
+${CTAGS} --quiet --options=NONE --options=./warning-extra.ctags --_force-quit=0
+echo warning extra: $? 1>&2
